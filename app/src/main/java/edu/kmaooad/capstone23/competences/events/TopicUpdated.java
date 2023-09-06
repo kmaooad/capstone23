82ca4374b9388ce5bb2144c56110a7b201bdeec1
Oleg Zagryvyi <olegzagraf@gmail.com>
2023-09-06 14:06:37 +0300

    draft update topic

--- app/src/main/java/edu/kmaooad/capstone23/competences/events/TopicUpdated.java
+++ app/src/main/java/edu/kmaooad/capstone23/competences/events/TopicUpdated.java
@@ -1,4 +1,13 @@
 package edu.kmaooad.capstone23.competences.events;
 
 public class TopicUpdated {
+    private final String topicId;
+
+    public String getTopicId() {
+        return topicId;
+    }
+
+    public TopicUpdated(String topicId) {
+        this.topicId = topicId;
+    }
 }
