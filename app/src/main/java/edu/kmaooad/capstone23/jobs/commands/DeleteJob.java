969defc02da1db6e37ece2cd15aa8156ec295938
Zhelizniak Anna <anna.zhelizniak@ukma.edu.ua>
2023-09-06 22:41:32 +0300

    add delete and update commands

--- app/src/main/java/edu/kmaooad/capstone23/jobs/commands/DeleteJob.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/commands/DeleteJob.java
@@ -1,4 +1,34 @@
 package edu.kmaooad.capstone23.jobs.commands;
 
+import org.bson.types.ObjectId;
+
+import java.util.Objects;
+
 public class DeleteJob {
+    private ObjectId jobId;
+
+    public DeleteJob(ObjectId jobId) {
+        this.jobId = jobId;
+    }
+
+    public ObjectId getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(ObjectId jobId) {
+        this.jobId = jobId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DeleteJob deleteJob = (DeleteJob) o;
+        return jobId.equals(deleteJob.jobId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(jobId);
+    }
 }
