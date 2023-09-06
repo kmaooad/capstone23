c1e7ef4fb3e59327aef24af803d951a826c4e7a6
Zhelizniak Anna <anna.zhelizniak@ukma.edu.ua>
2023-09-06 22:22:15 +0300

    add update controller

---
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/controllers/UpdateJobController.java
@@ -0,0 +1,48 @@
+package edu.kmaooad.capstone23.jobs.controllers;
+
+import edu.kmaooad.capstone23.common.CommandHandler;
+import edu.kmaooad.capstone23.common.HandlingError;
+import edu.kmaooad.capstone23.common.Result;
+import edu.kmaooad.capstone23.jobs.commands.DeleteJob;
+import edu.kmaooad.capstone23.jobs.commands.UpdateJob;
+import edu.kmaooad.capstone23.jobs.events.JobDeleted;
+import edu.kmaooad.capstone23.jobs.events.JobUpdated;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+@Path("/jobs/update")
+public class UpdateJobController {
+    @Inject
+    CommandHandler<UpdateJob, JobUpdated> updateJobHandler;
+    @PUT
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = JobDeleted.class)) }),
+            @APIResponse(responseCode = "400", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = HandlingError.class)) }),
+            @APIResponse(responseCode = "500")
+    })
+    public Response updateJob(UpdateJob command) {
+        try {
+            Result<JobUpdated> result = updateJobHandler.handle(command);
+
+            if (!result.isSuccess()) {
+                return Response.status(400).entity(result.toError()).build();
+            }
+
+            return Response.ok(result.getValue(), MediaType.APPLICATION_JSON).build();
+        } catch (Exception e) {
+            return Response.status(500).build();
+        }
+    }
+}
