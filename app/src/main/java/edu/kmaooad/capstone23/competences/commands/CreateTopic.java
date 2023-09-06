f9eef836964bcf35c02702248437b8c2c621925f
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2023-09-06 14:05:48 +0300

    validate topic create

--- app/src/main/java/edu/kmaooad/capstone23/competences/commands/CreateTopic.java
+++ app/src/main/java/edu/kmaooad/capstone23/competences/commands/CreateTopic.java
@@ -1,6 +1,11 @@
 package edu.kmaooad.capstone23.competences.commands;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class CreateTopic {
+    @NotBlank
+    @Size(min = 5, max = 100)
     public String name;
     public String parentId;
 }
