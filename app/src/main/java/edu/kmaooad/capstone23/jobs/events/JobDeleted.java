8870357431ac56f87f8b7c58a1d4354e129432b1
Mykhailenko Oleksandra <miksasha2003@gmail.com>
2023-09-06 22:49:12 +0300

    all events

--- app/src/main/java/edu/kmaooad/capstone23/jobs/events/JobDeleted.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/events/JobDeleted.java
@@ -1,4 +1,12 @@
 package edu.kmaooad.capstone23.jobs.events;
 
 public class JobDeleted {
+    private String jobId;
+    public String getJobId() {
+        return jobId;
+    }
+
+    public JobDeleted(String jobId) {
+        this.jobId = jobId;
+    }
 }
--- app/src/main/java/edu/kmaooad/capstone23/jobs/events/JobUpdated.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/events/JobUpdated.java
@@ -1,4 +1,12 @@
 package edu.kmaooad.capstone23.jobs.events;
 
 public class JobUpdated {
+    private String jobId;
+    public String getJobId() {
+        return jobId;
+    }
+
+    public JobUpdated(String jobId) {
+        this.jobId = jobId;
+    }
 }
