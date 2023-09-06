a5494b68f64eb59ffb3385eb8ae43cb2d8a4dc2f
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2023-09-06 12:06:55 +0300

    Delete projects

--- app/src/main/java/edu/kmaooad/capstone23/competences/dal/ProjectsRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.kmaooad.capstone23.competences.dal;
-
-import io.quarkus.mongodb.panache.PanacheMongoRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class ProjectsRepository implements PanacheMongoRepository<Project> {
-
-}
