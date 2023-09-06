8ee3228902807e4eea6c0ddc632dfd8442864490
Mykhailenko Oleksandra <miksasha2003@gmail.com>
2023-09-06 22:06:15 +0300

    delete controller

--- app/src/main/java/edu/kmaooad/capstone23/jobs/controllers/DeleteJobController.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/controllers/DeleteJobController.java
@@ -2,16 +2,15 @@ package edu.kmaooad.capstone23.jobs.controllers;
 
 import edu.kmaooad.capstone23.common.CommandHandler;
 import edu.kmaooad.capstone23.common.HandlingError;
+import edu.kmaooad.capstone23.common.Result;
 import edu.kmaooad.capstone23.common.TypicalController;
-import edu.kmaooad.capstone23.jobs.commands.CreateJob;
 import edu.kmaooad.capstone23.jobs.commands.DeleteJob;
-import edu.kmaooad.capstone23.jobs.events.JobCreated;
 import edu.kmaooad.capstone23.jobs.events.JobDeleted;
-import edu.kmaooad.capstone23.orgs.events.OrgCreated;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.POST;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
@@ -19,6 +18,27 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 public class DeleteJobController  extends TypicalController<DeleteJob, JobDeleted> {
     @Inject
-    CommandHandler<DeleteJob, JobDeleted> createOrgHandler;
+    CommandHandler<DeleteJob, JobDeleted> deleteJobHandler;
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = JobDeleted.class)) }),
+            @APIResponse(responseCode = "400", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = HandlingError.class)) }),
+            @APIResponse(responseCode = "500")
+    })
+    public Response deleteJob(DeleteJob command) {
+        try {
+            Result<JobDeleted> result = deleteJobHandler.handle(command);
 
+            if (!result.isSuccess()) {
+                return Response.status(400).entity(result.toError()).build();
+            }
+
+            return Response.ok(result.getValue(), MediaType.APPLICATION_JSON).build();
+        } catch (Exception e) {
+            return Response.status(500).build();
+        }
+    }
 }
