51a8c8aa6951336ed15709fbe09d10f9010394f8
Dan <stickbugdanny@gmail.com>
2023-09-08 01:12:59 +0300

    remove my test comment

--- app/src/main/java/edu/kmaooad/capstone23/orgs/commands/CreateOrg.java
+++ app/src/main/java/edu/kmaooad/capstone23/orgs/commands/CreateOrg.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class CreateOrg {
-// !
     @NotBlank
     @Size(min = 3, max = 50)
     @Pattern(regexp = "^[a-zA-Z0-9\s]*$")
