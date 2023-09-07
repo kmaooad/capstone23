4a735f9ce67264313c02d2f6906406980b93c769
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2023-09-07 14:43:14 +0300

    add member entity

---
+++ app/src/main/java/edu/kmaooad/capstone23/orgs/dal/Member.java
@@ -0,0 +1,15 @@
+package edu.kmaooad.capstone23.orgs.dal;
+
+import io.quarkus.mongodb.panache.common.MongoEntity;
+import org.bson.types.ObjectId;
+
+@MongoEntity(collection = "members")
+public class Member {
+    public ObjectId id;
+    public ObjectId orgId;
+    public String firstName;
+    public String lastName;
+    public String email;
+    public boolean isExpert;
+    public boolean isAdmin;
+}
