42b6b14f57f319f5027c4424b1e5f3d8a611449d
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2023-09-07 17:13:03 +0300

    add CreateMemberController

---
+++ app/src/main/java/edu/kmaooad/capstone23/members/controllers/CreateMemberController.java
@@ -0,0 +1,16 @@
+package edu.kmaooad.capstone23.members.controllers;
+
+import edu.kmaooad.capstone23.common.TypicalController;
+import edu.kmaooad.capstone23.members.commands.CreateMemberBasic;
+import edu.kmaooad.capstone23.members.events.CreatedMemberBasic;
+import edu.kmaooad.capstone23.orgs.events.OrgCreated;
+import jakarta.ws.rs.Path;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+
+@Path("/members/create")
+@APIResponse(responseCode = "200", content = {
+        @Content(mediaType = "application/json", schema = @Schema(implementation = CreatedMemberBasic.class)) })
+public class CreateMemberController extends TypicalController<CreateMemberBasic, CreatedMemberBasic> {
+}
