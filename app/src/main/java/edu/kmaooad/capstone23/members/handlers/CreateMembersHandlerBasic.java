d8c0b0e60c44dd50b04ccc66014c16b4c9e71d30
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2023-09-07 21:17:43 +0300

    Add org existence check

--- app/src/main/java/edu/kmaooad/capstone23/members/handlers/CreateMembersHandlerBasic.java
+++ app/src/main/java/edu/kmaooad/capstone23/members/handlers/CreateMembersHandlerBasic.java
@@ -1,29 +1,44 @@
 package edu.kmaooad.capstone23.members.handlers;
 
 import edu.kmaooad.capstone23.common.CommandHandler;
+import edu.kmaooad.capstone23.common.ErrorCode;
 import edu.kmaooad.capstone23.common.Result;
 import edu.kmaooad.capstone23.members.commands.CreateMemberBasic;
 import edu.kmaooad.capstone23.members.dal.Member;
 import edu.kmaooad.capstone23.members.dal.MembersRepository;
 import edu.kmaooad.capstone23.members.events.CreatedMemberBasic;
+import edu.kmaooad.capstone23.orgs.dal.Org;
+import edu.kmaooad.capstone23.orgs.dal.OrgsRepository;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import org.bson.types.ObjectId;
+
+import java.util.Optional;
 
 @RequestScoped
 public class CreateMembersHandlerBasic implements CommandHandler<CreateMemberBasic, CreatedMemberBasic> {
     @Inject
     MembersRepository membersRepository;
 
+    @Inject
+    OrgsRepository orgsRepository;
+
     @Override
     public Result<CreatedMemberBasic> handle(CreateMemberBasic command) {
-        Member member = new Member();
-        member.firstName = command.getFirstName();
-        member.lastName = command.getLastName();
-        member.email = command.getEmail();
-        member.orgId = new ObjectId(command.getOrgId());
-        membersRepository.insert(member);
-        CreatedMemberBasic result = new CreatedMemberBasic(member.id.toString());
-        return new Result<>(result);
+        try {
+            Member member = new Member();
+            member.firstName = command.getFirstName();
+            member.lastName = command.getLastName();
+            member.email = command.getEmail();
+            member.orgId = new ObjectId(command.getOrgId());
+            Optional<Org> memberOrg = orgsRepository.findByIdOptional(member.orgId);
+            if (memberOrg.isEmpty())
+                return new Result<>(ErrorCode.VALIDATION_FAILED, "Organisation not found");
+            membersRepository.insert(member);
+            CreatedMemberBasic result = new CreatedMemberBasic(member.id.toString());
+            return new Result<>(result);
+        } catch (IllegalArgumentException e) {
+            return new Result<>(ErrorCode.VALIDATION_FAILED, "Invalid org id provided");
+        }
     }
 }
