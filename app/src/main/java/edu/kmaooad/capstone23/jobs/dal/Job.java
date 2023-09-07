5f2fa4309c7361ad2fe15d6183202c7eafc3b8d4
Dan <stickbugdanny@gmail.com>
2023-09-08 00:52:27 +0300

    Update Job.java

--- app/src/main/java/edu/kmaooad/capstone23/jobs/dal/Job.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/dal/Job.java
@@ -1,4 +1,12 @@
 package edu.kmaooad.capstone23.jobs.dal;
 
+import org.bson.types.ObjectId;
+
+import io.quarkus.mongodb.panache.common.MongoEntity;
+
+@MongoEntity(collection = "jobs")
 public class Job {
+    public ObjectId id;
+    public String name;
 }
+
