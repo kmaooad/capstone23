0775f22e023497a5b5ba57971a77ff7daa6506e4
Mykhailenko Oleksandra <miksasha2003@gmail.com>
2023-09-07 17:36:12 +0300

    Delete handler

--- app/src/main/java/edu/kmaooad/capstone23/jobs/events/JobDeleted.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/events/JobDeleted.java
@@ -1,12 +1,14 @@
 package edu.kmaooad.capstone23.jobs.events;
 
+import org.bson.types.ObjectId;
+
 public class JobDeleted {
-    private String jobId;
-    public String getJobId() {
+    private ObjectId jobId;
+    public ObjectId getJobId() {
         return jobId;
     }
 
-    public JobDeleted(String jobId) {
+    public JobDeleted(ObjectId jobId) {
         this.jobId = jobId;
     }
 }
