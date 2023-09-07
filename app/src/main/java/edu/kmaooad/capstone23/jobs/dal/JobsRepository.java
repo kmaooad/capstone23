ac41e03fb75a3773171a65dd1982df787b6ac7f0
Dan <stickbugdanny@gmail.com>
2023-09-08 00:57:24 +0300

    Update JobsRepository.java

--- app/src/main/java/edu/kmaooad/capstone23/jobs/dal/JobsRepository.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/dal/JobsRepository.java
@@ -1,4 +1,18 @@
 package edu.kmaooad.capstone23.jobs.dal;
 
-public class JobsRepository {
+import io.quarkus.mongodb.panache.PanacheMongoRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+
+@ApplicationScoped
+public class JobsRepository implements PanacheMongoRepository<Job> {
+
+    public Job findByName(String name) {
+        return find("name", name).firstResult();
+    }
+
+    public Job insert(Job job){
+        persist(job);
+        return job;
+    }
 }
