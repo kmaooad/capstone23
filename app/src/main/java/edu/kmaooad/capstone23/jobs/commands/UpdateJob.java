906c20c40d383174a34eb3771f0a3931dd68e341
Dan <stickbugdanny@gmail.com>
2023-09-08 21:33:36 +0300

    Create UpdateJob.java

---
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/commands/UpdateJob.java
@@ -0,0 +1,19 @@
+package edu.kmaooad.capstone23.jobs.commands;
+
+public class UpdateJob {
+
+    private String jobId;
+    private String jobName;
+    private String jobDescription;
+
+    public String getJobId() {return jobId;}
+
+    public String getJobName() {return jobName;}
+
+    public void setJobName(String jobName) {this.jobName = jobName;}
+
+    public String getJobDescription() {return jobDescription;}
+
+    public void setJobDescription(String jobDescription) {this.jobDescription = jobDescription;}
+
+}
