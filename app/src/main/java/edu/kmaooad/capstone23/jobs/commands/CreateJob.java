18c81655f19e2c269dcf2e778b7263ecb3dc5dd3
Mykhailenko Oleksandra <miksasha2003@gmail.com>
2023-09-08 13:28:09 +0300

    create controllers tests done

--- app/src/main/java/edu/kmaooad/capstone23/jobs/commands/CreateJob.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/commands/CreateJob.java
@@ -1,9 +1,15 @@
 package edu.kmaooad.capstone23.jobs.commands;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import org.bson.types.ObjectId;
 
 public class CreateJob {
     private ObjectId id;
+    @NotBlank
+    @Size(min = 3, max = 50)
+    @Pattern(regexp = "^[a-zA-Z0-9\s]*$")
     private String name;
     private boolean active;
     private ObjectId[] competencesId;
--- app/src/test/java/edu/kmaooad/capstone23/orgs/jobs/controllers/DeleteJobControllerTest.java
+++ app/src/test/java/edu/kmaooad/capstone23/orgs/jobs/controllers/DeleteJobControllerTest.java
@@ -1,4 +1,28 @@
 package edu.kmaooad.capstone23.orgs.jobs.controllers;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
 public class DeleteJobControllerTest {
+
+    @Test
+    @DisplayName("Delete job: valid input")
+    public void testBasicJobDeleting() {
+        Map<String, Object> jsonAsMap = new HashMap<>();
+        jsonAsMap.put("name", "Professor");
+
+        given()
+                .contentType("application/json")
+                .body(jsonAsMap)
+                .when()
+                .delete("/jobs/delete")
+                .then()
+                .statusCode(200);
+    }
+
 }
