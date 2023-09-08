a30c7b6569d37f135bb2ece9895fefd257720b7d
Mykhailenko Oleksandra <miksasha2003@gmail.com>
2023-09-08 15:25:33 +0300

    job delete controller tests done

--- app/src/test/java/edu/kmaooad/capstone23/orgs/jobs/controllers/DeleteJobControllerTest.java
+++ app/src/test/java/edu/kmaooad/capstone23/orgs/jobs/controllers/DeleteJobControllerTest.java
@@ -1,23 +1,48 @@
 package edu.kmaooad.capstone23.orgs.jobs.controllers;
 
+import edu.kmaooad.capstone23.common.CommandHandler;
+import edu.kmaooad.capstone23.jobs.commands.CreateJob;
+import edu.kmaooad.capstone23.jobs.events.JobCreated;
 import io.quarkus.test.junit.QuarkusTest;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import edu.kmaooad.capstone23.common.Result;
 import java.util.HashMap;
 import java.util.Map;
-
+import jakarta.inject.Inject;
 import static io.restassured.RestAssured.given;
 
 @QuarkusTest
 public class DeleteJobControllerTest {
 
+    @Inject
+    CommandHandler<CreateJob, JobCreated> handler;
     @Test
     @DisplayName("Delete job: valid input")
     public void testBasicJobDeleting() {
+        //CommandHandler<CreateJob, JobCreated> handler = null;
+
+        CreateJob command = new CreateJob("Teacher", true);
+        Result<JobCreated> result = handler.handle(command);
+
+//        Map<String, Object> jsonAsMap = new HashMap<>();
+//        ObjectId id = new ObjectId("64faf6ad341e202c91f76c84");
+//        jsonAsMap.put("_id", id);
+
+        given()
+                .contentType("application/json")
+                .when()
+                .delete("/jobs/delete/".concat(result.getValue().getJobId()))
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Delete job: invalid input")
+    public void testJobDeletingWithInvalidInput() {
         Map<String, Object> jsonAsMap = new HashMap<>();
-        ObjectId id = new ObjectId("64faf6ad341e202c91f76c84");
+        ObjectId id = new ObjectId("64faf6ad322e202c91f76c84");
         jsonAsMap.put("_id", id);
 
         given()
@@ -26,6 +51,6 @@ public class DeleteJobControllerTest {
                 .when()
                 .delete("/jobs/delete/"+id)
                 .then()
-                .statusCode(200);
+                .statusCode(400);
     }
 }
