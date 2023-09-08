e38e5c9114a4ac2a539f7aa187a87136a27b732a
Zhelizniak Anna <anna.zhelizniak@ukma.edu.ua>
2023-09-08 14:51:59 +0300

    detailed output of delete error

--- app/src/main/java/edu/kmaooad/capstone23/jobs/controllers/TypicalDeleteController.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/controllers/TypicalDeleteController.java
@@ -6,10 +6,7 @@ import edu.kmaooad.capstone23.common.Result;
 import edu.kmaooad.capstone23.jobs.events.JobDeleted;
 import edu.kmaooad.capstone23.orgs.events.OrgCreated;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.DELETE;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
@@ -31,9 +28,9 @@ public class TypicalDeleteController<T1, T2> {
                     @Content(mediaType = "application/json", schema = @Schema(implementation = HandlingError.class)) }),
             @APIResponse(responseCode = "500")
     })
-    public Response delete(T1 command) {
+    public Response delete(@PathParam("id") String id) {
         try {
-            Result<T2> result = commandHandler.handle(command);
+            Result<T2> result = commandHandler.handle((T1) id);
 
             if (!result.isSuccess()) {
                 System.out.println(Response.status(400).entity(result.toError()).build());
