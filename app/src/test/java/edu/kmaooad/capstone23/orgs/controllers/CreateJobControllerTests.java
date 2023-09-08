14c9be29379bfd2a40f49d304a650b7109d038f4
Dan <stickbugdanny@gmail.com>
2023-09-08 19:25:17 +0300

    Update CreateJobControllerTests.java

--- app/src/test/java/edu/kmaooad/capstone23/orgs/controllers/CreateJobControllerTests.java
+++ app/src/test/java/edu/kmaooad/capstone23/orgs/controllers/CreateJobControllerTests.java
@@ -2,8 +2,30 @@ package edu.kmaooad.capstone23.orgs.controllers;
 
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
 
 @QuarkusTest
 public class CreateJobControllerTests {
 
+    //trying to make it all as close to ORGS as possible in order to make the code more comprehensible
+    @Test
+    @DisplayName("Create Job: Basic")
+    public void testBasicJobCreation() {
+        Map<String, Object> jsonAsMap = new HashMap<>();
+        jsonAsMap.put("jobName", "test_job_name");
+
+        given()
+                .contentType("application/json")
+                .body(jsonAsMap)
+                .when()
+                .post("/jobs/create")
+                .then()
+                .statusCode(200);
+    }
 }
