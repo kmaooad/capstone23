276f45a0c59fec3acd996a5fb3a0c9e02a67bcc7
Dan <stickbugdanny@gmail.com>
2023-09-08 12:35:24 +0300

    Update CreateJob.java

--- app/src/main/java/edu/kmaooad/capstone23/jobs/commands/CreateJob.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/commands/CreateJob.java
@@ -1,6 +1,37 @@
 package edu.kmaooad.capstone23.jobs.commands;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class CreateJob {
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    @Pattern(regexp = "^[a-zA-Z0-9\s]*$")
+    private String jobName;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    @Pattern(regexp = "^[a-zA-Z0-9\s]*$")
+    private String jobDescription;
+
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public String getJobDescription() {
+        return jobDescription;
+    }
+
+    public void setJobDescription(String jobDescription) {
+        this.jobDescription = jobDescription;
+    }
 
 }
