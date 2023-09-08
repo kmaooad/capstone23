bd4a78b3bcc08108ba6143953a0794c6586e04e4
Dan <stickbugdanny@gmail.com>
2023-09-08 21:33:30 +0300

    Update CreateJobHandlerTest.java

--- app/src/test/java/edu/kmaooad/capstone23/orgs/handlers/CreateJobHandlerTest.java
+++ app/src/test/java/edu/kmaooad/capstone23/orgs/handlers/CreateJobHandlerTest.java
@@ -4,8 +4,7 @@ import edu.kmaooad.capstone23.common.CommandHandler;
 import edu.kmaooad.capstone23.common.Result;
 import edu.kmaooad.capstone23.jobs.commands.CreateJob;
 import edu.kmaooad.capstone23.jobs.events.JobCreated;
-import edu.kmaooad.capstone23.orgs.commands.CreateOrg;
-import edu.kmaooad.capstone23.orgs.events.OrgCreated;
+
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Assertions;
