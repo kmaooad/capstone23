ae5f40d89c08be48180541aabf5bf06e6a70b1cf
Dan <stickbugdanny@gmail.com>
2023-09-08 13:43:32 +0300

    Update CreateJobHandler.java

--- app/src/main/java/edu/kmaooad/capstone23/jobs/handlers/CreateJobHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/handlers/CreateJobHandler.java
@@ -1,4 +1,29 @@
 package edu.kmaooad.capstone23.jobs.handlers;
 
-public class CreateJobHandler {
+import edu.kmaooad.capstone23.common.CommandHandler;
+import edu.kmaooad.capstone23.common.Result;
+import edu.kmaooad.capstone23.jobs.commands.CreateJob;
+import edu.kmaooad.capstone23.jobs.dal.Job;
+import edu.kmaooad.capstone23.jobs.dal.JobsRepository;
+import edu.kmaooad.capstone23.jobs.events.JobCreated;
+
+import jakarta.inject.Inject;
+
+
+public class CreateJobHandler implements CommandHandler<CreateJob, JobCreated> {
+
+    @Inject
+    private JobsRepository repository;
+
+    public Result<JobCreated> handle(CreateJob command) {
+
+        Job job = new Job();
+        job.name = command.getJobName();
+
+        repository.insert(job);
+
+        JobCreated result = new JobCreated(job.id.toString());
+
+        return new Result<JobCreated>(result);
+    }
 }
