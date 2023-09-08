d0103ae0307d10b0204b69e8169ac0392b256519
Dan <stickbugdanny@gmail.com>
2023-09-08 13:18:34 +0300

    Update JobCreated.java

--- app/src/main/java/edu/kmaooad/capstone23/jobs/events/JobCreated.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/events/JobCreated.java
@@ -1,4 +1,27 @@
 package edu.kmaooad.capstone23.jobs.events;
 
 public class JobCreated {
+
+    private String jobId;
+
+    private String jobDescription;
+
+
+
+    public String getJobId() {
+        return jobId;
+    }
+
+    public String getJobDescription() {return jobDescription;}
+
+
+    public JobCreated(String jobId) {
+        this.jobId = jobId;
+    }
+
+    public JobCreated(String jobId, String jobDescription) {
+        this.jobId = jobId;
+        this.jobDescription = jobDescription;
+    }
+
 }
