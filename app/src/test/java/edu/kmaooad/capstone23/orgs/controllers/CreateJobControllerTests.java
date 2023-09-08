5c116612b03c44dc97c7925e8a7e05a15498f05c
Dan <stickbugdanny@gmail.com>
2023-09-08 20:27:22 +0300

    Update CreateJobControllerTests.java

--- app/src/test/java/edu/kmaooad/capstone23/orgs/controllers/CreateJobControllerTests.java
+++ app/src/test/java/edu/kmaooad/capstone23/orgs/controllers/CreateJobControllerTests.java
@@ -13,27 +13,28 @@ import static io.restassured.RestAssured.given;
 @QuarkusTest
 public class CreateJobControllerTests {
 
-    //TODO: find out why this test isn't working (gives 400 instead of 200)
-//    @Test
-//    @DisplayName("Create Job: Basic")
-//    public void testBasicJobCreation() {
-//        Map<String, Object> jsonAsMap = new HashMap<>();
-//        jsonAsMap.put("jobName", "test_job_name!!");
-//
-//        given()
-//                .contentType("application/json")
-//                .body(jsonAsMap)
-//                .when()
-//                .post("/jobs/create")
-//                .then()
-//                .statusCode(200);
-//    }
+
+    @Test
+    @DisplayName("Create Job: Basic")
+    public void testBasicJobCreation() {
+        Map<String, Object> jsonAsMap = new HashMap<>();
+        jsonAsMap.put("jobName", "testJobName");
+
+        given()
+                .contentType("application/json")
+                .body(jsonAsMap)
+                .when()
+                .post("/jobs/create")
+                .then()
+                //.log().all() //for easier debugging
+                .statusCode(200);
+    }
 
     @Test
     @DisplayName("Create Job: Name validation")
     public void testJobCreationWithNameValidation() {
         Map<String, Object> jsonAsMap = new HashMap<>();
-        jsonAsMap.put("jobName", "test_invalid_job_name");
+        jsonAsMap.put("jobName", "test_Invalid_Job_Name");
 
         given()
                 .contentType("application/json")
