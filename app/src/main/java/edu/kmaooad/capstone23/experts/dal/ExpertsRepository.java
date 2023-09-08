1b3c370ab29d14db33715d3cad848a2726fa853f
Bohdan Arkhypchuk <arkhypchuk.b@gmail.com>
2023-09-08 09:34:30 +0200

    Updated Experts Repository

--- app/src/main/java/edu/kmaooad/capstone23/experts/dal/ExpertsRepository.java
+++ app/src/main/java/edu/kmaooad/capstone23/experts/dal/ExpertsRepository.java
@@ -1,4 +1,23 @@
 package edu.kmaooad.capstone23.experts.dal;
 
-public class ExpertsRepository {
+import edu.kmaooad.capstone23.orgs.dal.Org;
+import io.quarkus.mongodb.panache.PanacheMongoRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.List;
+
+@ApplicationScoped
+public class ExpertsRepository implements PanacheMongoRepository<Expert> {
+
+    public Expert findByName(String name) {
+        return find("name", name).firstResult();
+    }
+    public List<Expert> findByOrg(String org) {
+        return find("org", org).list();
+    }
+
+    public Expert insert(Expert expert){
+        persist(expert);
+        return expert;
+    }
 }
