5f63364f9a6bb4506f470ad39b00fe71f29dcc90
Zhelizniak Anna <anna.zhelizniak@ukma.edu.ua>
2023-09-08 14:12:07 +0300

    change delete dests

--- app/src/test/java/edu/kmaooad/capstone23/orgs/jobs/controllers/DeleteJobControllerTest.java
+++ app/src/test/java/edu/kmaooad/capstone23/orgs/jobs/controllers/DeleteJobControllerTest.java
@@ -16,13 +16,14 @@ public class DeleteJobControllerTest {
     @DisplayName("Delete job: valid input")
     public void testBasicJobDeleting() {
         Map<String, Object> jsonAsMap = new HashMap<>();
-        jsonAsMap.put("_id", "64faf6ad341e202c91f76c84");
+        String id = "64faf6ad341e202c91f76c84";
+        jsonAsMap.put("_id", id);
 
         given()
                 .contentType("application/json")
                 .body(jsonAsMap)
                 .when()
-                .delete("/jobs/delete")
+                .delete("/jobs/delete/"+id)
                 .then()
                 .statusCode(200);
     }
