b2079494a448c368c9a3a553d948a5968c74a9ef
Dan <stickbugdanny@gmail.com>
2023-09-08 13:38:07 +0300

    Update CreateJobController.java

--- app/src/main/java/edu/kmaooad/capstone23/jobs/controllers/CreateJobController.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/controllers/CreateJobController.java
@@ -5,6 +5,13 @@ import edu.kmaooad.capstone23.common.TypicalController;
 import edu.kmaooad.capstone23.jobs.commands.CreateJob;
 import edu.kmaooad.capstone23.jobs.events.JobCreated;
 
-public class CreateJobController extends TypicalController<CreateJob, JobCreated> {
+import jakarta.ws.rs.Path;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
+@Path("/jobs/create")
+@APIResponse(responseCode = "200", content = {
+        @Content(mediaType = "application/json", schema = @Schema(implementation = JobCreated.class)) })
+public class CreateJobController extends TypicalController<CreateJob, JobCreated> {
 }
