99c7a437865a13dda645193dfec28b73411afb0d
Dan <stickbugdanny@gmail.com>
2023-09-10 00:39:20 +0300

    Update JobUpdated.java

--- app/src/main/java/edu/kmaooad/capstone23/jobs/events/JobUpdated.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/events/JobUpdated.java
@@ -1,4 +1,10 @@
 package edu.kmaooad.capstone23.jobs.events;
 
 public class JobUpdated {
+
+    private String jobId;
+
+    public JobUpdated(String jobId) {
+        this.jobId = jobId;
+    }
 }
