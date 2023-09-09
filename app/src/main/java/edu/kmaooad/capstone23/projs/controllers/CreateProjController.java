071cbe789c11e5ec1528d4f9961b1e281912c671
Fourier2718281828 <non-linear@ukr.net>
2023-09-09 19:01:56 +0300

    added: CreateProjController

--- app/src/main/java/edu/kmaooad/capstone23/projs/controllers/CreateProjController.java
+++ app/src/main/java/edu/kmaooad/capstone23/projs/controllers/CreateProjController.java
@@ -1,4 +1,15 @@
 package edu.kmaooad.capstone23.projs.controllers;
 
-public class CreateProjController {
+import edu.kmaooad.capstone23.common.TypicalController;
+import edu.kmaooad.capstone23.projs.commands.CreateProj;
+import edu.kmaooad.capstone23.projs.events.ProjCreated;
+import jakarta.ws.rs.Path;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+
+@Path("/projs/create")
+@APIResponse(responseCode = "200", content = {
+        @Content(mediaType = "application/json", schema = @Schema(implementation = ProjCreated.class)) })
+public class CreateProjController extends TypicalController<CreateProj, ProjCreated> {
 }
