1045f079acbc4401a54e08b948b56571d653d621
Andrii Dubovyk <and.dubovyk@gmail.com>
2023-09-09 18:31:00 +0300

    Add skill set created event

---
+++ app/src/main/java/edu/kmaooad/capstone23/competences/events/SkillSetCreated.java
@@ -0,0 +1,13 @@
+package edu.kmaooad.capstone23.orgs.events;
+
+public class SkillSetCreated {
+    private String skillSetId;
+
+    public String getSkillSetId() {
+        return skillSetId;
+    }
+
+    public SkillSetCreated(String skillSetId) {
+        this.skillSetId = orgId;
+    }
+}
