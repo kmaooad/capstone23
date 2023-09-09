95988fc63509c4d60c9f0cbd61c5a8252d46f4f2
Annnanke <kirikanna3005@gmail.com>
2023-09-09 13:02:32 -0400

    ProjRepository updated
    findByName() and insert() created

--- app/src/main/java/edu/kmaooad/capstone23/projs/dal/ProjsRepository.java
+++ app/src/main/java/edu/kmaooad/capstone23/projs/dal/ProjsRepository.java
@@ -1,4 +1,17 @@
 package edu.kmaooad.capstone23.projs.dal;
 
-public class ProjsRepository {
+import io.quarkus.mongodb.panache.PanacheMongoRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ProjsRepository implements PanacheMongoRepository<Proj> {
+
+    public Proj findByName(String name) {
+        return find("name", name).firstResult();
+    }
+
+    public Proj insert(Proj proj) {
+        persist(proj);
+        return proj;
+    }
 }
