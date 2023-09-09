b8ccf641eb30a55c25e25da455b633dbf7e17129
Andrii Dubovyk <and.dubovyk@gmail.com>
2023-09-09 20:37:52 +0300

    Change name of the field in skill set update event

--- app/src/main/java/edu/kmaooad/capstone23/competences/events/SkillSetUpdated.java
+++ app/src/main/java/edu/kmaooad/capstone23/competences/events/SkillSetUpdated.java
@@ -2,18 +2,18 @@ package edu.kmaooad.capstone23.competences.events;
 
 public class SkillSetUpdated {
     private final String skillSetId;
-    private final String name;
+    private final String skillSetName;
 
     public SkillSetUpdated(String skillSetId, String name) {
         this.skillSetId = skillSetId;
-        this.name = name;
+        this.skillSetName = name;
     }
 
     public String getSkillSetId() {
         return skillSetId;
     }
 
-    public String getName() {
-        return name;
+    public String getSkillSetName() {
+        return skillSetName;
     }
 }
