6108f71829ec0c50b54ab0d68725f7544f2adcf9
Bohdan Arkhypchuk <arkhypchuk.b@gmail.com>
2023-09-09 15:17:14 +0200

    added all files for experts

---
+++ app/src/main/java/edu/kmaooad/capstone23/experts/controllers/UpdateExpertController.java
@@ -0,0 +1,4 @@
+package edu.kmaooad.capstone23.experts.controllers;
+
+public class UpdateExpertController {
+}
---
+++ app/src/main/java/edu/kmaooad/capstone23/experts/handlers/UpdateExpertHadler.java
@@ -0,0 +1,4 @@
+package edu.kmaooad.capstone23.experts.handlers;
+
+public class UpdateExpertHadler {
+}
