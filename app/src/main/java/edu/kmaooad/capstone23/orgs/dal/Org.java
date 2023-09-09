bf99b935cbfabca5574d74fbd6d99aa100f1599b
annazhelizniak <84460049+annazhelizniak@users.noreply.github.com>
2023-09-09 18:26:33 +0300

    Update Org.java

--- app/src/main/java/edu/kmaooad/capstone23/orgs/dal/Org.java
+++ app/src/main/java/edu/kmaooad/capstone23/orgs/dal/Org.java
@@ -1,12 +1,10 @@
 package edu.kmaooad.capstone23.orgs.dal;
 
-import jakarta.enterprise.context.ApplicationScoped;
 import org.bson.types.ObjectId;
 
 import io.quarkus.mongodb.panache.common.MongoEntity;
 
 @MongoEntity(collection = "orgs")
-
 public class Org {
     public ObjectId id;
     public String name;
