7a5dfd5e95709cefff3a2e335a8d88e092eddfcf
IvanovAdrew <zyablik7771@gmail.com>
2023-09-09 22:06:20 +0300

    fixed CreateOrgController and created DeleteDepartment

--- app/src/main/java/edu/kmaooad/capstone23/departments/controllers/CreateOrgController.java
+++ app/src/main/java/edu/kmaooad/capstone23/departments/controllers/CreateOrgController.java
@@ -2,9 +2,9 @@ package edu.kmaooad.capstone23.departments.controllers;
 
 import edu.kmaooad.capstone23.common.TypicalController;
 import edu.kmaooad.capstone23.departments.commands.CreateDepartment;
-import edu.kmaooad.capstone23.departments.events.DepartmentsCreated;
+import edu.kmaooad.capstone23.departments.events.DepartmentCreated;
 import jakarta.ws.rs.Path;
 
 @Path("/departemnts/create")
-public class CreateOrgController extends TypicalController<CreateDepartment, DepartmentsCreated> {
+public class CreateOrgController extends TypicalController<CreateDepartment, DepartmentCreated> {
 }
