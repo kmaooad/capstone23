a3b4a0f65c0ab156e138b91d91ff5c2f7f886eee
Annnanke <kirikanna3005@gmail.com>
2023-09-09 13:05:03 -0400

    Events created
    events for delete, create and update actions

--- app/src/main/java/edu/kmaooad/capstone23/projs/events/ProjDeleted.java
+++ app/src/main/java/edu/kmaooad/capstone23/projs/events/ProjDeleted.java
@@ -1,12 +1,13 @@
 package edu.kmaooad.capstone23.projs.events;
 import java.util.List;
-public class ProjCreated {
+public class ProjDeleted {
     private String projId;
 
     public String getProjId() {
         return projId;
     }
-    public ProjCreated(String projId) {
+
+    public ProjDeleted(String projId) {
         this.projId = projId;
     }
 }
