5c68d5fde6ec61c90755387ea3bcfafaa8dcfbc3
Annnanke <kirikanna3005@gmail.com>
2023-09-09 12:25:43 -0400

    Project entity created
    CreateProj.java created with necessary getters&setters

--- app/src/main/java/edu/kmaooad/capstone23/projs/commands/CreateProj.java
+++ app/src/main/java/edu/kmaooad/capstone23/projs/commands/CreateProj.java
@@ -1,4 +1,51 @@
 package edu.kmaooad.capstone23.projs.commands;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class CreateProj {
+
+    @NotBlank
+    @Size(min = 3, max = 30)
+    @Pattern(regexp = "^[a-zA-Z0-9\\s]*$")
+    private String name;
+
+    private String description;
+
+    private List<String> skills;
+
+    private List<String> skillSets;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<String> skills) {
+        this.skills = skills;
+    }
+
+    public List<String> getSkillSets() {
+        return skillSets;
+    }
+
+    public void setSkillSets(List<String> skillSets) {
+        this.skillSets = skillSets;
+    }
 }
--- app/src/main/java/edu/kmaooad/capstone23/projs/dal/Proj.java
+++ app/src/main/java/edu/kmaooad/capstone23/projs/dal/Proj.java
@@ -1,4 +1,14 @@
 package edu.kmaooad.capstone23.projs.dal;
+import org.bson.types.ObjectId;
 
-public class Proj {
+import io.quarkus.mongodb.panache.common.MongoEntity;
+
+@MongoEntity(collection = "projs")
+public class Projs {
+    public ObjectId id;
+    public String name;
+    public String description;
+    public List<String> skills;
+    public List<String> skillSets;
+
 }
