31d4d2824203f75da0a292f9e50a309e6e2850d4
illia.melnyk <alexendemic@gmail.com>
2023-09-09 19:10:24 +0300

    add findById

--- app/src/main/java/edu/kmaooad/capstone23/competences/dal/SkillSetRepository.java
+++ app/src/main/java/edu/kmaooad/capstone23/competences/dal/SkillSetRepository.java
@@ -2,9 +2,21 @@ package edu.kmaooad.capstone23.competences.dal;
 
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import org.bson.types.ObjectId;
+
+import java.util.Optional;
 
 @ApplicationScoped
 public class SkillSetRepository implements PanacheMongoRepository<SkillSet> {
+
+    public Optional<SkillSet> findById(String id) {
+        try {
+            var objectId = new ObjectId(id);
+            return findByIdOptional(objectId);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
 
     public SkillSet insert(SkillSet skillSet){
         persist(skillSet);
