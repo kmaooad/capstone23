e09717e3ef5b16fbc8fdfc1148277e7d35ea38f9
Anastasia Zolotar <anazolotar28@gmail.com>
2023-09-11 11:24:50 +0300

    add UpdateCV feature

--- app/src/main/java/edu/kmaooad/capstone23/cvs/commands/UpdateCV.java
+++ app/src/main/java/edu/kmaooad/capstone23/cvs/commands/UpdateCV.java
@@ -2,50 +2,30 @@ package edu.kmaooad.capstone23.cvs.commands;
 
 import edu.kmaooad.capstone23.cvs.dal.CV;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
-import jakarta.validation.constraints.Size;
 
-import java.time.LocalDateTime;
-
-public class CreateCV {
+public class UpdateCV {
 
     @NotNull
     private String cvId;
 
-    @PastOrPresent
-    private LocalDateTime dateTimeCreated;
+    private CV.Status status;
 
-    @Size(min = 10, max = 500)
+    private CV.Visibility visibility;
+
     private String textInfo;
 
-    private CV.Status status;
-    private CV.Visibility visibility;
-    private boolean autoAddCompetences;
-
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
+    public String getCvId() {
+        return cvId;
     }
 
-    public LocalDateTime getDateTimeCreated() {
-        return dateTimeCreated;
-    }
-    public void setDateTimeCreated(LocalDateTime dateTimeCreated) {
-        this.dateTimeCreated = dateTimeCreated;
-    }
-
-    public String getTextInfo() {
-        return textInfo;
-    }
-    public void setTextInfo(String textInfo) {
-        this.textInfo = textInfo;
+    public void setCvId(String cvId) {
+        this.cvId = cvId;
     }
 
     public CV.Status getStatus() {
         return status;
     }
+
     public void setStatus(CV.Status status) {
         this.status = status;
     }
@@ -53,15 +33,16 @@ public class CreateCV {
     public CV.Visibility getVisibility() {
         return visibility;
     }
+
     public void setVisibility(CV.Visibility visibility) {
         this.visibility = visibility;
     }
 
-    public boolean isAutoAddCompetences() {
-        return autoAddCompetences;
-    }
-    public void setAutoAddCompetences(boolean autoAddCompetences) {
-        this.autoAddCompetences = autoAddCompetences;
+    public String getTextInfo() {
+        return textInfo;
     }
 
+    public void setTextInfo(String textInfo) {
+        this.textInfo = textInfo;
+    }
 }
--- app/src/main/java/edu/kmaooad/capstone23/cvs/events/CVUpdated.java
+++ app/src/main/java/edu/kmaooad/capstone23/cvs/events/CVUpdated.java
@@ -1,43 +1,13 @@
 package edu.kmaooad.capstone23.cvs.events;
 
 public class CVUpdated {
-    private final String id;
-    private final LocalDateTime dateTimeCreated;
-    private final String textInfo;
-    private final CV.Status status;
-    private final CV.Visibility visibility;
-    private final boolean autoAddCompetences;
+    private String cvId;
 
-    public CVUpdated(String id,LocalDateTime dateTimeCreated, String textInfo, CV.Status status,CV.Visibility visibility,boolean autoAddCompetences) {
-        this.id = id;
-        this.dateTimeCreated = dateTimeCreated;
-        this.textInfo = textInfo;
-        this.status = status;
-        this.visibility = visibility;
-        this.autoAddCompetences = autoAddCompetences;
+    public String getCVId(){
+        return cvId;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public LocalDateTime getDateTimeCreated() {
-        return dateTimeCreated;
-    }
-
-    public String getTextInfo() {
-        return textInfo;
-    }
-
-    public CV.Status getStatus() {
-        return status;
-    }
-
-    public CV.Visibility getVisibility() {
-        return visibility;
-    }
-
-    public boolean isAutoAddCompetences() {
-        return autoAddCompetences;
+    public CVUpdated(String cvId) {
+        this.cvId = cvId;
     }
 }
