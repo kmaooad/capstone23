874eb2c302f8884d9cf56edafdf32142447217cc
MityA32 <hetmandmitriy@gmail.com>
2023-09-11 19:23:46 +0300

    create Student entity

---
+++ app/src/main/java/edu/kmaooad/capstone23/students/Student.java
@@ -0,0 +1,8 @@
+package edu.kmaooad.capstone23.students;
+
+import edu.kmaooad.capstone23.activities.Activity;
+
+public class Student {
+    public String name;
+    public Activity[] activities;
+}
