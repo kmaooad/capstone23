6e8f507ab35956874162df979b3277ad08eae63b
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2023-09-11 23:19:11 +0300

    Add expert type to entity

--- app/src/main/java/edu/kmaooad/capstone23/experts/dal/ExpertInvitation.java
+++ app/src/main/java/edu/kmaooad/capstone23/experts/dal/ExpertInvitation.java
@@ -1,14 +1,15 @@
 package edu.kmaooad.capstone23.experts.dal;
 
+import edu.kmaooad.capstone23.experts.ExpertType;
 import io.quarkus.mongodb.panache.common.MongoEntity;
 import io.quarkus.mongodb.panache.PanacheMongoEntity;
-import org.bson.types.ObjectId;
 
 import java.time.LocalDateTime;
 
 @MongoEntity(collection = "expert_invitations")
 public class ExpertInvitation extends PanacheMongoEntity {
     public String email;
+    public ExpertType expertType;
     public LocalDateTime createdAt;
     public boolean accepted;
 }
