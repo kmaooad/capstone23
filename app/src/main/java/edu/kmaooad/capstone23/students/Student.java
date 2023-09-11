cfee49eee320e0dc40b4e2764cd732ad1da957c7
MityA32 <hetmandmitriy@gmail.com>
2023-09-11 21:13:48 +0300

    update Student entity

--- app/src/main/java/edu/kmaooad/capstone23/students/Student.java
+++ app/src/main/java/edu/kmaooad/capstone23/students/Student.java
@@ -1,8 +1,12 @@
 package edu.kmaooad.capstone23.students;
 
+import io.quarkus.mongodb.panache.common.MongoEntity;
 import edu.kmaooad.capstone23.activities.Activity;
+import org.bson.types.ObjectId;
 
+@MongoEntity(collection = "students")
 public class Student {
+    public ObjectId objectId;
     public String name;
     public Activity[] activities;
 }
