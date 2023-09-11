587a933e7f626599208efca09c6ac1a432571179
MityA32 <hetmandmitriy@gmail.com>
2023-09-11 21:54:35 +0300

    update Student entity

--- app/src/main/java/edu/kmaooad/capstone23/activities/Activity.java
+++ app/src/main/java/edu/kmaooad/capstone23/activities/Activity.java
@@ -29,6 +29,5 @@ public class Activity {
     public String getName() {
         return name;
     }
-
 }
 
--- app/src/main/java/edu/kmaooad/capstone23/students/dal/Student.java
+++ app/src/main/java/edu/kmaooad/capstone23/students/dal/Student.java
@@ -3,10 +3,33 @@ package edu.kmaooad.capstone23.students.dal;
 import io.quarkus.mongodb.panache.common.MongoEntity;
 import edu.kmaooad.capstone23.activities.Activity;
 import org.bson.types.ObjectId;
+import java.util.ArrayList;
+import java.util.List;
 
 @MongoEntity(collection = "students")
 public class Student {
     public ObjectId objectId;
-    public String name;
-    public Activity[] activities;
+    private String name;
+    private List<Activity> assignedActivities;
+
+    public Student(String name) {
+        this.name = name;
+        this.assignedActivities = new ArrayList<>();
+    }
+
+    public void assignActivity(Activity activity) {
+        assignedActivities.add(activity);
+    }
+
+    public void unassignActivity(Activity activity) {
+        assignedActivities.remove(activity);
+    }
+
+    public List<Activity> getAssignedActivities() {
+        return assignedActivities;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
