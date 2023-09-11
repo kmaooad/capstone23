fd9a949961b90868d124270597324b281887df21
Illia Shevchyk <illia.shevchyk@ukma.edu.ua>
2023-09-11 22:35:47 +0200

    Fix createInvitationLink controller

--- app/src/main/java/edu/kmaooad/capstone23/experts/controllers/CreateInvitationLinkController.java
+++ app/src/main/java/edu/kmaooad/capstone23/experts/controllers/CreateInvitationLinkController.java
@@ -2,7 +2,7 @@ package edu.kmaooad.capstone23.experts.controllers;
 
 import edu.kmaooad.capstone23.common.TypicalController;
 import edu.kmaooad.capstone23.experts.commands.CreateInvitationLink;
-import edu.kmaooad.capstone23.experts.handlers.CreateInvitationLinkHandler;
+import edu.kmaooad.capstone23.experts.events.InvitationLinkCreated;
 import jakarta.ws.rs.Path;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
@@ -12,5 +12,5 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 @APIResponse(responseCode = "200", content = {
         @Content(mediaType = "application/json", schema = @Schema(implementation = CreateInvitationLink.class))
 })
-public class CreateInvitationLinkController extends TypicalController<CreateInvitationLink, CreateInvitationLinkHandler> {
+public class CreateInvitationLinkController extends TypicalController<CreateInvitationLink, InvitationLinkCreated> {
 }
