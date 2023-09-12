599f8e9e9484f2a49990807e3ceae52cdb83b549
VasylPupok <dejnekaartem223@gmail.com>
2023-09-12 04:18:57 +0300

    Added handlers

--- app/src/main/java/edu/kmaooad/capstone23/jobs/handlers/ActivateJobHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/handlers/ActivateJobHandler.java
@@ -3,9 +3,13 @@ package edu.kmaooad.capstone23.jobs.handlers;
 import edu.kmaooad.capstone23.common.CommandHandler;
 import edu.kmaooad.capstone23.common.ErrorCode;
 import edu.kmaooad.capstone23.common.Result;
+import edu.kmaooad.capstone23.jobs.commands.ActivateJob;
+import edu.kmaooad.capstone23.jobs.commands.CreateJob;
 import edu.kmaooad.capstone23.jobs.commands.DeleteJob;
 import edu.kmaooad.capstone23.jobs.dal.Job;
 import edu.kmaooad.capstone23.jobs.dal.JobRepository;
+import edu.kmaooad.capstone23.jobs.events.JobActivated;
+import edu.kmaooad.capstone23.jobs.events.JobCreated;
 import edu.kmaooad.capstone23.jobs.events.JobDeleted;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
@@ -13,21 +17,21 @@ import jakarta.inject.Inject;
 import java.util.Optional;
 
 @RequestScoped
-public class DeleteJobHandler implements CommandHandler<DeleteJob, JobDeleted> {
+public class ActivateJobHandler implements CommandHandler<ActivateJob, JobActivated> {
 
     @Inject
     private JobRepository repository;
     @Override
-    public Result<JobDeleted> handle(DeleteJob commandDel) {
+    public Result<JobActivated> handle(ActivateJob activateJobCommand) {
 
-        Optional<Job> job = repository.findByIdOptional(commandDel.getJobId());
+        Optional<Job> job = repository.findByIdOptional(activateJobCommand.getId());
         if(job.isEmpty())
-            return new Result<>(ErrorCode.VALIDATION_FAILED, "This job was previously deleted or never existed");
+            return new Result<>(ErrorCode.VALIDATION_FAILED, "This job does not exist");
 
         Job j = job.get();
 
         repository.delete(j);
 
-        return new Result<>(new JobDeleted(commandDel.getJobId()));
+        return new Result<>(new JobActivated(activateJobCommand.getId()));
     }
 }
--- app/src/main/java/edu/kmaooad/capstone23/jobs/handlers/DeactivateJobHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/handlers/DeactivateJobHandler.java
@@ -3,31 +3,32 @@ package edu.kmaooad.capstone23.jobs.handlers;
 import edu.kmaooad.capstone23.common.CommandHandler;
 import edu.kmaooad.capstone23.common.ErrorCode;
 import edu.kmaooad.capstone23.common.Result;
-import edu.kmaooad.capstone23.jobs.commands.DeleteJob;
+import edu.kmaooad.capstone23.jobs.commands.DeactivateJob;
 import edu.kmaooad.capstone23.jobs.dal.Job;
 import edu.kmaooad.capstone23.jobs.dal.JobRepository;
-import edu.kmaooad.capstone23.jobs.events.JobDeleted;
+import edu.kmaooad.capstone23.jobs.events.JobDeactivated;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 
 import java.util.Optional;
 
 @RequestScoped
-public class DeleteJobHandler implements CommandHandler<DeleteJob, JobDeleted> {
+public class DeactivateJobHandler implements CommandHandler<DeactivateJob, JobDeactivated> {
 
     @Inject
     private JobRepository repository;
     @Override
-    public Result<JobDeleted> handle(DeleteJob commandDel) {
+    public Result<JobDeactivated> handle(DeactivateJob deactivateJobCommand) {
 
-        Optional<Job> job = repository.findByIdOptional(commandDel.getJobId());
+        Optional<Job> job = repository.findByIdOptional(deactivateJobCommand.getId());
         if(job.isEmpty())
-            return new Result<>(ErrorCode.VALIDATION_FAILED, "This job was previously deleted or never existed");
+            return new Result<>(ErrorCode.VALIDATION_FAILED, "This job does not exist");
 
         Job j = job.get();
 
         repository.delete(j);
 
-        return new Result<>(new JobDeleted(commandDel.getJobId()));
+        return new Result<>(new JobDeactivated(deactivateJobCommand.getId()));
     }
+
 }
