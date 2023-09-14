f1ae8eb393b954a6cfd323b7c235dfd0a2c9e3f4
miksasha <miksasha2003@gmail.com>
2023-09-14 22:28:02 +0300

    RelateJobToActivitiesHandler check for job

---
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/handlers/RelateJobToActivitiesHandler.java
@@ -0,0 +1,37 @@
+package edu.kmaooad.capstone23.jobs.handlers;
+
+import edu.kmaooad.capstone23.activities.dal.CourseRepository;
+import edu.kmaooad.capstone23.common.CommandHandler;
+import edu.kmaooad.capstone23.common.ErrorCode;
+import edu.kmaooad.capstone23.common.Result;
+import edu.kmaooad.capstone23.jobs.commands.CreateJob;
+import edu.kmaooad.capstone23.jobs.commands.RelateJobToActivities;
+import edu.kmaooad.capstone23.jobs.dal.Job;
+import edu.kmaooad.capstone23.jobs.dal.JobRepository;
+import edu.kmaooad.capstone23.jobs.events.ActivityRelated;
+import edu.kmaooad.capstone23.jobs.events.JobCreated;
+import edu.kmaooad.capstone23.jobs.events.JobDeleted;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+
+import java.util.Optional;
+
+@RequestScoped
+public class RelateJobToActivitiesHandler  implements CommandHandler<RelateJobToActivities, ActivityRelated> {
+
+    @Inject
+    private JobRepository repository;
+//    @Inject
+//    private CourseRepository courseRepository;
+    @Override
+    public Result<ActivityRelated> handle(RelateJobToActivities command) {
+
+        Optional<Job> job = repository.findByIdOptional(command.getJobId());
+        if(job.isEmpty())
+            return new Result<>(ErrorCode.VALIDATION_FAILED, "This job was previously deleted or never existed");
+
+        ActivityRelated result = new ActivityRelated(command.getActivitiesId());
+
+        return new Result<ActivityRelated>(result);
+    }
+}
