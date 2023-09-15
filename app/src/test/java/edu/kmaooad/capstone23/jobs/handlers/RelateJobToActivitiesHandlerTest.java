43bb6258ea51c031d03059607316d6271aa09515
annazhelizniak <anna.zhelizniak08@gmail.com>
2023-09-15 22:21:09 +0300

    create tests for handlers

---
+++ app/src/test/java/edu/kmaooad/capstone23/jobs/handlers/RelateJobToActivitiesHandlerTest.java
@@ -0,0 +1,27 @@
+package edu.kmaooad.capstone23.jobs.handlers;
+
+import edu.kmaooad.capstone23.common.CommandHandler;
+import edu.kmaooad.capstone23.common.Result;
+import edu.kmaooad.capstone23.jobs.commands.CreateJob;
+import edu.kmaooad.capstone23.jobs.commands.DeleteJob;
+import edu.kmaooad.capstone23.jobs.commands.RelateJobToActivities;
+import edu.kmaooad.capstone23.jobs.events.ActivityRelated;
+import edu.kmaooad.capstone23.jobs.events.JobCreated;
+import edu.kmaooad.capstone23.jobs.events.JobDeleted;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class RelateJobToActivitiesHandlerTest {
+    @Inject
+    CommandHandler<CreateJob, JobCreated> handler;
+    @Inject
+    CommandHandler<RelateJobToActivities, ActivityRelated> relateHandler;
+    @Test
+    void testSuccessfulHandling() {
+
+
+    }
+}
---
+++ app/src/test/java/edu/kmaooad/capstone23/jobs/handlers/RelateJobToCompetencesHandlerTest.java
@@ -0,0 +1,7 @@
+package edu.kmaooad.capstone23.jobs.handlers;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class RelateJobToCompetencesHandlerTest {
+}
