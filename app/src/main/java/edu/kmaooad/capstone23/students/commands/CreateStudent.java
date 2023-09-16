76e8f0ec8749f0abfc5ebe458f9c7accc8bce9ce
Ivan Dobrovolskyi <ivan.dobrovolskyi@ukma.edu.ua>
2023-09-16 11:58:56 -0400

    optimize imports

--- app/src/main/java/edu/kmaooad/capstone23/students/commands/CreateStudent.java
+++ app/src/main/java/edu/kmaooad/capstone23/students/commands/CreateStudent.java
@@ -1,8 +1,5 @@
 package edu.kmaooad.capstone23.students.commands;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import org.jboss.resteasy.reactive.RestForm;
 
 import java.io.File;
