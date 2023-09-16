90a91f94200f1371ca9c45fe3b30538c531b2fad
Iryna Melnyk <iryna2.melnyk@ukma.edu.ua>
2023-09-15 20:15:30 -0400

    change CreateStudent to contain File field

--- app/src/main/java/edu/kmaooad/capstone23/students/commands/CreateStudent.java
+++ app/src/main/java/edu/kmaooad/capstone23/students/commands/CreateStudent.java
@@ -3,64 +3,13 @@ package edu.kmaooad.capstone23.students.commands;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import org.jboss.resteasy.reactive.RestForm;
+
+import java.io.File;
 
 public class CreateStudent {
 
-    @NotBlank
-    @Pattern(regexp = "[a-zA-Z]+")
-    private String firstName;
+    @RestForm
+    public File content;
 
-    @NotBlank
-    @Pattern(regexp = "[a-zA-Z]*")
-    private String middleName;
-
-    @NotBlank
-    @Pattern(regexp = "[a-zA-Z]+")
-    private String lastName;
-
-    private long DOBTimestamp;
-
-    @NotBlank
-    @Email
-    private String email;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public long getDOBTimestamp() {
-        return DOBTimestamp;
-    }
-
-    public void setDOBTimestamp(long DOBTimestamp) {
-        this.DOBTimestamp = DOBTimestamp;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
