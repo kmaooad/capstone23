5091b69518f7e504422d2495d98df374e002cfb6
Ivan Dobrovolskyi <ivan.dobrovolskyi@ukma.edu.ua>
2023-09-16 12:02:18 -0400

    add logic to StudentRepository

--- app/src/main/java/edu/kmaooad/capstone23/students/dal/StudentRepository.java
+++ app/src/main/java/edu/kmaooad/capstone23/students/dal/StudentRepository.java
@@ -1,8 +1,30 @@
 package edu.kmaooad.capstone23.students.dal;
 
+import edu.kmaooad.capstone23.students.parser.CSVStudent;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @ApplicationScoped
 public class StudentRepository implements PanacheMongoRepository<Student> {
+    public List<Student> insert(List<CSVStudent> students){
+        List<Student> result = new ArrayList<>();
+        for (CSVStudent student : students) {
+            result.add(map(student));
+        }
+        persist(result);
+        return result;
+    }
+
+    private Student map(CSVStudent student){
+        Student result = new Student();
+        result.firstName = student.getFirstName();
+        result.middleName = student.getMiddleName();
+        result.lastName = student.getLastName();
+        result.DOBTimestamp = student.getDOBTimestamp();
+        result.email = student.getEmail();
+        return result;
+    }
 }
