2428030cf7df946ea70ee4a59f2d0c0c9eeaeca1
Bohdan Arkhypchuk <arkhypchuk.b@gmail.com>
2023-09-16 22:26:15 +0200

    fix of error

--- app/src/main/java/edu/kmaooad/capstone23/experts/controllers/RemoveExpertFromMemberController.java
+++ app/src/main/java/edu/kmaooad/capstone23/experts/controllers/RemoveExpertFromMemberController.java
@@ -1,8 +1,7 @@
 package edu.kmaooad.capstone23.experts.controllers;
 
 import edu.kmaooad.capstone23.common.TypicalController;
-import edu.kmaooad.capstone23.experts.commands.DeleteExpert;
-import edu.kmaooad.capstone23.experts.events.ExpertDeleted;
+import edu.kmaooad.capstone23.experts.commands.RemoveExpertFromMember;
 import edu.kmaooad.capstone23.experts.events.ExpertRemovedFromMember;
 import jakarta.ws.rs.Path;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
@@ -12,6 +11,5 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 @Path("/experts/member/delete")
 @APIResponse(responseCode = "200", content = {
         @Content(mediaType = "application/json", schema = @Schema(implementation = ExpertRemovedFromMember.class)) })
-public class RemoveExpertFromMemberController
-        extends TypicalController<RemoveExpertFromMemberController, ExpertRemovedFromMember> {
+public class RemoveExpertFromMemberController extends TypicalController<RemoveExpertFromMember, ExpertRemovedFromMember> {
 }
