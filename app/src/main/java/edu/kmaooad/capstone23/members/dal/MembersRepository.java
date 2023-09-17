3200997c7ca2769e299701223ae4475f2bf1fe4a
Liubomyr <liubomyr.stepanenko@ukma.edu.ua>
2023-09-17 22:45:53 +0200

    Unused methods commented

--- app/src/main/java/edu/kmaooad/capstone23/members/dal/MembersRepository.java
+++ app/src/main/java/edu/kmaooad/capstone23/members/dal/MembersRepository.java
@@ -1,6 +1,5 @@
 package edu.kmaooad.capstone23.members.dal;
 
-import edu.kmaooad.capstone23.experts.dal.Expert;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.bson.types.ObjectId;
@@ -8,13 +7,14 @@ import org.bson.types.ObjectId;
 @ApplicationScoped
 public class MembersRepository implements PanacheMongoRepository<Member> {
 
-    public Member getById(ObjectId id) {
-        return find("id", id).firstResult();
-    }
-
-    public Member getByEmail(String email) {
-        return find("email", email).firstResult();
-    }
+    // NOT USED YET
+//    public Member getById(ObjectId id) {
+//        return find("id", id).firstResult();
+//    }
+//
+//    public Member getByEmail(String email) {
+//        return find("email", email).firstResult();
+//    }
 
     public Member insert(Member member) {
         persist(member);
