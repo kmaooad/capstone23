7c0dbdf5b64a6f05422512d6737d662833a40992
ElizabethChernova <elizabethchernova@gmail.com>
2023-09-17 20:47:58 +0300

    unit tests
    unit tests for hide cv, show cv, set status and set job preference

---
+++ app/src/test/java/edu/kmaooad/capstone23/cvs/controllers/ShowCVControllerTest.java
@@ -0,0 +1,60 @@
+
+import edu.kmaooad.capstone23.cvs.dal.CV;
+import edu.kmaooad.capstone23.cvs.dal.CVRepository;
+import edu.kmaooad.capstone23.cvs.dal.JobPreference;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+public class ShowCVControllerTest {
+
+    @Inject
+    CVRepository cvRepository;
+
+    @Test
+    @DisplayName("show cv: valid values")
+    public void updateJobPref_AndGetStatus200(){
+        CV cv = provideCVWithPref();
+
+        Map<String, Object> pref = new HashMap<>();
+        pref.put("cvId", cv.id.toString());
+
+        given().contentType("application/json")
+                .body(pref)
+                .when()
+                .post("/cvs/show")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("show cv: invalid values")
+    public void updateInvalidJobPref_AndGetError(){
+        Map<String, Object> prefNoId = new HashMap<>();
+
+        given().contentType("application/json")
+                .body(prefNoId)
+                .when()
+                .post("/cvs/show")
+                .then()
+                .statusCode(400);
+    }
+
+    private CV provideCVWithPref() {
+        CV cv = new CV();
+        cv.dateTimeCreated = LocalDateTime.now();
+        cv.status = edu.kmaooad.capstone23.cvs.dal.CV.Status.OPEN;
+        cv.visibility = edu.kmaooad.capstone23.cvs.dal.CV.Visibility.VISIBLE;
+        cv.preference = new JobPreference("Kyiv", "IT", JobPreference.Category.FULL_TIME);
+        cvRepository.persist(cv);
+        return cv;
+    }
+}
