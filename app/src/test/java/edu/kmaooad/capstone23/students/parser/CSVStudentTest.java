75be804d9465cddc3eb40226d54bfe428e492c70
Ivan Dobrovolskyi <ivan.dobrovolskyi@ukma.edu.ua>
2023-09-17 19:15:10 -0400

    add more tests to CSVStudent

--- app/src/test/java/edu/kmaooad/capstone23/students/parser/CSVStudentTest.java
+++ app/src/test/java/edu/kmaooad/capstone23/students/parser/CSVStudentTest.java
@@ -8,16 +8,17 @@ import org.junit.jupiter.api.Test;
 @QuarkusTest
 public class CSVStudentTest {
 
+    private static final CSVStudent STUDENT = new CSVStudent(
+            "Ivan",
+            "",
+            "Dobrovolskyi",
+            1039132800000L,
+            "ivan.dobrovolskyi@ukma.edu.ua"
+    );
+
     @Test
     @DisplayName("Test CSVStudent equals: Success")
     public void testCSVStudentsEqualsSuccess() {
-        CSVStudent expected = new CSVStudent(
-                "Ivan",
-                "",
-                "Dobrovolskyi",
-                1039132800000L,
-                "ivan.dobrovolskyi@ukma.edu.ua"
-        );
         CSVStudent result = new CSVStudent(
                 "Ivan",
                 "",
@@ -25,20 +26,35 @@ public class CSVStudentTest {
                 1039132800000L,
                 "ivan.dobrovolskyi@ukma.edu.ua"
         );
-        Assertions.assertEquals(expected.hashCode(), result.hashCode());
-        Assertions.assertEquals(expected,result);
+        Assertions.assertEquals(STUDENT.hashCode(), result.hashCode());
+        Assertions.assertEquals(STUDENT,result);
     }
 
     @Test
-    @DisplayName("Test CSVStudent equals: Fail")
-    public void testCSVStudentsEqualsFail() {
-        CSVStudent expected = new CSVStudent(
-                "Ivan",
+    @DisplayName("Test CSVStudent equals: Success same object")
+    public void testCSVStudentsEqualsSuccessSameObject() {
+        CSVStudent result = STUDENT;
+        Assertions.assertEquals(STUDENT.hashCode(), result.hashCode());
+        Assertions.assertEquals(STUDENT,result);
+    }
+
+    @Test
+    @DisplayName("Test CSVStudent not equals: Different first name")
+    public void testCSVStudentsEqualsFailFirstName() {
+        CSVStudent result = new CSVStudent(
+                "Ivan2",
                 "",
                 "Dobrovolskyi",
                 1039132800000L,
                 "ivan.dobrovolskyi@ukma.edu.ua"
         );
+        Assertions.assertNotEquals(STUDENT.hashCode(), result.hashCode());
+        Assertions.assertNotEquals(STUDENT,result);
+    }
+
+    @Test
+    @DisplayName("Test CSVStudent not equals: Different middle name")
+    public void testCSVStudentsEqualsFailMiddleName() {
         CSVStudent result = new CSVStudent(
                 "Ivan",
                 "Oleksandrovych",
@@ -46,7 +62,63 @@ public class CSVStudentTest {
                 1039132800000L,
                 "ivan.dobrovolskyi@ukma.edu.ua"
         );
-        Assertions.assertNotEquals(expected.hashCode(), result.hashCode());
-        Assertions.assertNotEquals(expected,result);
+        Assertions.assertNotEquals(STUDENT.hashCode(), result.hashCode());
+        Assertions.assertNotEquals(STUDENT,result);
+    }
+
+    @Test
+    @DisplayName("Test CSVStudent not equals: Different last name")
+    public void testCSVStudentsEqualsFailLastName() {
+        CSVStudent result = new CSVStudent(
+                "Ivan",
+                "",
+                "Dobrovolsky",
+                1039132800000L,
+                "ivan.dobrovolskyi@ukma.edu.ua"
+        );
+        Assertions.assertNotEquals(STUDENT.hashCode(), result.hashCode());
+        Assertions.assertNotEquals(STUDENT,result);
+    }
+
+    @Test
+    @DisplayName("Test CSVStudent not equals: Different date of birth")
+    public void testCSVStudentsEqualsFailDOB() {
+        CSVStudent result = new CSVStudent(
+                "Ivan",
+                "",
+                "Dobrovolskyi",
+                1039142800000L,
+                "ivan.dobrovolskyi@ukma.edu.ua"
+        );
+        Assertions.assertNotEquals(STUDENT.hashCode(), result.hashCode());
+        Assertions.assertNotEquals(STUDENT,result);
+    }
+
+    @Test
+    @DisplayName("Test CSVStudent not equals: Different email")
+    public void testCSVStudentsEqualsFailEmail() {
+        CSVStudent result = new CSVStudent(
+                "Ivan",
+                "",
+                "Dobrovolskyi",
+                1039142800000L,
+                "iryan2.melnyk@ukma.edu.ua"
+        );
+        Assertions.assertNotEquals(STUDENT.hashCode(), result.hashCode());
+        Assertions.assertNotEquals(STUDENT,result);
+    }
+
+    @Test
+    @DisplayName("Test CSVStudent not equals: Null")
+    public void testCSVStudentsEqualsFailNull() {
+        CSVStudent result = null;
+        Assertions.assertNotEquals(STUDENT, result);
+    }
+
+    @Test
+    @DisplayName("Test CSVStudent not equals: Different object")
+    public void testCSVStudentsEqualsFailObjectType() {
+        Integer result = 1024;
+        Assertions.assertNotEquals(STUDENT, result);
     }
 }
