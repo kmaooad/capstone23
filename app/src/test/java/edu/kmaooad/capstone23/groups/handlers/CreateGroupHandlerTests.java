c276c04f2d9d425e7c94b119bce45b36dbb86d53
Нікіта Боровік <nikita.borovik@ukma.edu.ua>
2023-09-17 23:45:15 +0300

    +name validation test in CreateGroupHandlerTests +template for group test in CreateGroupHandlerTests

--- app/src/test/java/edu/kmaooad/capstone23/groups/handlers/CreateGroupHandlerTests.java
+++ app/src/test/java/edu/kmaooad/capstone23/groups/handlers/CreateGroupHandlerTests.java
@@ -35,4 +35,25 @@ public class CreateGroupHandlerTests {
         Assertions.assertNotNull(resultForGroup.getValue());
         Assertions.assertFalse(resultForGroup.getValue().getGroupId().isEmpty());
     }
+    @Test
+    @DisplayName("Create Group Template: Name Validation")
+    public void testNameValidation() {
+        CreateGroupTemplate templateCommand = new CreateGroupTemplate();
+        templateCommand.setGroupTemplateName("template");
+        Result<GroupTemplateCreated> resultForTemplate = templateHandler.handle(templateCommand);
+
+        CreateGroup groupCommand = new CreateGroup("", resultForTemplate.getValue().getGroupTemplateId().toString());
+        Result<GroupCreated> resultForGroup = groupHandler.handle(groupCommand);
+
+        Assertions.assertFalse(resultForGroup.isSuccess());
+    }
+    @Test
+    @DisplayName("Create Group Template: Template Validation")
+    public void testTemplateForGroupValidation() {
+
+        CreateGroup groupCommand = new CreateGroup("group", "123abcdef");
+        Result<GroupCreated> resultForGroup = groupHandler.handle(groupCommand);
+
+        Assertions.assertFalse(resultForGroup.isSuccess());
+    }
 }
