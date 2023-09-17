402e3115fb88aa148c49609ab3028cdfdd77e21c
Нікіта Боровік <nikita.borovik@ukma.edu.ua>
2023-09-17 23:55:09 +0300

    +UpdateGroupHandlerTests empty class +UpdateGroupControllerTests empty class +basic handling test in UpdateGroupHandlerTests

---
+++ app/src/test/java/edu/kmaooad/capstone23/groups/controllers/UpdateGroupControllerTests.java
@@ -0,0 +1,12 @@
+package edu.kmaooad.capstone23.groups.controllers;
+
+import edu.kmaooad.capstone23.group_templates.dal.GroupTemplatesRepository;
+import edu.kmaooad.capstone23.group_templates.handlers.UpdateGroupTemplateHandler;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+
+@QuarkusTest
+public class UpdateGroupControllerTests {
+
+
+}
---
+++ app/src/test/java/edu/kmaooad/capstone23/groups/handlers/UpdateGroupHandlerTests.java
@@ -0,0 +1,51 @@
+package edu.kmaooad.capstone23.groups.handlers;
+
+import edu.kmaooad.capstone23.common.Result;
+import edu.kmaooad.capstone23.group_templates.commands.UpdateGroupTemplate;
+import edu.kmaooad.capstone23.group_templates.dal.GroupTemplate;
+import edu.kmaooad.capstone23.group_templates.dal.GroupTemplatesRepository;
+import edu.kmaooad.capstone23.group_templates.events.GroupTemplateUpdated;
+import edu.kmaooad.capstone23.group_templates.handlers.UpdateGroupTemplateHandler;
+import edu.kmaooad.capstone23.groups.commands.UpdateGroup;
+import edu.kmaooad.capstone23.groups.dal.Group;
+import edu.kmaooad.capstone23.groups.dal.GroupsRepository;
+import edu.kmaooad.capstone23.groups.events.GroupUpdated;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class UpdateGroupHandlerTests {
+
+    @Inject
+    GroupTemplatesRepository groupTemplatesRepository;
+
+    @Inject
+    GroupsRepository groupsRepository;
+    @Inject
+    UpdateGroupHandler updateGroupHandler;
+    @Test
+    @DisplayName("Update Group : Basic")
+    public void testSuccessfulHandling() {
+        GroupTemplate groupTemplate = new GroupTemplate();
+        groupTemplate.name = "GroupTemplate";
+        groupTemplatesRepository.insert(groupTemplate);
+
+        Group group = new Group();
+        group.name = "test_group";
+        group.templateId = groupTemplate.id.toString();
+
+        UpdateGroup command = new UpdateGroup();
+        command.setId(groupTemplate.id.toString());
+        command.setGroupName("new_name");
+
+        Result<GroupUpdated> result = updateGroupHandler.handle(command);
+
+        Assertions.assertTrue(result.isSuccess());
+        Assertions.assertNotNull(result.getValue());
+        Assertions.assertFalse(result.getValue().getId().isEmpty());
+        Assertions.assertFalse(result.getValue().getName().isEmpty());
+    }
+}
