e068635da58019067d1454fa2fc7c916a06b6357
Нікіта Боровік <nikita.borovik@ukma.edu.ua>
2023-09-17 23:26:26 +0300

    + CreateGroupHandlerTests class + basic group creation test in CreateGroupHandlerTests

--- app/src/test/java/edu/kmaooad/capstone23/groups/controllers/CreateGroupControllerTests.java
+++ app/src/test/java/edu/kmaooad/capstone23/groups/controllers/CreateGroupControllerTests.java
@@ -38,4 +38,5 @@ public class CreateGroupControllerTests {
                 .then()
                 .statusCode(200);
     }
+
 }
---
+++ app/src/test/java/edu/kmaooad/capstone23/groups/handlers/CreateGroupHandlerTests.java
@@ -0,0 +1,38 @@
+package edu.kmaooad.capstone23.groups.handlers;
+
+import edu.kmaooad.capstone23.common.CommandHandler;
+import edu.kmaooad.capstone23.common.Result;
+import edu.kmaooad.capstone23.group_templates.commands.CreateGroupTemplate;
+import edu.kmaooad.capstone23.group_templates.events.GroupTemplateCreated;
+import edu.kmaooad.capstone23.groups.commands.CreateGroup;
+import edu.kmaooad.capstone23.groups.events.GroupCreated;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class CreateGroupHandlerTests {
+
+    @Inject
+    CommandHandler<CreateGroupTemplate, GroupTemplateCreated> templateHandler;
+
+    @Inject
+    CommandHandler<CreateGroup, GroupCreated> groupHandler;
+
+    @Test
+    @DisplayName("Create Group Template: Basic")
+    public void testSuccessfulHandling() {
+        CreateGroupTemplate templateCommand = new CreateGroupTemplate();
+        templateCommand.setGroupTemplateName("template");
+        Result<GroupTemplateCreated> resultForTemplate = templateHandler.handle(templateCommand);
+
+        CreateGroup groupCommand = new CreateGroup("group", resultForTemplate.getValue().getGroupTemplateId().toString());
+        Result<GroupCreated> resultForGroup = groupHandler.handle(groupCommand);
+
+        Assertions.assertTrue(resultForGroup.isSuccess());
+        Assertions.assertNotNull(resultForGroup.getValue());
+        Assertions.assertFalse(resultForGroup.getValue().getGroupId().isEmpty());
+    }
+}
