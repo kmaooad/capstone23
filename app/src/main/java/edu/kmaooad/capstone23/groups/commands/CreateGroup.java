ba75529c2e30340a3c5ea15ec892ee7756f181a9
IvanBlyzniuk <84847629+IvanBlyzniuk@users.noreply.github.com>
2023-09-17 21:44:36 +0300

    + CreateGroup command

---
+++ app/src/main/java/edu/kmaooad/capstone23/groups/commands/CreateGroup.java
@@ -0,0 +1,11 @@
+package edu.kmaooad.capstone23.groups.commands;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class CreateGroup {
+    @NotBlank
+    @Size(min = 1, max = 50)
+    public String groupName;
+    public String templateId;
+}
