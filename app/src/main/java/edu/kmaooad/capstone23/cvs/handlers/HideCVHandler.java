f15a1f4d5d89fb7649518dd087afa82915726172
ElizabethChernova <elizabethchernova@gmail.com>
2023-09-17 22:32:02 +0300

    Update HideCVHandler.java

--- app/src/main/java/edu/kmaooad/capstone23/cvs/handlers/HideCVHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/cvs/handlers/HideCVHandler.java
@@ -4,12 +4,13 @@ import edu.kmaooad.capstone23.common.CommandHandler;
 import edu.kmaooad.capstone23.common.Result;
 import edu.kmaooad.capstone23.cvs.commands.HideCV;
 import edu.kmaooad.capstone23.cvs.dal.CV;
+
+import edu.kmaooad.capstone23.cvs.dal.CV.Visibility;
 import edu.kmaooad.capstone23.cvs.dal.CVRepository;
 import edu.kmaooad.capstone23.cvs.events.CVHided;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 
-import edu.kmaooad.capstone23.cvs.dal.CV.Visibility;
 import org.bson.types.ObjectId;
 
 @RequestScoped
