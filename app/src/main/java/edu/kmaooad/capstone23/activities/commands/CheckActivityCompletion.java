f47451d79868aee8f36304763c4fcf210357d25b
MityA32 <hetmandmitriy@gmail.com>
2023-09-18 23:34:27 +0300

    implement CheckActivityCompletion

--- app/src/main/java/edu/kmaooad/capstone23/activities/commands/CheckActivityCompletion.java
+++ app/src/main/java/edu/kmaooad/capstone23/activities/commands/CheckActivityCompletion.java
@@ -1,5 +1,54 @@
 package edu.kmaooad.capstone23.activities.commands;
 
+import java.util.Date;
+
+import java.util.Date;
+
 public class CheckActivityCompletion {
-    
+    private String name;
+    private Date startDate;
+    private Date finishDate;
+    private boolean inProgress;
+    private boolean completed;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getFinishDate() {
+        return finishDate;
+    }
+
+    public void setFinishDate(Date finishDate) {
+        this.finishDate = finishDate;
+    }
+
+    public boolean isInProgress() {
+        return inProgress;
+    }
+
+    public void setInProgress(boolean inProgress) {
+        this.inProgress = inProgress;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
+
