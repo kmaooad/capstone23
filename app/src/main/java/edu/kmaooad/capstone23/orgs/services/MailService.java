78ce0ffb8ae47bc833e3f2b4cddaccc26562ba3b
Yehor Bilokin <91533724+Iwaway@users.noreply.github.com>
2023-09-18 12:44:58 +0300

    Create mail service for notifying orgs by email

---
+++ app/src/main/java/edu/kmaooad/capstone23/orgs/services/MailService.java
@@ -0,0 +1,12 @@
+package edu.kmaooad.capstone23.orgs.services;
+
+public class MailService {
+
+    MailService(){
+        
+    }
+
+    public void sendEmail(String emailText, String orgEmail) {
+
+    }
+}
