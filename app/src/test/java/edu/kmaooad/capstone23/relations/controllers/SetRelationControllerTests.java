cc7a5e0ace937a53f0f167b9fb4125074ac433f0
Annnanke <kirikanna3005@gmail.com>
2023-09-18 08:15:45 -0400

    SetRelationControllerTests fixed

--- app/src/test/java/edu/kmaooad/capstone23/relations/controllers/SetRelationControllerTests.java
+++ app/src/test/java/edu/kmaooad/capstone23/relations/controllers/SetRelationControllerTests.java
@@ -16,8 +16,8 @@ public class SetRelationControllerTests {
     @DisplayName("Basic Relation Setting")
     public void testBasicRelationSetting() {
         Map<String, Object> jsonAsMap = new HashMap<>();
-        jsonAsMap.put("collectionName1", "CollectionA");
-        jsonAsMap.put("collectionName2", "CollectionB");
+        jsonAsMap.put("collectionName1", "courses");
+        jsonAsMap.put("collectionName2", "projects");
         jsonAsMap.put("objectToConnectId1", "5f7e47fc8e1f7112d73c92a1");
         jsonAsMap.put("objectToConnectId2", "1a4cd132b123a1aa3bc2d142");
 
@@ -37,7 +37,7 @@ public class SetRelationControllerTests {
         jsonAsMap.put("collectionName1", "CollectionA");
         // Missing part
         jsonAsMap.put("objectToConnectId1", "5f7e47fc8e1f7112d73c92a1");
-        // Missing opart
+        // Missing part
 
         given()
                 .contentType("application/json")
