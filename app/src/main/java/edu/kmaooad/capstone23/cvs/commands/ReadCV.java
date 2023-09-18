a3a89528e0071f225fcad92bb5c80d306b0e09b3
Anastasia Zolotar <anazolotar28@gmail.com>
2023-09-18 18:32:32 +0300

    add job preference filter to cv

--- app/src/main/java/edu/kmaooad/capstone23/cvs/commands/ReadCV.java
+++ app/src/main/java/edu/kmaooad/capstone23/cvs/commands/ReadCV.java
@@ -1,10 +1,17 @@
 package edu.kmaooad.capstone23.cvs.commands;
 
 import edu.kmaooad.capstone23.cvs.dal.CV;
+import edu.kmaooad.capstone23.cvs.dal.JobPreference;
 
 public class ReadCV {
 
     private CV.Status status;
+
+    private String industry;
+
+    private String location;
+
+    private JobPreference.Category category;
 
     public CV.Status getStatus() {
         return status;
@@ -13,4 +20,31 @@ public class ReadCV {
     public void setStatus(CV.Status status) {
         this.status = status;
     }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public JobPreference.Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(JobPreference.Category category) {
+        this.category = category;
+    }
+
+
+
 }
--- app/src/main/java/edu/kmaooad/capstone23/cvs/handlers/ReadCVHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/cvs/handlers/ReadCVHandler.java
@@ -3,10 +3,13 @@ package edu.kmaooad.capstone23.cvs.handlers;
 import edu.kmaooad.capstone23.common.CommandHandler;
 import edu.kmaooad.capstone23.common.Result;
 import edu.kmaooad.capstone23.cvs.commands.ReadCV;
+import edu.kmaooad.capstone23.cvs.dal.CV;
 import edu.kmaooad.capstone23.cvs.dal.CVRepository;
 import edu.kmaooad.capstone23.cvs.events.CVRead;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
+
+import java.util.List;
 
 
 @RequestScoped
@@ -17,11 +20,27 @@ public class ReadCVHandler implements CommandHandler<ReadCV, CVRead> {
 
     @Override
     public Result<CVRead> handle(ReadCV command) {
-        CVRead res;
+        List<CV> cvs = cvRepository.find("visibility", "VISIBLE").stream().toList();
         if (command.getStatus() != null)
-            res = new CVRead(cvRepository.find("status", command.getStatus()).stream().toList());
-        else
-            res = new CVRead(cvRepository.findAll().stream().toList());
+            cvs = cvs.stream().filter(x -> x.status.equals(command.getStatus())).toList();
+
+        if (command.getIndustry() != null)
+            cvs = cvs.stream()
+                    .filter(x -> x.preference != null && x.preference.industry.equals(command.getIndustry()))
+                    .toList();
+
+        if (command.getLocation() != null)
+            cvs = cvs.stream()
+                    .filter(x -> x.preference != null && x.preference.location.equals(command.getLocation()))
+                    .toList();
+
+        if (command.getCategory() != null)
+            cvs = cvs.stream()
+                    .filter(x -> x.preference != null && x.preference.category.equals(command.getCategory()))
+                    .toList();
+
+        CVRead res = new CVRead(cvs);
         return new Result<>(res);
     }
+
 }
