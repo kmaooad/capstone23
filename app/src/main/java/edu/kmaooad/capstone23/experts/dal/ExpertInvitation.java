efbf9ae3618db53d1d3b89fdd346bbb01330cfaf
Oleh Klepatskyi <84376952+o-klepatskyi@users.noreply.github.com>
2023-09-19 08:16:24 +0300

    Accept expert invitation & delete topic (#60)
    * delete topic, move tests
    
    * Add orgName and expertName
    
    * Validate org name in invitation link request
    
    * Add accept invitation controller
    
    * Add handler for accepting invitation link
    
    * Add tests for handlers
    
    * Fix dependencies
    
    * Optimize imports
    
    * Create accept expert invitation test
    
    * Fix tests
    
    * Fix annotation in controller
    
    ---------
    
    Co-authored-by: Oleg Zagryvyi <olegzagraf@gmail.com>
    Co-authored-by: Illia Shevchyk <illia.shevchyk@ukma.edu.ua>

--- app/src/main/java/edu/kmaooad/capstone23/experts/dal/ExpertInvitation.java
+++ app/src/main/java/edu/kmaooad/capstone23/experts/dal/ExpertInvitation.java
@@ -1,6 +1,7 @@
 package edu.kmaooad.capstone23.experts.dal;
 
 import edu.kmaooad.capstone23.experts.ExpertType;
+import edu.kmaooad.capstone23.orgs.dal.Org;
 import io.quarkus.mongodb.panache.common.MongoEntity;
 import io.quarkus.mongodb.panache.PanacheMongoEntity;
 
@@ -11,5 +12,7 @@ public class ExpertInvitation extends PanacheMongoEntity {
     public String email;
     public ExpertType expertType;
     public LocalDateTime createdAt;
+    public String expertName;
+    public Org org;
     public boolean accepted;
 }
--- app/src/main/java/edu/kmaooad/capstone23/experts/handlers/CreateInvitationLinkHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/experts/handlers/CreateInvitationLinkHandler.java
@@ -8,8 +8,11 @@ import edu.kmaooad.capstone23.experts.dal.ExpertInvitationRepository;
 import edu.kmaooad.capstone23.experts.events.InvitationLinkCreated;
 import edu.kmaooad.capstone23.experts.service.ExpertInvitationMailService;
 import edu.kmaooad.capstone23.experts.service.ExpertService;
+import edu.kmaooad.capstone23.orgs.dal.OrgsRepository;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
+
+import static edu.kmaooad.capstone23.common.ErrorCode.VALIDATION_FAILED;
 
 @RequestScoped
 public class CreateInvitationLinkHandler implements CommandHandler<CreateInvitationLink, InvitationLinkCreated> {
@@ -20,12 +23,21 @@ public class CreateInvitationLinkHandler implements CommandHandler<CreateInvitat
     ExpertService expertService;
     @Inject
     ExpertInvitationMailService mailService;
+    @Inject
+    OrgsRepository orgsRepository;
 
     @Override
     public Result<InvitationLinkCreated> handle(CreateInvitationLink command) {
         var expertInvitation = new ExpertInvitation();
         expertInvitation.email = command.getEmail();
         expertInvitation.expertType = command.getExpertType();
+        expertInvitation.org = orgsRepository.findByName(command.getOrgName());
+        expertInvitation.expertName = command.getExpertName();
+
+        if (expertInvitation.org == null) {
+            return new Result<>(VALIDATION_FAILED, "Wrong organisation name");
+        }
+
         repository.persist(expertInvitation);
         var invitationLink = expertService.createInvitationLink(expertInvitation.id);
         mailService.sendInvitationLink(expertInvitation.email, invitationLink);
