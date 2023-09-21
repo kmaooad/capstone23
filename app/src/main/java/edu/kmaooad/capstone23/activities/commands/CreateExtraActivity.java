8cc9a08d91f9b0dd72169453d33954103a16cf02
ElizabethChernova <elizabethchernova@gmail.com>
2023-09-21 16:11:59 +0300

    add features

--- app/src/main/java/edu/kmaooad/capstone23/activities/commands/CreateExtraActivity.java
+++ app/src/main/java/edu/kmaooad/capstone23/activities/commands/CreateExtraActivity.java
@@ -1,25 +1,25 @@
 package edu.kmaooad.capstone23.activities.commands;
 import org.bson.types.ObjectId;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public class CreateExtraActivity {
+    @NotNull
+    private String id;
+    @NotBlank
+    @Size(max = 100)
     private String name;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String getId() { return id; }
 
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    private ObjectId id;
-    public ObjectId getId() {
-        return id;
-    }
+    public void setId(String id) { this.id = id; }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
