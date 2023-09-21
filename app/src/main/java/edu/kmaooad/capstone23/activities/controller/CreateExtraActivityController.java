797c4571a009f8fa201ce93b3defad115eca2cdc
ElizabethChernova <elizabethchernova@gmail.com>
2023-09-21 14:25:10 +0300

    Update CreateExtraActivityController.java

--- app/src/main/java/edu/kmaooad/capstone23/activities/controller/CreateExtraActivityController.java
+++ app/src/main/java/edu/kmaooad/capstone23/activities/controller/CreateExtraActivityController.java
@@ -7,9 +7,9 @@ import jakarta.ws.rs.Path;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-@Path("/extraActivities/create")
-@APIResponse(responseCode = "200", content = {
-        @Content(mediaType = "application/json", schema = @Schema(implementation = ExtraActivityCreated.class)) })
-public class CreateExtraActivityController extends TypicalController<CreateExtraActivity, ExtraActivityCreated> {
-
-}
+//@Path("/extraActivities/create")
+//@APIResponse(responseCode = "200", content = {
+//        @Content(mediaType = "application/json", schema = @Schema(implementation = ExtraActivityCreated.class)) })
+//public class CreateExtraActivityController extends TypicalController<CreateExtraActivity, ExtraActivityCreated> {
+//
+//}
