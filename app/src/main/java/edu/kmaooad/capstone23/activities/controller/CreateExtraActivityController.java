1fb3a7c3802d0bc2fd576948466fd6a39abb17f4
ElizabethChernova <elizabethchernova@gmail.com>
2023-09-21 14:09:44 +0300

    Update CreateExtraActivityController.java

--- app/src/main/java/edu/kmaooad/capstone23/activities/controller/CreateExtraActivityController.java
+++ app/src/main/java/edu/kmaooad/capstone23/activities/controller/CreateExtraActivityController.java
@@ -3,10 +3,10 @@ import edu.kmaooad.capstone23.activities.commands.CreateExtraActivity;
 //import edu.kmaooad.capstone23.activities.commands.CreateExtraActivity;
 import edu.kmaooad.capstone23.activities.events.ExtraActivityCreated;
 import edu.kmaooad.capstone23.common.TypicalController;
-//import jakarta.ws.rs.Path;
-//import org.eclipse.microprofile.openapi.annotations.media.Content;
-//import org.eclipse.microprofile.openapi.annotations.media.Schema;
-//import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import jakarta.ws.rs.Path;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
 @Path("/activities/extra/create")
 @APIResponse(responseCode = "200", content = {
