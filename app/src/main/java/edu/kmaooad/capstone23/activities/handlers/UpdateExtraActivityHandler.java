86bc0301da7e27d19de8ed57cef3fc00a8ed8fc7
ElizabethChernova <elizabethchernova@gmail.com>
2023-09-21 18:22:07 +0300

    Update UpdateExtraActivityHandler.java

--- app/src/main/java/edu/kmaooad/capstone23/activities/handlers/UpdateExtraActivityHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/activities/handlers/UpdateExtraActivityHandler.java
@@ -1,28 +1,28 @@
-package edu.kmaooad.capstone23.activities.handlers;
-import edu.kmaooad.capstone23.activities.dal.ExtraActivity;
-import edu.kmaooad.capstone23.activities.commands.UpdateExtraActivity;
-import edu.kmaooad.capstone23.activities.dal.ActivityRepository;
-import edu.kmaooad.capstone23.activities.events.ExtraActivityUpdated;
-import edu.kmaooad.capstone23.common.CommandHandler;
-import edu.kmaooad.capstone23.common.Result;
-import jakarta.enterprise.context.RequestScoped;
-import jakarta.inject.Inject;
-import org.bson.types.ObjectId;
-@RequestScoped
-public class UpdateExtraActivityHandler implements CommandHandler<UpdateExtraActivity, ExtraActivityUpdated> {
-
-
-    @Inject
-    ActivityRepository cvRepository;
-    @Override
-    public Result<ExtraActivityUpdated> handle(UpdateExtraActivity command) {
-        ExtraActivity act = cvRepository.findById(new ObjectId(command.getId()));
-
-        if (command.getName() != null)
-            act.name = command.getName();
-
-
-        var result = new ExtraActivityUpdated(act.getId());
-        return new Result<>(result);
-    }
-}
+//package edu.kmaooad.capstone23.activities.handlers;
+//import edu.kmaooad.capstone23.activities.dal.ExtraActivity;
+//import edu.kmaooad.capstone23.activities.commands.UpdateExtraActivity;
+//import edu.kmaooad.capstone23.activities.dal.ActivityRepository;
+//import edu.kmaooad.capstone23.activities.events.ExtraActivityUpdated;
+//import edu.kmaooad.capstone23.common.CommandHandler;
+//import edu.kmaooad.capstone23.common.Result;
+//import jakarta.enterprise.context.RequestScoped;
+//import jakarta.inject.Inject;
+//import org.bson.types.ObjectId;
+//@RequestScoped
+//public class UpdateExtraActivityHandler implements CommandHandler<UpdateExtraActivity, ExtraActivityUpdated> {
+//
+//
+//    @Inject
+//    ActivityRepository cvRepository;
+//    @Override
+//    public Result<ExtraActivityUpdated> handle(UpdateExtraActivity command) {
+//        ExtraActivity act = cvRepository.findById(new ObjectId(command.getId()));
+//
+//        if (command.getName() != null)
+//            act.name = command.getName();
+//
+//
+//        var result = new ExtraActivityUpdated(act.getId());
+//        return new Result<>(result);
+//    }
+//}
