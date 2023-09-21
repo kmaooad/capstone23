23aa36e9a314ce6eadaff1a16884a72674d1d52c
ElizabethChernova <elizabethchernova@gmail.com>
2023-09-21 12:30:50 +0300

    commands
    commands for creating, updating, deleting extra activities

---
+++ app/src/main/java/edu/kmaooad/capstone23/activities/commands/CreateExtraActivity.java
@@ -0,0 +1,25 @@
+package edu.kmaooad.capstone23.activities.commands;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class CreateExtraActivity {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    private ObjectId id;
+    public ObjectId getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ app/src/main/java/edu/kmaooad/capstone23/activities/dal/ExtraActivity.java
@@ -0,0 +1,10 @@
+package edu.kmaooad.capstone23.activities.dal;
+
+import io.quarkus.mongodb.panache.common.MongoEntity;
+import org.bson.types.ObjectId;
+
+@MongoEntity(collection = "extraActivity")
+public class ExtraActivity {
+    public ObjectId id;
+    public String name;
+}
