0f5b331f8a8375d613f56b145c91e6968bcb19c3
ElizabethChernova <elizabethchernova@gmail.com>
2023-09-21 15:56:11 +0300

    fix

--- app/src/main/java/edu/kmaooad/capstone23/activities/controller/CreateActivityController.java
+++ app/src/main/java/edu/kmaooad/capstone23/activities/controller/CreateActivityController.java
@@ -1,11 +1,7 @@
 package edu.kmaooad.capstone23.activities.controller;
 
 import edu.kmaooad.capstone23.activities.commands.CreateCourse;
-
-
-import edu.kmaooad.capstone23.activities.commands.CreateExtraActivity;
-
-import edu.kmaooad.capstone23.activities.events.ExtraActivityCreated;
+import edu.kmaooad.capstone23.activities.events.CourseCreated;
 import edu.kmaooad.capstone23.common.TypicalController;
 import jakarta.ws.rs.Path;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
@@ -14,7 +10,7 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
 @Path("/activities/extra/create")
 @APIResponse(responseCode = "200", content = {
-    @Content(mediaType = "application/json", schema = @Schema(implementation = ExtraActivityCreated.class)) })
-public class CreateActivityController extends TypicalController<CreateExtraActivity, ExtraActivityCreated> {
+    @Content(mediaType = "application/json", schema = @Schema(implementation = CourseCreated.class)) })
+public class CreateActivityController extends TypicalController<CreateCourse, CourseCreated> {
 
 }
