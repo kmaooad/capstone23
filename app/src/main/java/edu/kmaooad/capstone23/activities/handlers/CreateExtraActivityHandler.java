77bdba38aff57b3a57da36b0d69a35e5f5ac5f9d
ElizabethChernova <elizabethchernova@gmail.com>
2023-09-21 16:45:10 +0300

    Update CreateExtraActivityHandler.java

--- app/src/main/java/edu/kmaooad/capstone23/activities/handlers/CreateExtraActivityHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/activities/handlers/CreateExtraActivityHandler.java
@@ -1,8 +1,7 @@
 package edu.kmaooad.capstone23.activities.handlers;
 import edu.kmaooad.capstone23.activities.dal.ExtraActivity;
 import edu.kmaooad.capstone23.activities.commands.CreateExtraActivity;
-import edu.kmaooad.capstone23.activities.dal.Course;
-import edu.kmaooad.capstone23.activities.dal.CourseRepository;
+
 import edu.kmaooad.capstone23.activities.events.ExtraActivityCreated;
 import edu.kmaooad.capstone23.common.CommandHandler;
 import edu.kmaooad.capstone23.common.Result;
@@ -11,8 +10,7 @@ import jakarta.inject.Inject;
 
 @RequestScoped
 public class CreateExtraActivityHandler implements CommandHandler<CreateExtraActivity, ExtraActivityCreated> {
-    @Inject
-    private CourseRepository courseRepository;
+
 
     @Override
     public Result<ExtraActivityCreated> handle(CreateExtraActivity command) {
@@ -20,7 +18,7 @@ public class CreateExtraActivityHandler implements CommandHandler<CreateExtraAct
         activity.name = command.getName();
 
 
-        var result = new ExtraActivityCreated(course.id.toHexString());
+        var result = new ExtraActivityCreated(activity.id.toHexString());
         return new Result<>(result);
     }
 }
