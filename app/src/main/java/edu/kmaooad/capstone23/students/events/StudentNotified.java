e19849ce423298f67637ad69eeab353f25aff35e
Ivan Dobrovolskyi <ivan.dobrovolskyi@ukma.edu.ua>
2023-09-24 15:34:16 -0400

    add logic to student handler

--- app/src/main/java/edu/kmaooad/capstone23/students/events/StudentNotified.java
+++ app/src/main/java/edu/kmaooad/capstone23/students/events/StudentNotified.java
@@ -1,4 +1,15 @@
 package edu.kmaooad.capstone23.students.events;
 
+import org.bson.types.ObjectId;
+
 public class StudentNotified {
+    private ObjectId studentId;
+
+    public StudentNotified(ObjectId studentId) {
+        this.studentId = studentId;
+    }
+
+    public ObjectId getStudentId() {
+        return studentId;
+    }
 }
