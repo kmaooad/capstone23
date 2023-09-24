b33819cdc4c03287aa8148d5292ee2f08be5ee79
Andrii Zahorulko <72703146+Anoromi@users.noreply.github.com>
2023-09-24 14:59:36 +0300

    add ban validation for UpdateOrgHandler

--- app/src/main/java/edu/kmaooad/capstone23/ban/dal/BannedEntityType.java
+++ app/src/main/java/edu/kmaooad/capstone23/ban/dal/BannedEntityType.java
@@ -2,4 +2,12 @@ package edu.kmaooad.capstone23.ban.dal;
 
 public enum BannedEntityType {
     Member, Department, Organization;
+
+    public String inferredName() {
+        return switch (this) {
+            case Member -> "Member";
+            case Department -> "Department";
+            case Organization -> "Organization";
+        };
+    }
 }
--- app/src/test/java/edu/kmaooad/capstone23/orgs/handlers/UpdateOrgHandlerTest.java
+++ app/src/test/java/edu/kmaooad/capstone23/orgs/handlers/UpdateOrgHandlerTest.java
@@ -1,6 +1,10 @@
 package edu.kmaooad.capstone23.orgs.handlers;
 
+import edu.kmaooad.capstone23.ban.commands.BanEntity;
+import edu.kmaooad.capstone23.ban.dal.BannedEntityType;
+import edu.kmaooad.capstone23.ban.events.EntityBanned;
 import edu.kmaooad.capstone23.common.CommandHandler;
+import edu.kmaooad.capstone23.common.ErrorCode;
 import edu.kmaooad.capstone23.common.Result;
 import edu.kmaooad.capstone23.orgs.commands.CreateOrg;
 import edu.kmaooad.capstone23.orgs.commands.UpdateOrg;
@@ -8,6 +12,7 @@ import edu.kmaooad.capstone23.orgs.events.OrgCreated;
 import edu.kmaooad.capstone23.orgs.events.OrgUpdated;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -24,6 +29,9 @@ public class UpdateOrgHandlerTest {
     @Inject
     CommandHandler<CreateOrg, OrgCreated> createOrgHandler;
 
+    @Inject
+    CommandHandler<BanEntity, EntityBanned> banEntityHandler;
+
     @BeforeEach
     public void setup() {
         CreateOrg command = new CreateOrg();
@@ -36,16 +44,19 @@ public class UpdateOrgHandlerTest {
     @Test
     @DisplayName("Update Org: Basic")
     void testSuccessfulHandling() {
+        var result = updateOrg();
+
+        Assertions.assertTrue(result.isSuccess());
+        Assertions.assertNotNull(result.getValue());
+    }
+
+    Result<OrgUpdated> updateOrg() {
         UpdateOrg command = new UpdateOrg();
         command.orgId = this.createdOrgId;
         command.orgName = "KPI";
         command.industry = "Education";
         command.website = "https://www.ukma.edu.ua/eng/";
-
-        Result<OrgUpdated> result = handler.handle(command);
-
-        Assertions.assertTrue(result.isSuccess());
-        Assertions.assertNotNull(result.getValue());
+        return handler.handle(command);
     }
 
     @Test
@@ -60,5 +71,27 @@ public class UpdateOrgHandlerTest {
         Result<OrgUpdated> result = handler.handle(command);
         
         Assertions.assertFalse(result.isSuccess());
+    }
+
+    @Test
+    @DisplayName("Update Org: Banned org")
+    void testOrgBanned() {
+        var banRequest = new BanEntity();
+        banRequest.setReason("Hello there");
+        banRequest.setEntityId(new ObjectId(createdOrgId));
+        banRequest.setEntityType(BannedEntityType.Organization.inferredName());
+
+        var banResult = banEntityHandler.handle(banRequest);
+
+        Assertions.assertTrue(banResult.isSuccess());
+        Assertions.assertNotNull(banResult.getValue());
+
+        var updateResult = updateOrg();
+
+        Assertions.assertFalse(updateResult.isSuccess());
+        Assertions.assertNotNull(updateResult.toError());
+        Assertions.assertEquals(updateResult.getErrorCode(), ErrorCode.EXCEPTION);
+        Assertions.assertEquals(updateResult.getMessage(), "Org is banned");
+
     }
 }
