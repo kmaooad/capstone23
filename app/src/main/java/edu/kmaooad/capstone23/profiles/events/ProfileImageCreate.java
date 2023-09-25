6c7cb3c450c7f51bbc66b0b3a84ac2e181ecbf0e
Andriiko <andrii.permiakov@getimpulse.app>
2023-09-25 22:53:12 +0300

    Add properties to ProfileImageCreate

--- app/src/main/java/edu/kmaooad/capstone23/profiles/events/ProfileImageCreate.java
+++ app/src/main/java/edu/kmaooad/capstone23/profiles/events/ProfileImageCreate.java
@@ -1,5 +1,18 @@
 package edu.kmaooad.capstone23.profiles.events;
 
 public class ProfileImageCreate {
-    
+    private String base64ImageString;
+    private String id;
+
+    public ProfileImageCreate(String id, String base64ImageString) {
+        this.base64ImageString = base64ImageString;
+    }
+
+    public String getBase64ImageString() {
+        return base64ImageString;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
