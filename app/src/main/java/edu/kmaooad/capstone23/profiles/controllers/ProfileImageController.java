567b3f501c5edc37191c96c3d2132aeeaad0c53f
Andriiko <andrii.permiakov@getimpulse.app>
2023-09-25 22:43:06 +0300

    Create ProfileImageCreate class

--- app/src/main/java/edu/kmaooad/capstone23/profiles/controllers/ProfileImageController.java
+++ app/src/main/java/edu/kmaooad/capstone23/profiles/controllers/ProfileImageController.java
@@ -1,8 +1,14 @@
 package edu.kmaooad.capstone23.profiles.controllers;
 
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import edu.kmaooad.capstone23.profiles.daI.ProfileImageCreate;
 import jakarta.ws.rs.Path;
 
 @Path("/profile/create-image")
+@APIResponse(responseCode = "200", content = {
+        @Content(mediaType = "application/json", schema = @Schema(implementation = ProfileImageCreate.class)) })
 public class ProfileImageController {
     
 }
