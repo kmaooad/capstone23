f6d5f2baa0f1262badae6e5018f1b2789615e523
yuriimysko <mysko.yura@gmail.com>
2023-09-26 09:32:41 +0300

    fix test

--- app/src/main/java/edu/kmaooad/capstone23/orgs/controllers/SetHiringStatusOnController.java
+++ app/src/main/java/edu/kmaooad/capstone23/orgs/controllers/SetHiringStatusOnController.java
@@ -4,7 +4,16 @@ import edu.kmaooad.capstone23.common.TypicalController;
 import edu.kmaooad.capstone23.orgs.commands.SetHiringStatusOn;
 import edu.kmaooad.capstone23.orgs.events.HiringStatusSettedOn;
 import jakarta.ws.rs.Path;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
 @Path("/orgs/set-hiring-status-on")
+@APIResponse(responseCode = "200", content = {
+        @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = HiringStatusSettedOn.class)
+        )}
+)
 public class SetHiringStatusOnController extends TypicalController<SetHiringStatusOn, HiringStatusSettedOn> {
 }
