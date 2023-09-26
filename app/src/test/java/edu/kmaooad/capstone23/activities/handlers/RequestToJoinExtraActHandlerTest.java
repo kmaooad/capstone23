35b6e9be22bb54bb8201e6dc5ec7d4642150165b
Dmytro Parnak <75496516+dmtroo@users.noreply.github.com>
2023-09-26 23:22:23 +0300

    Feature/request to join extra act/basic handling (#122)
    * the request to join extracurricular activity was added
    
    * the request to join extracurricular activity test1 was added
    
    * Request to join Extra Act controller and dal fixed
    
    * the request to join extracurricular activity controller test1 was added
    
    * the request to join extracurricular activity controller test1 was added
    
    * codecov/patch fixed
    
    * No such activity issue fixed
    
    * Create Request to Join Activity: Basic
    
    * Create Request to Join Activity: Error handling when activity is not found
    
    * Create Request to Join Activity: Error handling when user is already part of the activity
    
    * codecov/patch fixed
    
    * Create Request to Join Activity: Error handling when user sends multiple requests for the same activity
    
    * Create Request to Join Activity: Basic handling
    
    ---------
    
    Co-authored-by: margoshiiik <margo.budenkova@gmail.com>

--- app/src/test/java/edu/kmaooad/capstone23/activities/handlers/RequestToJoinExtraActHandlerTest.java
+++ app/src/test/java/edu/kmaooad/capstone23/activities/handlers/RequestToJoinExtraActHandlerTest.java
@@ -36,7 +36,7 @@ public class RequestToJoinExtraActHandlerTest {
 
         extraActId = extraAct.id.toString();
     }
-  
+
     @Test
     @DisplayName("Create Request to Join Activity: Basic")
     public void testBasicRequestToJoinActivity() {
@@ -107,6 +107,27 @@ public class RequestToJoinExtraActHandlerTest {
         Result<RequestCreated> secondResult = handler.handle(secondCommand);
         Assertions.assertFalse(secondResult.isSuccess());
         Assertions.assertEquals("User is already part of the activity", secondResult.getMessage());
+    }
+
+    @Test
+    @DisplayName("Create Request to Join Activity: Basic handling when the activity has no participants yet")
+    public void testRequestToJoinActivityWithNoParticipants() {
+        String userName = "person3";
+
+        ExtracurricularActivity extraAct = new ExtracurricularActivity();
+        extraAct.extracurricularActivityName = "No Participants Activity";
+        extraActRepository.insert(extraAct);
+        String noParticipantsExtraActId = extraAct.id.toString();
+
+        RequestToJoinExtraAct command = new RequestToJoinExtraAct();
+        command.setUserName(userName);
+        command.setExtraActId(noParticipantsExtraActId);
+        Result<RequestCreated> result = handler.handle(command);
+        Assertions.assertTrue(result.isSuccess());
+        Assertions.assertNotNull(result.getValue());
+
+        Request resultRequest = requestsRepository.findById(result.getValue().getId());
+        Assertions.assertNotNull(resultRequest);
     }
 
 }
