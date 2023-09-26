3339454718ddad93b21b4d29328079766759d9fb
annazhelizniak <84460049+annazhelizniak@users.noreply.github.com>
2023-09-26 11:43:06 +0300

    Feature/unrelate job to competences handler (#101)
    * event for delete relation job to activity
    
    * Add command
    
    * Add handler
    
    * event handler test for delete relation job to activity
    
    * controller for delete relation job to activity
    
    * Add test with valid and not valid job
    
    * add controller test for delete relation job to activity. Tests run successfully
    
    * event for delete relation job to competences.
    
    * event for delete relation job to competences.
    
    * event for delete relation job to activity
    
    * add controller
    
    * Feature/experts department interaction assign (#85)
    
    * department entity assigned to expert entity
    
    * Department assignment to expert + basic controller and handler tests
    
    * test coverage
    
    * organisation name changed
    
    ---------
    
    Co-authored-by: Liubomyr <liubomyr.stepanenko@ukma.edu.ua>
    
    * add command
    
    * add handler
    
    * handler test for delete relation job to competences.
    
    * fix error in test
    
    ---------
    
    Co-authored-by: miksasha <miksasha2003@gmail.com>
    Co-authored-by: Liubomyr Stepanenko <98842543+liubcheck@users.noreply.github.com>
    Co-authored-by: Liubomyr <liubomyr.stepanenko@ukma.edu.ua>

--- app/src/main/java/edu/kmaooad/capstone23/jobs/handlers/DeleteRelationJobToCompetencesHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/handlers/DeleteRelationJobToCompetencesHandler.java
@@ -3,36 +3,34 @@ package edu.kmaooad.capstone23.jobs.handlers;
 import edu.kmaooad.capstone23.common.CommandHandler;
 import edu.kmaooad.capstone23.common.ErrorCode;
 import edu.kmaooad.capstone23.common.Result;
-import edu.kmaooad.capstone23.jobs.commands.RelateJobToActivities;
-import edu.kmaooad.capstone23.jobs.commands.RelateJobToCompetences;
+import edu.kmaooad.capstone23.jobs.commands.DeleteRelateJobToCompetences;
 import edu.kmaooad.capstone23.jobs.dal.Job;
 import edu.kmaooad.capstone23.jobs.dal.JobRepository;
-import edu.kmaooad.capstone23.jobs.events.ActivityRelated;
-import edu.kmaooad.capstone23.jobs.events.CompetenceRelated;
+import edu.kmaooad.capstone23.jobs.events.CompetenceUnrelated;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 
 import java.util.Optional;
-
 @RequestScoped
-public class RelateJobToCompetencesHandler implements CommandHandler<RelateJobToCompetences, CompetenceRelated> {
+public class DeleteRelationJobToCompetencesHandler  implements CommandHandler<DeleteRelateJobToCompetences, CompetenceUnrelated> {
+
     @Inject
     private JobRepository repository;
-
     @Override
-    public Result<CompetenceRelated> handle(RelateJobToCompetences command) {
+    public Result<CompetenceUnrelated> handle(DeleteRelateJobToCompetences command) {
 
         Optional<Job> job = repository.findByIdOptional(command.getJobId());
         if(job.isEmpty())
             return new Result<>(ErrorCode.VALIDATION_FAILED, "This job was previously deleted or never existed");
 
-        CompetenceRelated result = new CompetenceRelated(command.getJobId(), command.getCompetenceId());
+        CompetenceUnrelated result = new CompetenceUnrelated(command.getJobId(), command.getCompetenceId());
 
         Job j = job.get();
-        j.competencesId.add(command.getCompetenceId());
-
+        if (!j.competencesId.contains(command.getCompetenceId()))
+            return new Result<>(ErrorCode.VALIDATION_FAILED, "This job doesn't contain this competence");
+        j.competencesId.remove(command.getCompetenceId());
         repository.update(j);
 
-        return new Result<CompetenceRelated>(result);
+        return new Result<CompetenceUnrelated>(result);
     }
 }
