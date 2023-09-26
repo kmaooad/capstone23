7eb84b8ff435b5703c7cc460bc6e5e135bf93813
Danylo Nechyporchuk <75624164+CorTexDotMe@users.noreply.github.com>
2023-09-26 10:26:47 +0300

    Feature/join org with corp email/create member by corp email handler (#92)
    * add email domain field
    
    * create unique index for member email
    
    * add tests
    
    * add controller tests
    
    * add email parser
    
    * test commit
    
    * add CorpEmailParserTest
    
    * Remove ci on pull request
    
    * Revert "create unique index for member email"
    
    This reverts commit 8e3380b63156b4d9a94b23b9d48911d539444662.
    
    * Make OrgsRepository method findByEmailDomain(...) return optional
    
    * Refactor create member handler
    
    * Add command and handler for member creation by corp email
    
    * Fix updateOrg test
    
    * Handler tests
    
    * Delete experts before and after tests
    
    * Fix handler test
    
    * Rewrite create member by corp email handler
    
    Change handler to reuse CreateMemberHandler
    
    ---------
    
    Co-authored-by: Vsevolod Kalinbet <vsevolod180203@gmail.com>
    Co-authored-by: Anton Shabinskiy <a@bnsk.pro>

---
+++ app/src/main/java/edu/kmaooad/capstone23/members/handlers/CreateMemberByCorpEmailHandler.java
@@ -0,0 +1,43 @@
+package edu.kmaooad.capstone23.members.handlers;
+
+import edu.kmaooad.capstone23.common.CommandHandler;
+import edu.kmaooad.capstone23.common.ErrorCode;
+import edu.kmaooad.capstone23.common.Result;
+import edu.kmaooad.capstone23.members.commands.CreateBasicMember;
+import edu.kmaooad.capstone23.members.commands.CreateMemberByCorpEmail;
+import edu.kmaooad.capstone23.members.events.BasicMemberCreated;
+import edu.kmaooad.capstone23.members.utils.CorpEmailParser;
+import edu.kmaooad.capstone23.orgs.dal.Org;
+import edu.kmaooad.capstone23.orgs.dal.OrgsRepository;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+
+import java.util.Optional;
+
+@RequestScoped
+public class CreateMemberByCorpEmailHandler implements CommandHandler<CreateMemberByCorpEmail, BasicMemberCreated> {
+    @Inject
+    CommandHandler<CreateBasicMember, BasicMemberCreated> handler;
+
+    @Inject
+    OrgsRepository orgsRepository;
+    @Inject
+    CorpEmailParser corpEmailParser;
+
+    @Override
+    public Result<BasicMemberCreated> handle(CreateMemberByCorpEmail command) {
+        String orgEmailDomain = corpEmailParser.getCorpEmailDomain(command.getCorpEmail());
+        Optional<Org> memberOrg = orgsRepository.findByEmailDomainOptional(orgEmailDomain);
+        if (memberOrg.isEmpty())
+            return new Result<>(ErrorCode.VALIDATION_FAILED, "Organisation not found");
+
+        CreateBasicMember createBasicMemberCommand = new CreateBasicMember();
+        createBasicMemberCommand.setFirstName(command.getFirstName());
+        createBasicMemberCommand.setLastName(command.getLastName());
+        createBasicMemberCommand.setEmail(command.getCorpEmail());
+        createBasicMemberCommand.setIsExpert(command.getIsExpert());
+        createBasicMemberCommand.setOrgId(memberOrg.get().id);
+
+        return handler.handle(createBasicMemberCommand);
+    }
+}
