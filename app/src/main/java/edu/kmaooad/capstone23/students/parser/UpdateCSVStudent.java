afb534a20e2f5ca91266b1c69ea8a2d68ca0d715
ireneMiller <84085656+ireneMel@users.noreply.github.com>
2023-09-26 14:08:10 -0400

    Create UpdateCSVStudentParser (#83)
    * create student dal
    
    * create command CreateStudent
    
    * create controller that accepts files
    
    * change CreateStudent to contain File field
    
    * create empty repository
    
    * create empty event
    
    * create empty controller
    
    * add StudentsCreated
    
    * add empty content to CreateStudentHandler
    
    * optimize imports
    
    * create csv student model
    
    * create empty csv student parser
    
    * add logic to StudentRepository
    
    * add logic to CreateStudentHandler
    
    * change FileController with CreateStudentController
    
    * rename content to csvFile
    
    * add implementation to CSVStudentParser
    
    * add implementation to CreateStudentHandler
    
    * add check for correct type upload
    
    * add student fields amount exception
    
    * add CreateStudentControllerTest
    
    * set CSVStudentParser date formatter to strict parse
    
    * add test files
    
    * add check for empty string. close readers.
    
    * add more unit tests
    
    * add comment
    
    * fix tests
    
    * remove unused methods
    
    * Optimize imports
    
    * add equals and hashcode to CSVStudent
    
    * set timezone to GMT for parser
    
    * add tests for parser
    
    * add tests for CSVStudent
    
    * extract student to static
    
    * add quarkus test annotation
    
    * add more tests to CSVStudent
    
    * Add update csv student parser
    
    * Rename CSVStudentParser to CreateCSVStudentParser
    
    * Add empty constructor to CSVStudent
    
    * Add test files for UpdateCSVStudentParser testing
    
    * Add test for UpdateCSVStudent
    
    * Add test for UpdateCSVStudentParser
    
    * Update condition for min. acceptable number of fields
    
    * Change testUpdateStudentsNotEnoughFields exception
    
    * remove unused code
    
    * remove duplicated file
    
    ---------
    
    Co-authored-by: Ivan Dobrovolskyi <ivan.dobrovolskyi@ukma.edu.ua>

---
+++ app/src/main/java/edu/kmaooad/capstone23/students/parser/UpdateCSVStudent.java
@@ -0,0 +1,70 @@
+package edu.kmaooad.capstone23.students.parser;
+
+import org.bson.types.ObjectId;
+
+import java.util.Objects;
+
+public class UpdateCSVStudent {
+
+    private ObjectId id;
+    private String firstName = "";
+    private String middleName = "";
+    private String lastName = "";
+    private long DOBTimestamp = 0;
+    private String email = "";
+
+    public UpdateCSVStudent() {
+    }
+
+    public UpdateCSVStudent(ObjectId id, String firstName, String middleName, String lastName, long DOBTimestamp, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.DOBTimestamp = DOBTimestamp;
+        this.email = email;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDOBTimestamp(long DOBTimestamp) {
+        this.DOBTimestamp = DOBTimestamp;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UpdateCSVStudent that = (UpdateCSVStudent) o;
+        return DOBTimestamp == that.DOBTimestamp &&
+                Objects.equals(id, that.id) &&
+                Objects.equals(firstName, that.firstName) &&
+                Objects.equals(middleName, that.middleName) &&
+                Objects.equals(lastName, that.lastName) &&
+                Objects.equals(email, that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, middleName, lastName, DOBTimestamp, email);
+    }
+}
+
