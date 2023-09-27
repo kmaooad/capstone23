4b6c7a8b26056dca0119c614c676bbfe8acf3370
annazhelizniak <anna.zhelizniak08@gmail.com>
2023-09-27 14:03:16 +0300

    add test case for not existing activity

--- app/src/main/java/edu/kmaooad/capstone23/groups/handlers/AssignGroupToActivitiesHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/groups/handlers/AssignGroupToActivitiesHandler.java
@@ -1,5 +1,9 @@
 package edu.kmaooad.capstone23.groups.handlers;
 
+import edu.kmaooad.capstone23.activities.dal.Course;
+import edu.kmaooad.capstone23.activities.dal.CourseRepository;
+import edu.kmaooad.capstone23.activities.dal.ExtracurricularActivity;
+import edu.kmaooad.capstone23.activities.dal.ExtracurricularActivityRepository;
 import edu.kmaooad.capstone23.common.CommandHandler;
 import edu.kmaooad.capstone23.common.ErrorCode;
 import edu.kmaooad.capstone23.common.Result;
@@ -18,6 +22,15 @@ public class AssignGroupToActivitiesHandler  implements CommandHandler<AssignGro
     @Inject
     private GroupsRepository repository;
 
+    @Inject
+    private ExtracurricularActivityRepository extracurricularActivityRepository;
+
+    @Inject
+    private CourseRepository courseRepository;
+
+
+    @Inject
+
     @Override
     public Result<ActivityAssigned> handle(AssignGroupToActivity command) {
 
@@ -26,7 +39,12 @@ public class AssignGroupToActivitiesHandler  implements CommandHandler<AssignGro
             return new Result<>(ErrorCode.VALIDATION_FAILED, "This group was previously deleted or never existed");
 
         ActivityAssigned result = new ActivityAssigned(command.getGroupId(), command.getActivityId());
-
+        Optional<Course> activity = courseRepository.findByIdOptional(command.getGroupId());
+        if(activity.isEmpty()){
+            Optional<ExtracurricularActivity> activity1 = extracurricularActivityRepository.findByIdOptional(command.getGroupId());
+            if(activity1.isEmpty())
+                return new Result<>(ErrorCode.VALIDATION_FAILED, "This activity doesn't exist");
+        }
         Group g = group.get();
         if(g.activitiesId.contains(command.getActivityId()))
             return new Result<>(ErrorCode.VALIDATION_FAILED, "This activity is already assigned to this group");
