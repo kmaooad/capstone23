26f6c48e0b1492745d27e311287ec20107c2506d
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2023-10-01 23:39:25 +0300

    Add update tag controller

--- app/src/main/java/edu/kmaooad/capstone23/tag/dal/Tag.java
+++ app/src/main/java/edu/kmaooad/capstone23/tag/dal/Tag.java
@@ -2,9 +2,8 @@ package edu.kmaooad.capstone23.tag.dal;
 
 import io.quarkus.mongodb.panache.PanacheMongoEntity;
 import io.quarkus.mongodb.panache.common.MongoEntity;
-import jakarta.validation.constraints.Pattern;
 
 @MongoEntity(collection = "tags")
 public class Tag extends PanacheMongoEntity {
-    String tagName;
+    public String tagName;
 }
