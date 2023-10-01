b7d27570a6ad45d83cf9e8ead18009afc5fd6029
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2023-10-01 23:55:27 +0300

    Add update tag controller and tests

---
+++ app/src/test/java/edu/kmaooad/capstone23/tag/controllers/UpdateTagControllerTests.java
@@ -0,0 +1,107 @@
+package edu.kmaooad.capstone23.tag.controllers;
+
+import edu.kmaooad.capstone23.tag.dal.Tag;
+import edu.kmaooad.capstone23.tag.dal.TagRepository;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+@QuarkusTest
+public class UpdateTagControllerTests {
+    @Inject
+    TagRepository tagRepository;
+
+    @Test
+    @DisplayName("Update Tag : Basic")
+    public void testTagNameUpdate() {
+        var tag = new Tag();
+        tag.tagName = "testTag";
+        tagRepository.persist(tag);
+
+        Map<String, Object> jsonAsMap = new HashMap<>();
+        jsonAsMap.put("id",tag.id);
+        jsonAsMap.put("tagName", "new tag name");
+
+        given()
+                .contentType("application/json")
+                .body(jsonAsMap)
+                .when()
+                .post("/tags/update")
+                .then()
+                .statusCode(200)
+                .body("id", notNullValue())
+                .body("name", notNullValue());
+    }
+    @Test
+    @DisplayName("Update Tag : Name Validation")
+    public void testUpdateGroupForInvalidGroupName() {
+        var tag = new Tag();
+        tag.tagName = "testTag";
+        tagRepository.persist(tag);
+
+        Map<String, Object> jsonAsMap = new HashMap<>();
+        jsonAsMap.put("id",tag.id);
+        jsonAsMap.put("tagName", "");
+
+        given()
+                .contentType("application/json")
+                .body(jsonAsMap)
+                .when()
+                .post("/tags/update")
+                .then()
+                .statusCode(400);
+    }
+
+    @Test
+    @DisplayName("Update Tag : Invalid ID")
+    public void testUpdateGroupForInvalidID() {
+        var tag = new Tag();
+        tag.tagName = "testTag";
+        tagRepository.persist(tag);
+
+        Map<String, Object> jsonAsMap = new HashMap<>();
+        jsonAsMap.put("id", "abcdef");
+        jsonAsMap.put("tagName", "new tag name");
+
+        given()
+                .contentType("application/json")
+                .body(jsonAsMap)
+                .when()
+                .post("/tags/update")
+                .then()
+                .statusCode(400);
+    }
+    @Test
+    @DisplayName("Update Tag : Non Existent Tag")
+    public void testUpdateNonExistentGroup() {
+        var tag = new Tag();
+        tag.tagName = "testTag";
+        tagRepository.persist(tag);
+
+        Map<String, Object> jsonAsMap = new HashMap<>();
+        jsonAsMap.put("id", ObjectId.get());
+        jsonAsMap.put("tagName", "");
+
+        given()
+                .contentType("application/json")
+                .body(jsonAsMap)
+                .when()
+                .post("/tags/update")
+                .then()
+                .statusCode(400);
+    }
+
+    @AfterEach
+    public void teardown() {
+        tagRepository.deleteAll();
+    }
+}
