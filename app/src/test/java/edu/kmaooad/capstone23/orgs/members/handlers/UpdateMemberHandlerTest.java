9cd855ec514de20b2fee7346ba814e9d4e2bacc4
Danylo Nechyporchuk <danylo.nechyporchuk@gmail.com>
2023-10-01 17:58:46 +0300

    Fix member update behavior
    Throw UniquenessViolationException after member update only if another member with provided email exists. This fix gives opportunity to update specific field instead of all fields with member update.
    
    New test to check new behavior and fix for previous test

--- app/src/test/java/edu/kmaooad/capstone23/orgs/members/handlers/UpdateMemberHandlerTest.java
+++ app/src/test/java/edu/kmaooad/capstone23/orgs/members/handlers/UpdateMemberHandlerTest.java
@@ -4,6 +4,7 @@ import edu.kmaooad.capstone23.common.CommandHandler;
 import edu.kmaooad.capstone23.common.ErrorCode;
 import edu.kmaooad.capstone23.common.Result;
 import edu.kmaooad.capstone23.members.commands.UpdateMember;
+import edu.kmaooad.capstone23.members.dal.Member;
 import edu.kmaooad.capstone23.members.events.MemberUpdated;
 import edu.kmaooad.capstone23.orgs.members.TestWithOrgSetUp;
 import io.quarkus.test.junit.QuarkusTest;
@@ -54,11 +55,20 @@ public class UpdateMemberHandlerTest extends TestWithOrgSetUp {
     @Test
     @DisplayName("Update Member: Email uniqueness validation handling")
     void testEmailUniquenessValidation() {
+        String notUniqueEmail = "updateToCheckUniquenessValidation@gmail.com";
+
+        Member member = new Member();
+        member.firstName = "New";
+        member.lastName = "Member";
+        member.orgId = createdOrgId;
+        member.email = notUniqueEmail;
+        membersRepository.insert(member);
+
         UpdateMember command = new UpdateMember();
         command.setFirstName("firstName");
         command.setLastName("lastName");
         command.setOrgId(createdOrgId);
-        command.setEmail("email@email.com1");
+        command.setEmail(notUniqueEmail);
         command.setId(new ObjectId(createMember()));
 
         Result<MemberUpdated> result = handler.handle(command);
@@ -81,5 +91,31 @@ public class UpdateMemberHandlerTest extends TestWithOrgSetUp {
 
         Assertions.assertFalse(result.isSuccess());
         Assertions.assertEquals(ErrorCode.NOT_FOUND, result.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("Update member: only name field update")
+    void testUpdateOnlyMemberName() {
+        Member member = new Member();
+        member.firstName = "New";
+        member.lastName = "Member";
+        member.orgId = createdOrgId;
+        member.email = "updateOnlyMemberName@gmail.com";
+        member = membersRepository.insert(member);
+
+        String newMemberName = "newMemberName";
+        UpdateMember command = new UpdateMember();
+        command.setFirstName(newMemberName);
+        command.setLastName(member.lastName);
+        command.setOrgId(member.orgId);
+        command.setEmail(member.email);
+        command.setId(member.id);
+
+        Result<MemberUpdated> result = handler.handle(command);
+
+        Assertions.assertTrue(result.isSuccess());
+        Assertions.assertNotNull(result.getValue());
+        Assertions.assertEquals(result.getValue().getId(), command.getId());
+        Assertions.assertEquals(result.getValue().getFirstName(), newMemberName);
     }
 }
