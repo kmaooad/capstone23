cf1cae8f9154fdd537c2eefb050197236420ed36
Illia Shevchyk <illia.shevchyk@ukma.edu.ua>
2023-10-01 22:19:46 +0200

    Add tagHandler

---
+++ app/src/main/java/edu/kmaooad/capstone23/tag/events/TagCreated.java
@@ -0,0 +1,13 @@
+package edu.kmaooad.capstone23.tag.events;
+
+import org.bson.types.ObjectId;
+
+public class TagCreated {
+    ObjectId objectId;
+    String tagName;
+
+    public TagCreated(ObjectId objectId, String tagName) {
+        this.objectId = objectId;
+        this.tagName = tagName;
+    }
+}
