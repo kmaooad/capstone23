4927687e419144f38c2341e9b197510b7bad6aad
dmytro-pelovych <dmytro.pelovych@gmail.com>
2023-10-02 23:00:06 +0300

    Refactor existing flows

--- app/src/test/java/edu/kmaooad/capstone23/common/HandlerTest.java
+++ app/src/test/java/edu/kmaooad/capstone23/common/HandlerTest.java
@@ -3,7 +3,7 @@ package edu.kmaooad.capstone23.common;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Assertions;
 
-public abstract class HandlerTest<EntityType, CommandType, EventType extends Event> {
+public abstract class HandlerTest<EntityType, CommandType, EventType> {
   protected CommandType command;
 
   @Inject
@@ -23,7 +23,6 @@ public abstract class HandlerTest<EntityType, CommandType, EventType extends Eve
   protected void assertHandled(Result<EventType> result) {
     Assertions.assertTrue(result.isSuccess());
     Assertions.assertNotNull(result.getValue());
-    Assertions.assertFalse(result.getValue().getId().isEmpty());
   }
 
   protected void assertDenied(Result<EventType> result) {
