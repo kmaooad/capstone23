feca8f64b45e36354ef2004a84515dbcd096811b
Dmytro Pelovych <75496546+jstmpelowycz@users.noreply.github.com>
2023-10-02 20:27:23 +0300

    [Chats]: Setup creation flow with test coverage (#159)
    * Add basic entities
    
    * Add basic repositories
    
    * Handle request
    
    * Add tests abstractions
    
    * Update command/event
    
    * Add mocks
    
    * Add tests themselves
    
    * Add tests themselves

---
+++ app/src/main/java/edu/kmaooad/capstone23/communication/events/ChatCreated.java
@@ -0,0 +1,9 @@
+package edu.kmaooad.capstone23.communication.events;
+
+import edu.kmaooad.capstone23.common.Event;
+
+public class ChatCreated extends Event {
+  public ChatCreated(String id) {
+    super(id);
+  }
+}
