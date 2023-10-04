f36ea146a870e228c5ea8ddd31aad4999ea4d64b
IvanBlyzniuk <84847629+IvanBlyzniuk@users.noreply.github.com>
2023-10-04 19:25:42 +0300

    + UpdateAccessRuleHandler ~ AccessRuleUpdated now uses enums instead of strings

--- app/src/main/java/edu/kmaooad/capstone23/access_rules/events/AccessRuleUpdated.java
+++ app/src/main/java/edu/kmaooad/capstone23/access_rules/events/AccessRuleUpdated.java
@@ -2,6 +2,7 @@ package edu.kmaooad.capstone23.access_rules.events;
 
 import edu.kmaooad.capstone23.access_rules.dal.AccessRuleFromEntityType;
 import edu.kmaooad.capstone23.access_rules.dal.AccessRuleToEntityType;
+import edu.kmaooad.capstone23.access_rules.dal.AccessRuleType;
 
-public record AccessRuleUpdated (String id, String ruleType, AccessRuleFromEntityType fromEntityType, String fromId,
+public record AccessRuleUpdated (String id, AccessRuleType ruleType, AccessRuleFromEntityType fromEntityType, String fromId,
                                  AccessRuleToEntityType toEntityType, String toId) {}
