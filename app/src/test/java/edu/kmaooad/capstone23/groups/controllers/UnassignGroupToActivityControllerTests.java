868a734cb4238da4d0995ac1ad7229d3b42b1c0e
miksasha <miksasha2003@gmail.com>
2023-10-04 18:05:49 +0300

    tests run successfully for unassign activity to group, not existing group

--- app/src/test/java/edu/kmaooad/capstone23/groups/controllers/UnassignGroupToActivityControllerTests.java
+++ app/src/test/java/edu/kmaooad/capstone23/groups/controllers/UnassignGroupToActivityControllerTests.java
@@ -1,6 +1,8 @@
 package edu.kmaooad.capstone23.groups.controllers;
 
 import edu.kmaooad.capstone23.activities.commands.CreateCourse;
+import edu.kmaooad.capstone23.activities.dal.Course;
+import edu.kmaooad.capstone23.activities.dal.CourseRepository;
 import edu.kmaooad.capstone23.activities.events.CourseCreated;
 import edu.kmaooad.capstone23.common.CommandHandler;
 import edu.kmaooad.capstone23.common.Result;
@@ -17,6 +19,7 @@ import edu.kmaooad.capstone23.jobs.events.JobCreated;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 import org.bson.types.ObjectId;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -36,6 +39,20 @@ public class UnassignGroupToActivityControllerTests {
     @Inject
     CommandHandler<CreateGroupTemplate, GroupTemplateCreated> templateHandler;
 
+    private ObjectId idToUpdate;
+
+
+    @Inject
+    CourseRepository courseRepository;
+
+    @BeforeEach
+    void setUp() {
+        Course course = new Course();
+        course.name = "Initial Course";
+        courseRepository.insert(course);
+        idToUpdate = course.id;
+
+    }
     @Test
     @DisplayName("Delete Relate Job To Activities: existed relation, valid input")
     public void testBasicJobActivitiesConnectionRemoving() {
@@ -73,6 +90,30 @@ public class UnassignGroupToActivityControllerTests {
                 .post("/groups/unassign_group_to_activities")
                 .then()
                 .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Relate Group To Activities: notExisted group")
+    public void testNotExistedGroupActivityConnectionCreation() {
+        ObjectId id = new ObjectId("aaaaaaaaaaaaaaaaaaaaaaaa");
+
+        CreateCourse commandCourse = new CreateCourse();
+        commandCourse.setName("Math");
+        Result<CourseCreated> resultCourse = handlerForActivities.handle(commandCourse);
+        ObjectId idCourse = new ObjectId(resultCourse.getValue().getId());
+
+        Map<String, Object> jsonAsMap = new HashMap<>();
+
+        jsonAsMap.put("groupId", id);
+        jsonAsMap.put("activityId", idCourse);
+
+        given()
+                .contentType("application/json")
+                .body(jsonAsMap)
+                .when()
+                .post("/groups/unassign_group_to_activities")
+                .then()
+                .statusCode(400);
     }
 
 }
