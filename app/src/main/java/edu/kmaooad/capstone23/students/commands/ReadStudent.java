a511c205c6cbcbd6aeb8cf7f7b7443f5e4f251cf
Iryna Melnyk <iryna2.melnyk@ukma.edu.ua>
2023-10-08 12:07:39 -0400

    Add instances for input and output search for a student by id

---
+++ app/src/main/java/edu/kmaooad/capstone23/students/commands/ReadStudent.java
@@ -0,0 +1,13 @@
+package edu.kmaooad.capstone23.students.commands;
+
+public class ReadStudent {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
