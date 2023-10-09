e91f8599f3941ff5eb8d6de676d7744e9f862572
Anna-Kudiakova <ann.kudiakova@gmail.com>
2023-10-08 23:33:44 -0400

    add delete access rule controller test - non existent id

--- app/src/test/java/edu/kmaooad/capstone23/access_rules/controllers/DeleteAccessRuleControllerTests.java
+++ app/src/test/java/edu/kmaooad/capstone23/access_rules/controllers/DeleteAccessRuleControllerTests.java
@@ -5,6 +5,7 @@ import edu.kmaooad.capstone23.access_rules.dal.AccessRuleRepository;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.*;
 
 import static io.restassured.RestAssured.given;
@@ -37,6 +38,12 @@ public class DeleteAccessRuleControllerTests {
     @DisplayName("Delete Access Rule: invalid ID format")
     public void deleteRuleInvalidId() {
         deleteAccessRule("1234abc", 400);
+    }
+
+        @Test
+    @DisplayName("Delete Access Rule: non-existent ID")
+    public void deleteRuleNonExistentId() {
+        deleteAccessRule(new ObjectId().toString(), 400);
     }
 
 
