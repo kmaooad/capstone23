8e364495a665689b9c1260b2568b8314ca3fa22e
Anna-Kudiakova <ann.kudiakova@gmail.com>
2023-10-08 23:36:20 -0400

    add ban flag and implement logic for banning in rules repository

--- app/src/main/java/edu/kmaooad/capstone23/access_rules/dal/AccessRuleRepository.java
+++ app/src/main/java/edu/kmaooad/capstone23/access_rules/dal/AccessRuleRepository.java
@@ -1,5 +1,7 @@
 package edu.kmaooad.capstone23.access_rules.dal;
 
+import java.util.List;
+
 import org.bson.types.ObjectId;
 
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
@@ -16,4 +18,14 @@ public class AccessRuleRepository implements PanacheMongoRepository<AccessRule> 
     public void deleteRule(ObjectId id) {
         delete("id", id);
     }
+
+    public List<AccessRule> findByEntityIdAndType(ObjectId entityId, AccessRuleFromEntityType entityType) {
+        return find("entityId = ?1 and entityType = ?2", entityId, entityType).list();
+    }
+
+    public void ban(ObjectId entityId, String entityType) {
+        update("banned = true where fromEntityId = ?1 and fromEntityType = ?2", entityId, entityType);
+        update("banned = true where toEntityId = ?1 and toEntityType = ?2", entityId, entityType);
+    }
+
 }
