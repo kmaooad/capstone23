8fa8fb5d49fb7f8add787b502b10e9bb79afdfed
Ivan Dobrovolskyi <ivan.dobrovolskyi@ukma.edu.ua>
2023-10-09 18:09:10 -0400

    add handler test

---
+++ app/src/test/java/edu/kmaooad/capstone23/students/handlers/FindStudentHandlerTest.java
@@ -0,0 +1,112 @@
+package edu.kmaooad.capstone23.students.handlers;
+
+import edu.kmaooad.capstone23.common.Result;
+import edu.kmaooad.capstone23.students.commands.FindStudent;
+import edu.kmaooad.capstone23.students.dal.StudentRepository;
+import edu.kmaooad.capstone23.students.events.StudentsFound;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.*;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+public class FindStudentHandlerTest {
+
+    @Inject
+    FindStudentHandler handler;
+
+    @Inject
+    StudentRepository repository;
+
+    @BeforeEach
+    public void setup() {
+        repository.deleteAll();
+
+        given()
+                .multiPart("csvFile", new File("src/test/resources/students/find_init.csv"), "text/csv")
+                .when()
+                .post("/students/create_csv")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Find all students by default")
+    public void testFindAllStudents() {
+        FindStudent command = new FindStudent();
+        Result<StudentsFound> result = handler.handle(command);
+
+        Assertions.assertTrue(result.isSuccess());
+        Assertions.assertNotNull(result.getValue());
+        Assertions.assertEquals(3, result.getValue().getStudents().size());
+    }
+
+    @Test
+    @DisplayName("Find students by first name")
+    public void testFindStudentsByFirstName() {
+        FindStudent command = new FindStudent();
+        command.setFirstName("Iva");
+        Result<StudentsFound> result = handler.handle(command);
+
+        Assertions.assertTrue(result.isSuccess());
+        Assertions.assertNotNull(result.getValue());
+        Assertions.assertEquals(3, result.getValue().getStudents().size());
+    }
+
+    @Test
+    @DisplayName("Find students by middle name")
+    public void testFindStudentsByMiddleName() {
+        FindStudent command = new FindStudent();
+        command.setMiddleName("Oleksandrovych");
+        Result<StudentsFound> result = handler.handle(command);
+
+        Assertions.assertTrue(result.isSuccess());
+        Assertions.assertNotNull(result.getValue());
+        Assertions.assertEquals(2, result.getValue().getStudents().size());
+    }
+
+    @Test
+    @DisplayName("Find students by last name")
+    public void testFindStudentsByLastName() {
+        FindStudent command = new FindStudent();
+        command.setLastName("Dobrovolskyiii");
+        Result<StudentsFound> result = handler.handle(command);
+
+        Assertions.assertTrue(result.isSuccess());
+        Assertions.assertNotNull(result.getValue());
+        Assertions.assertEquals(1, result.getValue().getStudents().size());
+    }
+
+    @Test
+    @DisplayName("Find students by multiple filters: Empty")
+    public void testFindStudentsByMultipleFiltersEmpty() {
+        FindStudent command = new FindStudent();
+        command.setFirstName("Ivan1");
+        command.setMiddleName("Oleksandrovychh");
+        command.setLastName("Dobrovolskyiii");
+        Result<StudentsFound> result = handler.handle(command);
+
+        Assertions.assertTrue(result.isSuccess());
+        Assertions.assertNotNull(result.getValue());
+        Assertions.assertTrue(result.getValue().getStudents().isEmpty());
+    }
+
+    @Test
+    @DisplayName("Find students by multiple filters")
+    public void testFindStudentsByMultipleFilters() {
+        FindStudent command = new FindStudent();
+        command.setFirstName("Iva");
+        command.setLastName("Dobrovolskyi");
+        Result<StudentsFound> result = handler.handle(command);
+
+        Assertions.assertTrue(result.isSuccess());
+        Assertions.assertNotNull(result.getValue());
+        Assertions.assertEquals(2, result.getValue().getStudents().size());
+    }
+
+}
