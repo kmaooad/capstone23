7a28b46a0db8b22c446e8c9e641ac1bb5d5cabee
VasylPupok <dejnekaartem223@gmail.com>
2023-10-10 02:32:46 +0300

    added validations

--- app/src/main/java/edu/kmaooad/capstone23/proffesors/handlers/AddProfessorToGroupHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/proffesors/handlers/AddProfessorToGroupHandler.java
@@ -1,7 +1,10 @@
 package edu.kmaooad.capstone23.proffesors.handlers;
 
 import edu.kmaooad.capstone23.common.CommandHandler;
+import edu.kmaooad.capstone23.common.ErrorCode;
 import edu.kmaooad.capstone23.common.Result;
+import edu.kmaooad.capstone23.groups.dal.Group;
+import edu.kmaooad.capstone23.groups.dal.GroupsRepository;
 import edu.kmaooad.capstone23.proffesors.commands.AddProfessorToGroup;
 import edu.kmaooad.capstone23.proffesors.dal.Proffesor;
 import edu.kmaooad.capstone23.proffesors.dal.ProffesorsRepository;
@@ -17,9 +20,19 @@ public class AddProfessorToGroupHandler implements CommandHandler<AddProfessorTo
     @Inject
     ProffesorsRepository proffesorsRepository;
 
+    @Inject
+    GroupsRepository groupRepositiory;
+
     @Override
     public Result<ProfessorAddedToGroup> handle(AddProfessorToGroup command) {
         Optional<Proffesor> maybeProfessor = proffesorsRepository.findByIdOptional(command.getProfessorId());
+        Optional<Group> maybeGroup = groupRepositiory.findByIdOptional(command.getGroupId());
+
+        if (maybeProfessor.isEmpty()) {
+            return new Result<>(ErrorCode.VALIDATION_FAILED, "Professor does not exist");
+        } else if (maybeGroup.isEmpty()) {
+            return new Result<>(ErrorCode.VALIDATION_FAILED, "Group does not exist");
+        }
 
         maybeProfessor.get().getGroups().add(command.getGroupId());
 
