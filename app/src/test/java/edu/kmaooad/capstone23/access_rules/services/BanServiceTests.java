0e49e8e302670553752b10ed79f8514f26056a78
Anna-Kudiakova <ann.kudiakova@gmail.com>
2023-10-09 01:17:48 -0400

    create test for banning non exisiting department

--- app/src/test/java/edu/kmaooad/capstone23/access_rules/services/BanServiceTests.java
+++ app/src/test/java/edu/kmaooad/capstone23/access_rules/services/BanServiceTests.java
@@ -124,6 +124,16 @@ public class BanServiceTests {
         Assertions.assertEquals(ErrorCode.VALIDATION_FAILED, result.getErrorCode());
     }
 
+    @Test
+    @DisplayName("Ban Non-existing Organisation")
+    public void banNonExistingOrganisation() {
+        ObjectId nonExistingOrgId = new ObjectId();
+        Result<EntityBanned> result = banService.banEntity(nonExistingOrgId, AccessRuleFromEntityType.Organisation);
+        
+        Assertions.assertFalse(result.isSuccess());
+        Assertions.assertEquals(ErrorCode.VALIDATION_FAILED, result.getErrorCode());
+    }
+
    
     private ObjectId createMember(){
         CreateBasicMember command = new CreateBasicMember();
