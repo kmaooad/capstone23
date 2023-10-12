d4ed8dc36d589ee17b2cd846426958cf266a6812
Danylo Nechyporchuk <75624164+CorTexDotMe@users.noreply.github.com>
2023-10-12 08:32:35 +0300

    Feature/test member repository - update member (#261)
    * delegate command validation to decorator
    
    * enforce User Email uniqueness
    
    * delete previously duplicated test data
    
    * change index to sparse
    
    * drop prev created indexes on test db
    
    * drop prev created indexes on test db
    
    * revert drop indexes
    
    * make Member a relation between User and Org
    
    * test member repo
    
    * remove unused event
    
    * Change test order
    
    start with insert tests and end with update tests
    
    * Test updateEntry check for member existence
    
    ---------
    
    Co-authored-by: Vsevolod Kalinbet <vsevolod180203@gmail.com>
    Co-authored-by: Vsevolod Kalinbet <75529126+VsevladHort@users.noreply.github.com>

--- app/src/test/java/edu/kmaooad/capstone23/orgs/members/repositories/MembersRepositoryTest.java
+++ app/src/test/java/edu/kmaooad/capstone23/orgs/members/repositories/MembersRepositoryTest.java
@@ -3,6 +3,7 @@ package edu.kmaooad.capstone23.orgs.members.repositories;
 import com.mongodb.MongoException;
 import edu.kmaooad.capstone23.members.dal.Member;
 import edu.kmaooad.capstone23.members.dal.MembersRepository;
+import edu.kmaooad.capstone23.members.exceptions.MemberNotFoundException;
 import edu.kmaooad.capstone23.members.exceptions.UniquenessViolationException;
 import edu.kmaooad.capstone23.orgs.dal.Org;
 import edu.kmaooad.capstone23.orgs.dal.OrgsRepository;
@@ -75,5 +76,32 @@ public class MembersRepositoryTest extends TestWithDbClearance {
         newMember.userId = setUpMember.userId;
 
         assertThrows(MongoException.class, () -> membersRepository.persist(newMember));
+    }
+
+    @Test
+    public void testUpdateToHaveWithDuplicatedUserAndOrgId() {
+        Member newMember = new Member();
+        newMember.orgId = setUpMember.orgId;
+        newMember.userId = new ObjectId();
+
+        assertDoesNotThrow(() -> {
+            membersRepository.insert(newMember);
+        });
+
+        newMember.userId = setUpMember.userId;
+
+        assertThrows(UniquenessViolationException.class, () -> membersRepository.updateEntry(newMember));
+    }
+
+    @Test
+    public void testUpdateNonExistentMember() {
+        var user = userRepository.insert(UserMocks.validUser());
+
+        Member member = new Member();
+        member.id = new ObjectId();
+        member.orgId = setUpMember.orgId;
+        member.userId = user.id;
+
+        assertThrows(MemberNotFoundException.class, () -> membersRepository.updateEntry(member));
     }
 }
