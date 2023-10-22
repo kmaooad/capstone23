068cdcfdfa03797284f56a3a61351a27ae729bda
annazhelizniak <anna.zhelizniak08@gmail.com>
2023-10-22 12:30:35 +0300

    add find by id to service and replace repository dependency in handler

--- app/src/main/java/edu/kmaooad/capstone23/jobs/service/JobService.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/service/JobService.java
@@ -3,7 +3,10 @@ package edu.kmaooad.capstone23.jobs.service;
 import edu.kmaooad.capstone23.jobs.dal.Job;
 import org.bson.types.ObjectId;
 
+import java.util.Optional;
+
 public interface JobService {
+    Optional<Job> findJobById(ObjectId id);
     Job insert(Job job);
     void delete(Job job);
 }
