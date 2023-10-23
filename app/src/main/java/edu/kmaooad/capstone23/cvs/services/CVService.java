f167730ddf683d554971971531945ceaeac7a44f
Anastasia Zolotar <anazolotar28@gmail.com>
2023-10-23 21:40:55 +0300

    refactor ReadCVHandler

--- app/src/main/java/edu/kmaooad/capstone23/cvs/services/CVService.java
+++ app/src/main/java/edu/kmaooad/capstone23/cvs/services/CVService.java
@@ -2,6 +2,7 @@ package edu.kmaooad.capstone23.cvs.services;
 
 import edu.kmaooad.capstone23.cvs.dal.CV;
 import edu.kmaooad.capstone23.cvs.dal.CVRepository;
+import io.quarkus.mongodb.panache.PanacheQuery;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.bson.types.ObjectId;
@@ -23,6 +24,10 @@ public class CVService {
 
     public CV findById(ObjectId id){
         return cvRepository.findById(id);
+    }
+
+    public PanacheQuery<CV> find(String query, Object... params) {
+        return cvRepository.find(query, params);
     }
 
 
