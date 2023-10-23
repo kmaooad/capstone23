21e97aa1e219d90d2b618e264d0c78303ccd7bf3
kkpagaev <kkpagaev@gmail.com>
2023-10-23 18:35:51 +0300

    refactor(communication): removed repo from handler in CreateChatHandler

--- app/src/main/java/edu/kmaooad/capstone23/communication/handlers/CreateChatHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/communication/handlers/CreateChatHandler.java
@@ -4,15 +4,15 @@ import edu.kmaooad.capstone23.common.CommandHandler;
 import edu.kmaooad.capstone23.common.Result;
 import edu.kmaooad.capstone23.communication.commands.CreateChat;
 import edu.kmaooad.capstone23.communication.dal.entities.Chat;
-import edu.kmaooad.capstone23.communication.dal.repositories.ChatRepository;
 import edu.kmaooad.capstone23.communication.events.ChatCreated;
+import edu.kmaooad.capstone23.communication.services.ChatService;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 
 @RequestScoped
 public class CreateChatHandler implements CommandHandler<CreateChat, ChatCreated> {
   @Inject
-  ChatRepository chatRepository;
+  ChatService chatService;
 
   private Chat chat;
 
@@ -20,9 +20,9 @@ public class CreateChatHandler implements CommandHandler<CreateChat, ChatCreated
   public Result<ChatCreated> handle(CreateChat command) {
     initChat(command);
 
-    chatRepository.insert(chat);
+    var insertedChat = chatService.insert(chat);
 
-    ChatCreated createdChat = new ChatCreated(chat.id.toHexString());
+    ChatCreated createdChat = new ChatCreated(insertedChat.id.toHexString());
 
     return new Result<ChatCreated>(createdChat);
   }
