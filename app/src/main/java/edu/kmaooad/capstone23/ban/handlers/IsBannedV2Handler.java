b084556fb122bcd906a93d72ed6d0f55e5a66910
Oleg Syniakevych <olegsyniakevych@gmail.com>
2023-10-23 19:26:30 +0300

    fix errors from main

--- app/src/main/java/edu/kmaooad/capstone23/ban/handlers/IsBannedV2Handler.java
+++ app/src/main/java/edu/kmaooad/capstone23/ban/handlers/IsBannedV2Handler.java
@@ -4,6 +4,7 @@ import edu.kmaooad.capstone23.ban.commands.IsEntityBanned;
 import edu.kmaooad.capstone23.ban.commands.IsEntityBannedV2;
 import edu.kmaooad.capstone23.ban.dal.BannedEntityType;
 import edu.kmaooad.capstone23.ban.events.EntityIsBanned;
+import edu.kmaooad.capstone23.ban.events.EntityIsBannedV2;
 import edu.kmaooad.capstone23.common.CommandHandler;
 import edu.kmaooad.capstone23.common.Result;
 import jakarta.enterprise.context.RequestScoped;
@@ -13,19 +14,25 @@ import org.bson.types.ObjectId;
 import java.util.Optional;
 
 @RequestScoped
-public class IsBannedV2Handler implements CommandHandler<IsEntityBannedV2, EntityIsBanned> {
+public class IsBannedV2Handler implements CommandHandler<IsEntityBannedV2, EntityIsBannedV2> {
 
     @Inject
     CommandHandler<IsEntityBanned, EntityIsBanned> isBannedHandler;
 
     @Override
-    public Result<EntityIsBanned> handle(IsEntityBannedV2 command) {
+    public Result<EntityIsBannedV2> handle(IsEntityBannedV2 command) {
         var entityType = getBannedType(command.getEntityType());
         if(entityType.isEmpty())
-            return new Result<>(new EntityIsBanned(true));
+            return new Result<>(new EntityIsBannedV2(true));
 
         var entityId = new ObjectId(command.getEntityId());
-        return isBannedHandler.handle(new IsEntityBanned(entityId, entityType.get().name()));
+        Result<EntityIsBanned> res = isBannedHandler.handle(new IsEntityBanned(entityId, entityType.get().name()));
+
+        if(res.isSuccess()) {
+            return new Result<>(new EntityIsBannedV2(res.getValue().value()));
+        } else {
+            return new Result<>(res.getErrorCode(), res.getMessage());
+        }
     }
 
     private Optional<BannedEntityType> getBannedType(String entityType) {
