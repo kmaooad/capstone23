69911436a44849b3337a670b886b40a63990e2e6
Anastasia Zolotar <anazolotar28@gmail.com>
2023-10-23 21:30:15 +0300

    refactor DeleteCVHandler

--- app/src/main/java/edu/kmaooad/capstone23/cvs/services/CVService.java
+++ app/src/main/java/edu/kmaooad/capstone23/cvs/services/CVService.java
@@ -4,6 +4,7 @@ import edu.kmaooad.capstone23.cvs.dal.CV;
 import edu.kmaooad.capstone23.cvs.dal.CVRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.bson.types.ObjectId;
 
 @ApplicationScoped
 public class CVService {
@@ -11,9 +12,17 @@ public class CVService {
     @Inject
     CVRepository cvRepository;
 
-    public CV create(CV cv){
+    public CV create(CV cv) {
         cvRepository.persist(cv);
         return cv;
+    }
+
+    public void delete(CV cv) {
+        cvRepository.delete(cv);
+    }
+
+    public CV findById(ObjectId id){
+        return cvRepository.findById(id);
     }
 
 
