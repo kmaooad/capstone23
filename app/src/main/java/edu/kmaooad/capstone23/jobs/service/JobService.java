421fdd780ac63cfa388f73c18df3e4527a49b9d0
annazhelizniak <84460049+annazhelizniak@users.noreply.github.com>
2023-10-24 08:44:30 +0300

    refactor delete job, remove dependency (#340)
    * event for delete relation job to activity
    
    * Add command
    
    * Add handler
    
    * event handler test for delete relation job to activity
    
    * controller for delete relation job to activity
    
    * Add test with valid and not valid job
    
    * add controller test for delete relation job to activity. Tests run successfully
    
    * event for delete relation job to competences.
    
    * event for delete relation job to competences.
    
    * event for delete relation job to activity
    
    * add controller
    
    * Feature/experts department interaction assign (#85)
    
    * department entity assigned to expert entity
    
    * Department assignment to expert + basic controller and handler tests
    
    * test coverage
    
    * organisation name changed
    
    ---------
    
    Co-authored-by: Liubomyr <liubomyr.stepanenko@ukma.edu.ua>
    
    * add command
    
    * add handler
    
    * handler test for delete relation job to competences.
    
    * fix error in test
    
    * event for assign activity to group
    
    * add command
    
    * handler for assign activity to group
    
    * final version of handler
    
    * add tests
    
    * add tests
    
    * test runs successfully
    
    * add test case for adding the same instance
    
    * test for handler assign activity to group runs successfully
    
    * add test case for not existing activity
    
    * check errors
    
    * test for handler assign activity to group for non-existed activity runs successfully
    
    * controller assign activity to group
    
    * add tests for existing and not existing groups
    
    * controller tests runs successfully
    
    * add test case of not existing activity
    
    * handler test for relate job to activities runs successfully
    
    * add test case of duplicates
    
    * handler test for relate job to activities, where duplicated activities runs successfully
    
    * add command
    
    * event for unassign activity to group
    
    * add tests
    
    * handler for unassign activity to group
    
    * handler test runs successfully for unassign activity to group
    
    * Update UnassignGroupToActivitiesHandler.java
    
    * add command
    
    * handler for unassign activity to group
    
    * add tests
    
    * handler test runs successfully for unassign activity to group
    
    * handler correct errors for unassign activity to group
    
    * add controller
    
    * controller test runs successfully for unassign activity to group
    
    * add check whether group exists
    
    * add check test to handler
    
    * tests run successfully for unassign activity to group, not existing group
    
    * add check tests to handler
    
    * tests run successfully for unassign activity to group, not existing activity
    
    * add check whether competence exists
    
    * add test to handler
    
    * tests run successfully for relate job to competences, not existing competences
    
    * check mistake in enum
    
    * interface for Job
    
    * add job service implementation
    
    * handler improving
    
    * job delete improvement
    
    * add find by id to service and replace repository dependency in handler
    
    ---------
    
    Co-authored-by: miksasha <miksasha2003@gmail.com>
    Co-authored-by: Liubomyr Stepanenko <98842543+liubcheck@users.noreply.github.com>
    Co-authored-by: Liubomyr <liubomyr.stepanenko@ukma.edu.ua>

--- app/src/main/java/edu/kmaooad/capstone23/jobs/service/JobService.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/service/JobService.java
@@ -3,6 +3,11 @@ package edu.kmaooad.capstone23.jobs.service;
 import edu.kmaooad.capstone23.jobs.dal.Job;
 import org.bson.types.ObjectId;
 
+
+import java.util.Optional;
+
 public interface JobService {
+    Optional<Job> findJobById(ObjectId id);
     Job insert(Job job);
+    void delete(Job job);
 }
--- app/src/main/java/edu/kmaooad/capstone23/jobs/service/JobServiceImpl.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/service/JobServiceImpl.java
@@ -3,12 +3,26 @@ package edu.kmaooad.capstone23.jobs.service;
 import edu.kmaooad.capstone23.jobs.dal.Job;
 import edu.kmaooad.capstone23.jobs.dal.JobRepository;
 import jakarta.inject.Inject;
+import org.bson.types.ObjectId;
+
+import java.util.Optional;
+
 
 public class JobServiceImpl implements JobService{
     @Inject
     private JobRepository jobRepository;
+
+    @Override
+    public Optional<Job> findJobById(ObjectId id) {
+        return jobRepository.findByIdOptional(id);
+    }
+
     @Override
     public Job insert(Job job) {
         return jobRepository.insert(job);
+    }
+    @Override
+    public void delete(Job job) {
+        jobRepository.delete(job);
     }
 }
