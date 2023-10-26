aa6e81de3d3e521bd50e05d255215c9ca218c5a6
IvanBlyzniuk <84847629+IvanBlyzniuk@users.noreply.github.com>
2023-10-26 21:58:04 +0300

    Refactoring group_templates remove mongo dependency from update group template handler (#336)
    * + GroupTemplatesService
    + GroupTemplatesServiceImpl
    ~ CreateGroupTemplateHandler now uses GroupTemplatesService instead of mongo directly
    
    * ~ UpdateGroupTemplateHandler now doesn't use MongoDb dependency
    + findById method in GroupTemplatesService

--- app/src/main/java/edu/kmaooad/capstone23/group_templates/services/GroupTemplatesServiceImpl.java
+++ app/src/main/java/edu/kmaooad/capstone23/group_templates/services/GroupTemplatesServiceImpl.java
@@ -5,6 +5,7 @@ import edu.kmaooad.capstone23.group_templates.dal.GroupTemplatesRepository;
 import edu.kmaooad.capstone23.groups.dal.Group;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.bson.types.ObjectId;
 
 import java.util.List;
 
@@ -32,5 +33,10 @@ public class GroupTemplatesServiceImpl implements GroupTemplatesService {
     @Override
     public void update(GroupTemplate groupTemplate) {
         repository.update(groupTemplate);
+    }
+
+    @Override
+    public GroupTemplate findById(ObjectId objectId) {
+        return repository.findById(objectId);
     }
 }
