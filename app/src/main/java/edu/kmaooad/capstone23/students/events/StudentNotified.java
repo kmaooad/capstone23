86e9303cfaf2b5bcd1f502aa4d408e6590a78e1a
Iryna Melnyk <iryna2.melnyk@ukma.edu.ua>
2023-10-28 18:57:26 -0400

    convert StudentNotified.java to record

--- app/src/main/java/edu/kmaooad/capstone23/students/events/StudentNotified.java
+++ app/src/main/java/edu/kmaooad/capstone23/students/events/StudentNotified.java
@@ -2,14 +2,4 @@ package edu.kmaooad.capstone23.students.events;
 
 import org.bson.types.ObjectId;
 
-public class StudentNotified {
-    private ObjectId studentId;
-
-    public StudentNotified(ObjectId studentId) {
-        this.studentId = studentId;
-    }
-
-    public ObjectId getStudentId() {
-        return studentId;
-    }
-}
+public record StudentNotified(ObjectId studentId) {}
