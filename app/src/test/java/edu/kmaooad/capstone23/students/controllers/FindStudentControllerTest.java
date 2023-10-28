14c16ea16ed30fb5030c57ff39eace81c9e900cb
Ivan Dobrovolskyi <ivan.dobrovolskyi@ukma.edu.ua>
2023-10-28 12:23:39 -0400

    Feature/students find by name (#296)
    * create SearchBuilder and SearchParam
    
    * change query to protected string
    
    * add non null check for parameters
    
    * add test for search builder
    
    * add equals search param
    
    * add equals search param test
    
    * add prefix search param with test
    
    * add find student endpoint
    
    * add default page values
    
    * add controller test
    
    * add handler test

---
+++ app/src/test/java/edu/kmaooad/capstone23/students/controllers/FindStudentControllerTest.java
@@ -0,0 +1,85 @@
+package edu.kmaooad.capstone23.students.controllers;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+public class FindStudentControllerTest {
+
+    @BeforeEach
+    public void setup() {
+        given()
+                .multiPart("csvFile", new File("src/test/resources/students/find_init.csv"), "text/csv")
+                .when()
+                .post("/students/create_csv")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Find all students by default")
+    public void testFindAllStudents() {
+        given()
+                .contentType("application/json")
+                .body(Map.of())
+                .when()
+                .post("/search/students")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Find students by first name")
+    public void testFindStudentsByFirstName() {
+        Map<String, Object> jsonAsMap = new HashMap<>();
+        jsonAsMap.put("firstName", "Iva");
+
+        given()
+                .contentType("application/json")
+                .body(jsonAsMap)
+                .when()
+                .post("/search/students")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Find students by middle name")
+    public void testFindStudentsByMiddleName() {
+        Map<String, Object> jsonAsMap = new HashMap<>();
+        jsonAsMap.put("middleName", "Oleksandrovy");
+
+        given()
+                .contentType("application/json")
+                .body(jsonAsMap)
+                .when()
+                .post("/search/students")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Find students by last name")
+    public void testFindStudentsByLastName() {
+        Map<String, Object> jsonAsMap = new HashMap<>();
+        jsonAsMap.put("lastName", "Dobrovolskyiii");
+
+        given()
+                .contentType("application/json")
+                .body(jsonAsMap)
+                .when()
+                .post("/search/students")
+                .then()
+                .statusCode(200);
+    }
+
+}
