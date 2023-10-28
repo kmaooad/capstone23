32269d60b2f0452b91634da87a375c333396d483
Iryna Melnyk <iryna2.melnyk@ukma.edu.ua>
2023-10-28 19:01:57 -0400

    convert StudentsCreated.java to record

--- app/src/main/java/edu/kmaooad/capstone23/students/events/StudentsCreated.java
+++ app/src/main/java/edu/kmaooad/capstone23/students/events/StudentsCreated.java
@@ -2,15 +2,5 @@ package edu.kmaooad.capstone23.students.events;
 
 import java.util.List;
 
-public class StudentsCreated {
-    private List<StudentCreated> students;
-
-    public StudentsCreated(List<StudentCreated> students) {
-        this.students = students;
-    }
-
-    public List<StudentCreated> getStudents() {
-        return students;
-    }
-
+public record StudentsCreated (List<StudentCreated> students){
 }
