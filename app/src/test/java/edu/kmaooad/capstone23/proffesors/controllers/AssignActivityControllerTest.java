e80da675d40fa27440541009323e73db0f581007
ElizabethChernova <elizabethchernova@gmail.com>
2023-10-30 20:35:18 +0200

    Create AssignActivityControllerTest.java

---
+++ app/src/test/java/edu/kmaooad/capstone23/proffesors/controllers/AssignActivityControllerTest.java
@@ -0,0 +1,37 @@
+package edu.kmaooad.capstone23.proffesors.controllers;
+
+import edu.kmaooad.capstone23.cvs.dal.CV;
+import edu.kmaooad.capstone23.proffesors.dal.Proffesor;
+import edu.kmaooad.capstone23.proffesors.dal.ProffesorsRepository;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class AssignActivityControllerTest {
+
+        @Inject
+        ProffesorsRepository proffesorsRepository;
+
+        @Test
+        @DisplayName("Update CV: valid values")
+        public void updateCV_AndGetStatus200() {
+            Proffesor proffesor = new Proffesor();
+            proffesorsRepository.persist(proffesor);
+
+            Map<String, Object> allValuesUpdate = new HashMap<>();
+            allValuesUpdate.put("cvId", proffesor.id.toString());
+
+            given().contentType("application/json")
+                    .body(allValuesUpdate)
+                    .when()
+                    .post("/proffesor/assignActivity")
+                    .then()
+                    .statusCode(200);
+        }
+}
