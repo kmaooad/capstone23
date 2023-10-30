88bb3ee3f15c7c7c591fe11b4a8c8cb6b0c5ece7
Illia Shevchyk <illia.shevchyk@ukma.edu.ua>
2023-10-30 21:59:43 +0100

    Refactor delete expert handler

--- app/src/main/java/edu/kmaooad/capstone23/experts/handlers/CreateInvitationLinkHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/experts/handlers/CreateInvitationLinkHandler.java
@@ -4,7 +4,6 @@ import edu.kmaooad.capstone23.common.CommandHandler;
 import edu.kmaooad.capstone23.common.Result;
 import edu.kmaooad.capstone23.experts.commands.CreateInvitationLink;
 import edu.kmaooad.capstone23.experts.dal.ExpertInvitation;
-import edu.kmaooad.capstone23.experts.dal.ExpertInvitationRepository;
 import edu.kmaooad.capstone23.experts.events.InvitationLinkCreated;
 import edu.kmaooad.capstone23.experts.service.ExpertInvitationMailService;
 import edu.kmaooad.capstone23.experts.service.ExpertService;
@@ -35,7 +34,7 @@ public class CreateInvitationLinkHandler implements CommandHandler<CreateInvitat
             return new Result<>(VALIDATION_FAILED, "Wrong organisation name");
         }
 
-        expertService.persist(expertInvitation);
+        expertService.insertInvitationLink(expertInvitation);
         var invitationLink = expertService.createInvitationLink(expertInvitation.id);
         mailService.sendInvitationLink(expertInvitation.email, invitationLink);
         return new Result<>(new InvitationLinkCreated(invitationLink));
