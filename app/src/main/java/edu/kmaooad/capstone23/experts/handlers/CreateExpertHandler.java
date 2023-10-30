9ddbba009d2686ed286c23fed4fe4610c1c39ad5
Illia Shevchyk <illia.shevchyk@ukma.edu.ua>
2023-10-30 21:55:34 +0100

    Refactor create/update expert handler

--- app/src/main/java/edu/kmaooad/capstone23/experts/handlers/CreateExpertHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/experts/handlers/CreateExpertHandler.java
@@ -7,6 +7,7 @@ import edu.kmaooad.capstone23.experts.commands.CreateExpert;
 import edu.kmaooad.capstone23.experts.dal.Expert;
 import edu.kmaooad.capstone23.experts.dal.ExpertsRepository;
 import edu.kmaooad.capstone23.experts.events.ExpertCreated;
+import edu.kmaooad.capstone23.experts.service.ExpertService;
 import edu.kmaooad.capstone23.orgs.dal.OrgsRepository;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
@@ -15,7 +16,7 @@ import jakarta.inject.Inject;
 public class CreateExpertHandler implements CommandHandler<CreateExpert, ExpertCreated> {
 
     @Inject
-    private ExpertsRepository expertsRepository;
+    private ExpertService expertService;
     @Inject
     private OrgsRepository orgsRepository;
 
@@ -29,7 +30,7 @@ public class CreateExpertHandler implements CommandHandler<CreateExpert, ExpertC
             return new Result<>(ErrorCode.NOT_FOUND, "Organisation not found");
         }
 
-        expertsRepository.insert(expert);
+        expertService.insertExpert(expert);
 
         ExpertCreated result = new ExpertCreated(expert.id.toString(), expert.org);
 
