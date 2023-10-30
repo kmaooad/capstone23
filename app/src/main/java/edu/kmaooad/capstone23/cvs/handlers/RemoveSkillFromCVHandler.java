3f09453bdb719fc84c859b6a9569e64ea9cab719
Anastasia Zolotar <anazolotar28@gmail.com>
2023-10-30 09:34:53 +0200

    refactor RemoveSkillFromCVHandler

--- app/src/main/java/edu/kmaooad/capstone23/cvs/handlers/RemoveSkillFromCVHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/cvs/handlers/RemoveSkillFromCVHandler.java
@@ -3,38 +3,35 @@ package edu.kmaooad.capstone23.cvs.handlers;
 import edu.kmaooad.capstone23.common.CommandHandler;
 import edu.kmaooad.capstone23.common.ErrorCode;
 import edu.kmaooad.capstone23.common.Result;
-import edu.kmaooad.capstone23.cvs.commands.AddSkillToCV;
 import edu.kmaooad.capstone23.cvs.commands.RemoveSkillFromCV;
 import edu.kmaooad.capstone23.cvs.dal.CV;
-import edu.kmaooad.capstone23.cvs.dal.CVRepository;
 import edu.kmaooad.capstone23.cvs.events.CVUpdated;
+import edu.kmaooad.capstone23.cvs.services.CVService;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
-import org.bson.types.ObjectId;
 
-import java.util.Optional;
 
 @RequestScoped
 public class RemoveSkillFromCVHandler implements CommandHandler<RemoveSkillFromCV, CVUpdated>  {
 
     @Inject
-    CVRepository cvRepository;
+    CVService cvService;
 
     @Override
     public Result<CVUpdated> handle(RemoveSkillFromCV command) {
         if (command == null || command.getSkillId() == null || command.getCvId() == null) {
             return new Result<>(ErrorCode.VALIDATION_FAILED, "Illegal command sent");
         }
-        Optional<CV> cv = cvRepository.findByIdOptional(command.getCvId());
+        CV cv = cvService.findById(command.getCvId());
 
-        if (cv.isEmpty()) {
+        if (cv == null) {
             return new Result<>(ErrorCode.VALIDATION_FAILED, "This cv does not exist");
         }
 
-        cv.get().skills.remove(command.getSkillId());
-        cvRepository.update(cv.get());
+        cv.skills.remove(command.getSkillId());
+        cvService.update(cv);
 
-        CVUpdated result = new CVUpdated(cv.get().id);
+        CVUpdated result = new CVUpdated(cv.id);
         return new Result<>(result);
     }
 }
