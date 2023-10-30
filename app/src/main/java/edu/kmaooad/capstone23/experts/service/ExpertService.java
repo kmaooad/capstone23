185b2c8c96f8645341779e637a88c88d9ca610aa
Illia Shevchyk <illia.shevchyk@ukma.edu.ua>
2023-10-30 21:46:07 +0100

    Add expert service interface

--- app/src/main/java/edu/kmaooad/capstone23/experts/service/ExpertService.java
+++ app/src/main/java/edu/kmaooad/capstone23/experts/service/ExpertService.java
@@ -1,20 +1,15 @@
 package edu.kmaooad.capstone23.experts.service;
 
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.core.UriBuilder;
+import edu.kmaooad.capstone23.experts.dal.ExpertInvitation;
 import org.bson.types.ObjectId;
 
-@ApplicationScoped
-public class ExpertService {
+import java.util.Optional;
 
-    public static final String ACCEPT_INVITATION_ENDPOINT = "/experts/invitation/accept";
+public interface ExpertService {
+    String ACCEPT_INVITATION_ENDPOINT = "/experts/invitation/accept";
 
-    public String createInvitationLink(ObjectId invitationId) {
-        String host = System.getProperty("quarkus.http.host");
-        var invitationLink = UriBuilder.fromUri(host);
-        invitationLink.path(ACCEPT_INVITATION_ENDPOINT);
-        invitationLink.queryParam("id", invitationId.toString());
-        return invitationLink.build().toString();
-    }
+    String createInvitationLink(ObjectId invitationId);
+    Optional<ExpertInvitation> findInvitationById(ObjectId id);
+    void persist(ExpertInvitation expertInvitation);
 
 }
