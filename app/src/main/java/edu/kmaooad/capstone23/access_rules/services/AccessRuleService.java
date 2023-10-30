6e537e49bdc7e8f59b19ff1382332ed4760d089b
Heorhii Sanchenko <jorhesancho@gmail.com>
2023-10-30 21:35:59 +0200

    refactor: add access rule service to create access rule handler

---
+++ app/src/main/java/edu/kmaooad/capstone23/access_rules/services/AccessRuleService.java
@@ -0,0 +1,17 @@
+package edu.kmaooad.capstone23.access_rules.services;
+
+import edu.kmaooad.capstone23.access_rules.dal.AccessRule;
+import edu.kmaooad.capstone23.access_rules.dal.AccessRuleFromEntityType;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
+public interface AccessRuleService {
+  AccessRule insert(AccessRule accessRule);
+
+  void deleteRule(ObjectId id);
+
+  List<AccessRule> findByEntityIdAndType(ObjectId entityId, AccessRuleFromEntityType entityType);
+
+  void ban(ObjectId entityId, AccessRuleFromEntityType entityType);
+}
