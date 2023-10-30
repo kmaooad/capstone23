851a21bd04cbe9931959942e9e6ce4ca7607aaf2
Liubomyr <liubomyr.stepanenko@ukma.edu.ua>
2023-10-30 23:43:59 +0100

    Implementing Expert DTOs into assigning and creation handlers

--- app/src/main/java/edu/kmaooad/capstone23/experts/handlers/AssignExpertToProjectHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/experts/handlers/AssignExpertToProjectHandler.java
@@ -6,15 +6,15 @@ import edu.kmaooad.capstone23.common.Result;
 import edu.kmaooad.capstone23.competences.dal.Project;
 import edu.kmaooad.capstone23.competences.dal.ProjsRepository;
 import edu.kmaooad.capstone23.experts.commands.AssignExpertToProject;
-import edu.kmaooad.capstone23.experts.dal.Expert;
-import edu.kmaooad.capstone23.experts.dal.ExpertsRepository;
+import edu.kmaooad.capstone23.experts.dal.dto.ExpertRequestDto;
+import edu.kmaooad.capstone23.experts.dal.dto.ExpertResponseDto;
 import edu.kmaooad.capstone23.experts.events.ExpertAssignedToProject;
+import edu.kmaooad.capstone23.experts.service.ExpertMapper;
 import edu.kmaooad.capstone23.experts.service.ExpertService;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
-import org.bson.types.ObjectId;
-
 import java.util.List;
+import org.bson.types.ObjectId;
 
 @RequestScoped
 public class AssignExpertToProjectHandler implements CommandHandler<AssignExpertToProject, ExpertAssignedToProject> {
@@ -23,32 +23,46 @@ public class AssignExpertToProjectHandler implements CommandHandler<AssignExpert
     private ProjsRepository projsRepository;
     @Inject
     ExpertService expertService;
+    ExpertMapper expertMapper;
 
     public Result<ExpertAssignedToProject> handle(AssignExpertToProject command) {
+        expertMapper = new ExpertMapper();
+
         ObjectId projId = command.getProjectId();
         ObjectId expertId = command.getExpertId();
         Project proj = projsRepository.findById(projId);
-        Expert expert = expertService.findById(expertId);
+        ExpertResponseDto expertResponseDto =
+                expertMapper.toDto(expertService.findById(expertId));
 
         if (proj == null) {
             return new Result<>(ErrorCode.NOT_FOUND, "Project not found");
         }
 
-        if (expert == null) {
+        if (expertResponseDto == null) {
             return new Result<>(ErrorCode.NOT_FOUND, "Expert not found");
         }
 
-        if (expert.projects != null) {
-            if (expert.projects.stream().anyMatch(p -> p.id.equals(projId))) {
+        ExpertRequestDto expertRequestDto = new ExpertRequestDto();
+        expertRequestDto.setName(expertResponseDto.getName());
+        expertRequestDto.setOrgId(expertResponseDto.getOrg().id.toHexString());
+        expertRequestDto.setDepartmentIds(expertResponseDto.getDepartments().stream()
+                .map(d -> d.id.toHexString())
+                .toList());
+        expertRequestDto.setProjectsIds(expertResponseDto.getProjects().stream()
+                .map(p -> p.id.toHexString())
+                .toList());
+
+        if (expertResponseDto.getProjects() != null) {
+            if (expertResponseDto.getProjects().stream().anyMatch(p -> p.id.equals(projId))) {
                 return new Result<>(ErrorCode.CONFLICT, "Expert is already assigned to project");
             }
-            expert.projects.add(proj);
+            expertRequestDto.getProjectsIds().add(proj.id.toHexString());
         } else {
-            expert.projects = List.of(proj);
+            expertRequestDto.setProjectsIds(List.of(proj.id.toHexString()));
         }
 
-        expertService.modify(expert);
+        expertService.modify(expertMapper.toModel(expertRequestDto));
 
-        return new Result<ExpertAssignedToProject>(new ExpertAssignedToProject(expert.id.toString()));
+        return new Result<ExpertAssignedToProject>(new ExpertAssignedToProject(expertResponseDto.getId().toString()));
     }
 }
--- app/src/main/java/edu/kmaooad/capstone23/experts/handlers/CreateExpertHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/experts/handlers/CreateExpertHandler.java
@@ -4,9 +4,10 @@ import edu.kmaooad.capstone23.common.CommandHandler;
 import edu.kmaooad.capstone23.common.ErrorCode;
 import edu.kmaooad.capstone23.common.Result;
 import edu.kmaooad.capstone23.experts.commands.CreateExpert;
-import edu.kmaooad.capstone23.experts.dal.Expert;
-import edu.kmaooad.capstone23.experts.dal.ExpertsRepository;
+import edu.kmaooad.capstone23.experts.dal.dto.ExpertRequestDto;
+import edu.kmaooad.capstone23.experts.dal.dto.ExpertResponseDto;
 import edu.kmaooad.capstone23.experts.events.ExpertCreated;
+import edu.kmaooad.capstone23.experts.service.ExpertMapper;
 import edu.kmaooad.capstone23.experts.service.ExpertService;
 import edu.kmaooad.capstone23.orgs.dal.OrgsRepository;
 import jakarta.enterprise.context.RequestScoped;
@@ -19,20 +20,24 @@ public class CreateExpertHandler implements CommandHandler<CreateExpert, ExpertC
     private ExpertService expertService;
     @Inject
     private OrgsRepository orgsRepository;
+    ExpertMapper expertMapper;
 
     public Result<ExpertCreated> handle(CreateExpert command) {
+        expertMapper = new ExpertMapper();
 
-        Expert expert = new Expert();
-        expert.name = command.getExpertName();
-        expert.org = orgsRepository.findByName(command.getOrgName());
+        ExpertRequestDto expertRequestDto = new ExpertRequestDto();
+        expertRequestDto.setName(command.getExpertName());
+        expertRequestDto.setOrgId(orgsRepository.findByName(command.getOrgName()).id.toHexString());
 
-        if (expert.org == null) {
+        if (expertRequestDto.getOrgId() == null) {
             return new Result<>(ErrorCode.NOT_FOUND, "Organisation not found");
         }
 
-        expertService.insert(expert);
+        ExpertResponseDto expertResponseDto =
+                expertMapper.toDto(expertService.insert(expertMapper.toModel(expertRequestDto)));
 
-        ExpertCreated result = new ExpertCreated(expert.id.toString(), expert.org);
+        ExpertCreated result = new ExpertCreated(expertResponseDto.getId().toString(),
+                expertResponseDto.getOrg());
 
         return new Result<ExpertCreated>(result);
     }
