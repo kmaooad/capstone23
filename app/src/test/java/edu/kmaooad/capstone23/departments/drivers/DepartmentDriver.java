b48ab37441ae0b33d589646fed0a21292fe09839
VasylPupok <80713961+VasylPupok@users.noreply.github.com>
2023-10-31 05:29:51 +0200

    Fixes
    added some annotations so this stuff compiles

---
+++ app/src/test/java/edu/kmaooad/capstone23/departments/drivers/DepartmentDriver.java
@@ -0,0 +1,18 @@
+package edu.kmaooad.capstone23.departments.drivers;
+
+import edu.kmaooad.capstone23.departments.dal.Department;
+import edu.kmaooad.capstone23.departments.services.DepartmentService;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+public class DepartmentDriver {
+
+    @Inject
+    DepartmentService departmentService;
+
+    public Department createDepartment() {
+        departmentService.deleteAllDepartments();
+        return departmentService.createDepartment("Department to Delete", "This is a department to be deleted", "NaUKMA");
+    }
+}
