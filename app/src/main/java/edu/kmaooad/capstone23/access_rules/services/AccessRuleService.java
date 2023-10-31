8c9541a09d3a0854f7519c0871f6dcd79e48f0c1
dmparnak <dmparnak@gmail.com>
2023-10-31 21:46:25 +0200

    Refactor Create Access Rule

---
+++ app/src/main/java/edu/kmaooad/capstone23/access_rules/services/AccessRuleService.java
@@ -0,0 +1,11 @@
+package edu.kmaooad.capstone23.access_rules.services;
+
+import edu.kmaooad.capstone23.access_rules.dal.AccessRule;
+import org.bson.types.ObjectId;
+
+public interface AccessRuleService {
+    public AccessRule insert(AccessRule accessRule);
+    public AccessRule findById(ObjectId id);
+    public AccessRule update(AccessRule accessRule);
+    public void deleteAccessRule(AccessRule accessRule);
+}
