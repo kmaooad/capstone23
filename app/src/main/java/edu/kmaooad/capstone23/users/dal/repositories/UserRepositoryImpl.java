7f13c3b09f510fb72d3f30f8e534ef561da1ce17
dmytro-pelovych <dmytro.pelovych@gmail.com>
2023-10-31 07:43:23 +0200

    Implement repository from interface

--- app/src/main/java/edu/kmaooad/capstone23/users/dal/repositories/UserRepositoryImpl.java
+++ app/src/main/java/edu/kmaooad/capstone23/users/dal/repositories/UserRepositoryImpl.java
@@ -1,7 +1,7 @@
 package edu.kmaooad.capstone23.users.dal.repositories;
 
 import edu.kmaooad.capstone23.users.dal.entities.User;
-import io.quarkus.mongodb.panache.PanacheMongoRepository;
+import edu.kmaooad.capstone23.users.interfaces.UserRepository;
 import io.quarkus.mongodb.panache.PanacheQuery;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.validation.constraints.Email;
@@ -10,25 +10,29 @@ import org.bson.types.ObjectId;
 import java.util.Optional;
 
 @ApplicationScoped
-public class UserRepository implements PanacheMongoRepository<User> {
+public class UserRepositoryImpl implements UserRepository {
+  @Override
   public Optional<User> findById(String id) {
     ObjectId parsedId = new ObjectId(id);
 
     return findByIdOptional(parsedId);
   }
 
+  @Override
   public Optional<User> findByEmail(@Email String email) {
     PanacheQuery<User> user = find("unique_email", email);
 
     return user.firstResultOptional();
   }
 
+  @Override
   public User insert(User user) {
     persist(user);
 
     return user;
   }
 
+  @Override
   public void deleteByEmail (String email) {
     delete("unique_email", email);
   }
