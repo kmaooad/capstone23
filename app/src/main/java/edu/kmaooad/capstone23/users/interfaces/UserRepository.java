f460063637b399b05dff37a7dbcd1080408bcb4f
dmytro-pelovych <dmytro.pelovych@gmail.com>
2023-10-31 07:42:39 +0200

    Create abstraction

---
+++ app/src/main/java/edu/kmaooad/capstone23/users/interfaces/UserRepository.java
@@ -0,0 +1,17 @@
+package edu.kmaooad.capstone23.users.interfaces;
+
+import edu.kmaooad.capstone23.users.dal.entities.User;
+import io.quarkus.mongodb.panache.PanacheMongoRepository;
+import jakarta.validation.constraints.Email;
+
+import java.util.Optional;
+
+public interface UserRepository extends PanacheMongoRepository<User> {
+  Optional<User> findById(String id);
+
+  Optional<User> findByEmail(@Email String email);
+
+  User insert(User user);
+
+  void deleteByEmail (String email);
+}
