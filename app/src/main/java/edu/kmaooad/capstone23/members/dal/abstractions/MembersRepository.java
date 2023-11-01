0b940f02770aa12deec632dd6aa99d1aa9dd968e
Vsevolod Kalinbet <vsevolod180203@gmail.com>
2023-11-01 21:22:10 +0200

    remove mongo repo dependency from GetMemberByEmailHandler

--- app/src/main/java/edu/kmaooad/capstone23/members/dal/abstractions/MembersRepository.java
+++ app/src/main/java/edu/kmaooad/capstone23/members/dal/abstractions/MembersRepository.java
@@ -3,6 +3,7 @@ package edu.kmaooad.capstone23.members.dal.abstractions;
 import edu.kmaooad.capstone23.members.commands.GetAllMembers;
 import edu.kmaooad.capstone23.members.commands.GetAllMembersByOrg;
 import edu.kmaooad.capstone23.members.dal.Member;
+import edu.kmaooad.capstone23.members.dto.UserDTO;
 import edu.kmaooad.capstone23.members.exceptions.MemberNotFoundException;
 import edu.kmaooad.capstone23.members.exceptions.UniquenessViolationException;
 
@@ -23,4 +24,5 @@ public interface MembersRepository {
     List<Member> getAllByOrg(GetAllMembersByOrg command);
 
     List<Member> getAll(GetAllMembers command);
+    List<Member> getByUser(UserDTO userDTO);
 }
