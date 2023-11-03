f198b39806c6aaaf322556edda81680494c813e6
annazhelizniak <anna.zhelizniak08@gmail.com>
2023-11-03 15:13:39 +0200

    add find child repositories function to service

--- app/src/main/java/edu/kmaooad/capstone23/competences/services/SkillService.java
+++ app/src/main/java/edu/kmaooad/capstone23/competences/services/SkillService.java
@@ -5,10 +5,12 @@ import edu.kmaooad.capstone23.competences.dal.SkillSet;
 import edu.kmaooad.capstone23.jobs.dal.Job;
 import org.bson.types.ObjectId;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SkillService {
     Optional<Skill> findById(ObjectId id);
     Skill insert(Skill skill);
     void delete(Skill skill);
+    List<Skill> findChildRepositories(ObjectId parentSkill);
 }
