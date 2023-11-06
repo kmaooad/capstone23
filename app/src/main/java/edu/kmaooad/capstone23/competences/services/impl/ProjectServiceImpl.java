39045c71fb791a597409afa64dee6a4113b0c3a6
kkpagaev <kkpagaev@gmail.com>
2023-11-06 18:06:53 +0200

    created ProjectService and ProjectServiceImpl

---
+++ app/src/main/java/edu/kmaooad/capstone23/competences/services/impl/ProjectServiceImpl.java
@@ -0,0 +1,7 @@
+package edu.kmaooad.capstone23.competences.services.impl;
+
+import edu.kmaooad.capstone23.competences.services.ProjectService;
+
+public class ProjectServiceImpl implements ProjectService {
+
+}
