439ccc5bd6bffe3e5da018ba301e82327a03130f
Heorhii Sanchenko <jorhesancho@gmail.com>
2023-11-06 21:41:08 +0200

    add mailer to mail service impl

--- app/src/main/java/edu/kmaooad/capstone23/mail/service/impl/MailServiceImpl.java
+++ app/src/main/java/edu/kmaooad/capstone23/mail/service/impl/MailServiceImpl.java
@@ -1,11 +1,21 @@
 package edu.kmaooad.capstone23.mail.service.impl;
 
 import edu.kmaooad.capstone23.mail.service.MailService;
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class MailServiceImpl implements MailService {
+  @Inject
+  Mailer mailer;
+
   public void sendEmail(String emailText, String orgEmail) {
-    System.out.println("Sending email to " + orgEmail + " with text: " + emailText);
+    Mail mail = new Mail()
+        .addTo(orgEmail)
+        .setText(emailText);
+
+    mailer.send(mail);
   }
 }
