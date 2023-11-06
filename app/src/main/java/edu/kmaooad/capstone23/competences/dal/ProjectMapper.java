630a39ff0380ccc7a7ef4c43253c1fe1634af9da
Liubomyr <liubomyr.stepanenko@ukma.edu.ua>
2023-11-06 21:51:18 +0100

    Create project DTO & model mapper

---
+++ app/src/main/java/edu/kmaooad/capstone23/competences/dal/ProjectMapper.java
@@ -0,0 +1,43 @@
+package edu.kmaooad.capstone23.competences.dal;
+
+import edu.kmaooad.capstone23.competences.dal.dto.ProjectRequestDto;
+import edu.kmaooad.capstone23.competences.dal.dto.ProjectResponseDto;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.bson.types.ObjectId;
+
+public class ProjectMapper {
+
+    public ProjectResponseDto toDto(Project project) {
+        if (project != null) {
+            ProjectResponseDto dto = new ProjectResponseDto();
+            dto.setId(project.id);
+            dto.setName(project.name);
+            dto.setDescription(project.description);
+            dto.setSkills(project.skills);
+            dto.setSkillSets(project.skillSets);
+            return dto;
+        }
+        return null;
+    }
+
+    public Project toModel(ProjectRequestDto dto) {
+        if (dto != null) {
+            Project project = new Project();
+            project.id = new ObjectId(dto.getId());
+            project.name = dto.getName();
+            project.description = dto.getDescription();
+            project.skills = convertToObjectIdList(dto.getSkills());
+            project.skillSets = convertToObjectIdList(dto.getSkillSets());
+            return project;
+        }
+        return null;
+    }
+
+    private List<ObjectId> convertToObjectIdList(List<String> stringIds) {
+        if (stringIds != null) {
+            return stringIds.stream().map(ObjectId::new).collect(Collectors.toList());
+        }
+        return null;
+    }
+}
