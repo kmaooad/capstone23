adc3f2bcacd368c830fbb3799943774ef36c4362
Heorhii Sanchenko <66861608+Jorge3129@users.noreply.github.com>
2023-11-06 20:51:06 +0100

    refactor: add access rule service to create access rule handler (#438)

---
+++ app/src/main/java/edu/kmaooad/capstone23/access_rules/services/AccessRuleServiceImpl.java
@@ -0,0 +1,38 @@
+package edu.kmaooad.capstone23.access_rules.services;
+
+import edu.kmaooad.capstone23.access_rules.dal.AccessRule;
+import edu.kmaooad.capstone23.access_rules.dal.AccessRuleFromEntityType;
+import edu.kmaooad.capstone23.access_rules.dal.AccessRuleRepository;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
+@RequestScoped
+public class AccessRuleServiceImpl implements AccessRuleService {
+
+  @Inject
+  private AccessRuleRepository accessRuleRepository;
+
+
+  @Override
+  public AccessRule insert(AccessRule accessRule) {
+    return this.accessRuleRepository.insert(accessRule);
+  }
+
+  @Override
+  public void deleteRule(ObjectId id) {
+    this.accessRuleRepository.deleteRule(id);
+  }
+
+  @Override
+  public List<AccessRule> findByEntityIdAndType(ObjectId entityId, AccessRuleFromEntityType entityType) {
+    return this.accessRuleRepository.findByEntityIdAndType(entityId, entityType);
+  }
+
+  @Override
+  public void ban(ObjectId entityId, AccessRuleFromEntityType entityType) {
+    this.accessRuleRepository.updateOnBan(entityId, entityType);
+  }
+}
