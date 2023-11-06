39dd60849160f8e4266007df5dd697b2b93a9f57
Fourier2718281828 <non-linear@ukr.net>
2023-11-07 01:58:46 +0200

    added: ProjectsRepository now implements Listable<Project>

--- app/src/main/java/edu/kmaooad/capstone23/competences/dal/ProjectsRepository.java
+++ app/src/main/java/edu/kmaooad/capstone23/competences/dal/ProjectsRepository.java
@@ -1,9 +1,11 @@
 package edu.kmaooad.capstone23.competences.dal;
 
 import java.util.Optional;
+
+import edu.kmaooad.capstone23.search.Listable;
 import org.bson.types.ObjectId;
 
-public interface ProjectsRepository {
+public interface ProjectsRepository extends Listable<Project> {
     Optional<Project> findByIdMaybe(ObjectId id);
     Project insertProject(Project project);
     Project findProjectById(String id);
