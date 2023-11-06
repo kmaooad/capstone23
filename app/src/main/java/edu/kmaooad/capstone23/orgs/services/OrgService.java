420fcfcac774d0208292bf0fdb568eab7aabc76e
Heorhii Sanchenko <jorhesancho@gmail.com>
2023-11-06 21:29:28 +0200

    fix org service access modifier

--- app/src/main/java/edu/kmaooad/capstone23/orgs/services/OrgService.java
+++ app/src/main/java/edu/kmaooad/capstone23/orgs/services/OrgService.java
@@ -2,7 +2,7 @@ package edu.kmaooad.capstone23.orgs.services;
 
 import edu.kmaooad.capstone23.orgs.dal.Org;
 
- interface OrgService {
+public interface OrgService {
      Org createOrg(String name, String description, String industry, String website, String emailDomain);
 
      Org getOrgByName(String name);
