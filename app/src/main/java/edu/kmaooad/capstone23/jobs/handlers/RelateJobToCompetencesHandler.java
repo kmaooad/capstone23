296f0f14889aec4e461f434db2cd336eae809120
kkpagaev <kkpagaev@gmail.com>
2023-11-06 18:49:22 +0200

    moved isJobRelatedToCompetence logic to a service

--- app/src/main/java/edu/kmaooad/capstone23/jobs/handlers/RelateJobToCompetencesHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/handlers/RelateJobToCompetencesHandler.java
@@ -3,11 +3,6 @@ package edu.kmaooad.capstone23.jobs.handlers;
 import edu.kmaooad.capstone23.common.CommandHandler;
 import edu.kmaooad.capstone23.common.ErrorCode;
 import edu.kmaooad.capstone23.common.Result;
-import edu.kmaooad.capstone23.competences.dal.*;
-import edu.kmaooad.capstone23.competences.services.ProjectService;
-import edu.kmaooad.capstone23.competences.services.SkillService;
-import edu.kmaooad.capstone23.competences.services.SkillSetService;
-import edu.kmaooad.capstone23.competences.services.TopicService;
 import edu.kmaooad.capstone23.jobs.commands.RelateJobToCompetences;
 import edu.kmaooad.capstone23.jobs.dal.Job;
 import edu.kmaooad.capstone23.jobs.events.CompetenceRelated;
@@ -22,28 +17,17 @@ public class RelateJobToCompetencesHandler implements CommandHandler<RelateJobTo
     @Inject
     private JobService jobService;
 
-    @Inject
-    private SkillService skillService;
-
-    @Inject
-    private ProjectService projectService;
-
-    @Inject
-    private TopicService topicService;
-
     @Override
     public Result<CompetenceRelated> handle(RelateJobToCompetences command) {
 
         Optional<Job> job = jobService.findJobById(command.getJobId());
-        if(job.isEmpty())
+        if(job.isEmpty()) {
             return new Result<>(ErrorCode.VALIDATION_FAILED, "This job was previously deleted or never existed");
+        }
 
-        Optional<Project> project = projectService.findByIdOptional(command.getCompetenceId());
-        Optional<Skill> skill = skillService.findByIdOptional(command.getCompetenceId());
-        Optional<Topic> topic = topicService.findByIdOptional(command.getCompetenceId());
-
-        if(project.isEmpty() && skill.isEmpty() && topic.isEmpty())
+        if(jobService.isJobRelatedToCompetence(command.getCompetenceId())) {
             return new Result<>(ErrorCode.VALIDATION_FAILED, "This competence was previously deleted or never existed");
+        }
 
         CompetenceRelated result = new CompetenceRelated(command.getJobId(), command.getCompetenceId());
 
