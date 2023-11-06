1d8b9ed59aabb56e4aea6b77142ecb71dcc1f984
Яна Буга <yana.buga129@gmail.com>
2023-11-06 22:29:22 +0200

    added CreateProffesorHandler

--- app/src/main/java/edu/kmaooad/capstone23/proffesors/dal/Proffesor.java
+++ app/src/main/java/edu/kmaooad/capstone23/proffesors/dal/Proffesor.java
@@ -34,5 +34,32 @@ public class Proffesor {
         this.preference = preference;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
 }
 
