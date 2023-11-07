2dc5e2ba7b18682dab0ff01a7ee5632d0c75e8a6
Annnanke <kirikanna3005@gmail.com>
2023-11-06 23:12:34 -0500

    Service created
    needs further update

--- app/src/main/java/edu/kmaooad/capstone23/orgs/services/OrgService.java
+++ app/src/main/java/edu/kmaooad/capstone23/orgs/services/OrgService.java
@@ -1,8 +1,10 @@
 package edu.kmaooad.capstone23.orgs.services;
 
 import edu.kmaooad.capstone23.orgs.dal.Org;
+import edu.kmaooad.capstone23.search.Listable;
 
-public interface OrgService {
+public interface OrgService extends Listable<Org> /*please implement this*/
+{
      Org createOrg(String name, String description, String industry, String website, String emailDomain);
 
      Org getOrgByName(String name);
