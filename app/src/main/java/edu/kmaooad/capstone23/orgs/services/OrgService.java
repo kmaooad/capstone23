3d09e7193a549178029c774a7c9d1f9c5d0abfc0
Anna Kirik <80839420+Annnanke@users.noreply.github.com>
2023-11-08 13:16:43 -0500

    Refactor/search/query course by orgs/invert orgs repository dependency (#568)
    * added: Listable interface - interface-contract according to DIP
    
    * refactored QueryEssenceHandler
    
    * refactored: QueryEssenceHandler field namings
    
    * Service created
    
    needs further update
    
    ---------
    
    Co-authored-by: Fourier2718281828 <non-linear@ukr.net>

--- app/src/main/java/edu/kmaooad/capstone23/orgs/services/OrgService.java
+++ app/src/main/java/edu/kmaooad/capstone23/orgs/services/OrgService.java
@@ -1,9 +1,11 @@
 package edu.kmaooad.capstone23.orgs.services;
 
 import edu.kmaooad.capstone23.orgs.dal.Org;
+import edu.kmaooad.capstone23.search.Listable;
 
-public interface OrgService {
-    public Org createOrg(String name, String description, String industry, String website, String emailDomain);
+public interface OrgService extends Listable<Org> /*please implement this*/
+{
+     Org createOrg(String name, String description, String industry, String website, String emailDomain);
 
     public Org getOrgByName(String name);
 
