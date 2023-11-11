6d91164b1632fd9ca20c4868b46f6fa9b4c5d307
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2023-11-11 11:21:50 +0200

    fix merge

--- app/src/main/java/edu/kmaooad/capstone23/jobs/service/JobService.java
+++ app/src/main/java/edu/kmaooad/capstone23/jobs/service/JobService.java
@@ -1,13 +1,12 @@
 package edu.kmaooad.capstone23.jobs.service;
 
 import edu.kmaooad.capstone23.jobs.dal.Job;
-import org.bson.types.ObjectId;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface JobService {
-    Optional<Job> findJobById(ObjectId id);
+    Optional<Job> findJobById(String id);
     List<Job> listAll();
     long count();
     Job insert(Job job);
