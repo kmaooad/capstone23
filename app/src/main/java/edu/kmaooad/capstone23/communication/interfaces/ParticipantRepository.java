5c1faf6b40030e6ef0ce299d4dff0d52040bd3a0
dmytro-pelovych <dmytro.pelovych@gmail.com>
2023-11-12 15:59:01 +0200

    Resolve rebase conflicts

---
+++ app/src/main/java/edu/kmaooad/capstone23/communication/interfaces/ParticipantRepository.java
@@ -0,0 +1,14 @@
+package edu.kmaooad.capstone23.communication.interfaces;
+
+import edu.kmaooad.capstone23.communication.dal.entities.Participant;
+import io.quarkus.mongodb.panache.PanacheMongoRepository;
+
+import java.util.List;
+
+public interface ParticipantRepository extends PanacheMongoRepository<Participant> {
+  List<Participant> findAllByChatId(String chatId);
+
+  List<Participant> findByChatAndUserIds(String chatId, String userId);
+
+  Participant insert(Participant participant);
+}
