1f12d82e0926ca648baf3303046e45d021fe5165
Heorhii Sanchenko <jorhesancho@gmail.com>
2023-11-13 19:41:40 +0200

    refactor skill deleted

--- app/src/main/java/edu/kmaooad/capstone23/competences/events/SkillDeleted.java
+++ app/src/main/java/edu/kmaooad/capstone23/competences/events/SkillDeleted.java
@@ -2,12 +2,10 @@ package edu.kmaooad.capstone23.competences.events;
 
 import edu.kmaooad.capstone23.competences.dal.Skill;
 
-import java.util.Optional;
-
 public class SkillDeleted {
     private Skill skill;
 
-    public SkillDeleted(Optional<Skill> skill) {
+    public SkillDeleted(Skill skill) {
         this.skill = skill;
     }
 
