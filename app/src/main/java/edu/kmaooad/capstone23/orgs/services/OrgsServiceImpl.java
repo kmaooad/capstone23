9f5b6a3291de1a3e288d2b419e0883278fd53e96
Yehor Bilokin <91533724+Iwaway@users.noreply.github.com>
2023-11-13 17:30:42 +0200

    Refactor org service and delete excess service

--- app/src/main/java/edu/kmaooad/capstone23/orgs/services/OrgsServiceImpl.java
+++ app/src/main/java/edu/kmaooad/capstone23/orgs/services/OrgsServiceImpl.java
@@ -1,11 +1,11 @@
 package edu.kmaooad.capstone23.orgs.services;
 
-import edu.kmaooad.capstone23.orgs.dal.Org;
-import edu.kmaooad.capstone23.orgs.dal.OrgsRepository;
+import edu.kmaooad.capstone23.orgs.dal.*;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.bson.types.ObjectId;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,55 +13,101 @@ import java.util.Optional;
 public class OrgsServiceImpl implements OrgsService {
 
     @Inject
-    OrgsRepository repository;
+    OrgsRepository orgsRepository;
+
+    @Inject
+    JobsRepository jobRepository;
+
+    @Inject
+    RequestsRepository requestsRepository;
 
     @Override
     public Org insert(Org org) {
-        return repository.insert(org);
+        return orgsRepository.insert(org);
+    }
+
+    @Override
+    public Org createOrg(String name, String description, String industry, String website, String emailDomain) {
+      Org org = new Org();
+      org.name = name;
+      org.description = description;
+      org.jobs = new ArrayList<>();
+      org.industry = industry;
+      org.website = website;
+      org.emailDomain = emailDomain;
+      org.isActive=true;
+      return orgsRepository.insert(org);
     }
 
     @Override
     public List<Org> bulkInsert(List<Org> orgs) {
-        return repository.bulkInsert(orgs);
+        return orgsRepository.bulkInsert(orgs);
     }
     
     @Override
     public Org findByName(String name){
-        return repository.findByName(name);
+        return orgsRepository.findByName(name);
     }
 
     @Override
-    public Org findById(String id) {
-        return repository.findById(id);
+    public Org findById(ObjectId objectId) {
+        return orgsRepository.findById(objectId);
+    }
+
+    @Override
+    public Org findById(String objectId) {
+        return orgsRepository.findById(objectId);
     }
 
     @Override
     public Org getByPos(int position) {
-        return repository.listAll().get(position);
+        return orgsRepository.listAll().get(position);
     }
     
     @Override
     public Optional<Org> findByEmailDomainOptional(String email){
-        return repository.findByEmailDomainOptional(email);
+        return orgsRepository.findByEmailDomainOptional(email);
     }
 
     @Override
     public Optional<Org> findByIdOptional(String id){
-        return repository.findByIdOptional(id);
+        return orgsRepository.findByIdOptional(id);
     }
 
     @Override
     public boolean deleteById(ObjectId id) {
-        return repository.deleteById(id);
+        return orgsRepository.deleteById(id);
     }
 
     @Override
     public boolean isNotNull(){
-        return repository != null;
+        return orgsRepository != null;
     }
 
     @Override
     public long count(){
-        return repository.count();
+        return orgsRepository.count();
+    }
+
+    @Override
+    public void update(Org org){
+        orgsRepository.update(org);
+    }
+
+//Request
+    @Override
+    public Request insertRequest(Request request){
+        return requestsRepository.insert(request);
+    }
+
+//Jobs
+    @Override
+    public List<Job> findJobsByOrgId(String id) {
+        return jobRepository.findByOrgId(id);
+    }
+
+    @Override
+    public void updateJob(Job job){
+        jobRepository.update(job);
     }
 }
