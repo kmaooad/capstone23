10200ba1047663f5cdde3637c392a1c379ab5483
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2023-11-18 12:28:06 +0200

    Fix realization AccessRuleService

--- app/src/main/java/edu/kmaooad/capstone23/access_rules/handlers/DeleteAccessRuleHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/access_rules/handlers/DeleteAccessRuleHandler.java
@@ -8,19 +8,21 @@ import edu.kmaooad.capstone23.common.ErrorCode;
 import edu.kmaooad.capstone23.common.Result;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
+import org.bson.types.ObjectId;
 
 @RequestScoped
 public class DeleteAccessRuleHandler implements CommandHandler<DeleteAccessRule, AccessRuleDeleted> {
 
     @Inject
-    private AccessRuleService accessRuleService;
+    AccessRuleService accessRuleService;
 
     public Result<AccessRuleDeleted> handle(DeleteAccessRule command) {
         if (accessRuleService == null) {
             return new Result<>(ErrorCode.VALIDATION_FAILED, "Access rule doesn't exist");
         }
 
-        accessRuleService.deleteAccessRule(command.getId());
+        ObjectId id = ObjectId.isValid(command.getId()) ? new ObjectId(command.getId()) : null;
+        accessRuleService.deleteAccessRule(id);
         return new Result<>(new AccessRuleDeleted(command.getId()));
     }
 }
