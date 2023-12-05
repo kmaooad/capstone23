a0298c5c57e0318969c44dc5bc680375ffe14321
Andriiko <andrii.permiakov@getimpulse.app>
2023-12-05 20:26:03 +0200

    add inteface and service class

---
+++ app/src/main/java/edu/kmaooad/capstone23/notifications/commands/services/NotificationSchedulingService.java
@@ -0,0 +1,7 @@
+package edu.kmaooad.capstone23.notifications.commands.services;
+
+import edu.kmaooad.capstone23.notifications.commands.interfaces.NotificationSchedulingServiceInterface;
+
+public final class NotificationSchedulingService implements NotificationSchedulingServiceInterface {
+
+}
