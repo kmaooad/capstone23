d7f5c18189e7f609c849c896dcb01800da7036be
Danylo Litvinchuk <ltvnchkd@gmail.com>
2023-12-05 21:05:01 +0200

    conformance added to handler

--- app/src/main/java/edu/kmaooad/capstone23/notifications/handlers/PushNotificationHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/notifications/handlers/PushNotificationHandler.java
@@ -1,5 +1,14 @@
 package edu.kmaooad.capstone23.notifications.handlers;
 
-public class PushNotificationHandler {
+import edu.kmaooad.capstone23.common.CommandHandler;
+import edu.kmaooad.capstone23.common.Result;
+import edu.kmaooad.capstone23.notifications.commands.PushNotification;
+import edu.kmaooad.capstone23.notifications.events.NotificationPushed;
 
+public class PushNotificationHandler implements CommandHandler<PushNotification, NotificationPushed> {
+
+    @Override
+    public Result<NotificationPushed> handle(PushNotification command) {
+        return null;
+    }
 }
