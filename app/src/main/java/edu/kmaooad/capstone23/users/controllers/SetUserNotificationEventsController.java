fc088f92cc49a5201c4e4dcef628169e399849a2
Andrii Dubovyk <and.dubovyk@gmail.com>
2023-12-05 19:21:45 +0200

    Add set user notification events tests

--- app/src/main/java/edu/kmaooad/capstone23/users/controllers/SetUserNotificationEventsController.java
+++ app/src/main/java/edu/kmaooad/capstone23/users/controllers/SetUserNotificationEventsController.java
@@ -1,8 +1,8 @@
 package edu.kmaooad.capstone23.users.controllers;
 
 import edu.kmaooad.capstone23.common.TypicalController;
-import edu.kmaooad.capstone23.users.commands.SetUserNotificationMethods;
-import edu.kmaooad.capstone23.users.events.UserNotificationMethodsSetted;
+import edu.kmaooad.capstone23.users.commands.SetUserNotificationEvents;
+import edu.kmaooad.capstone23.users.events.UserNotificationEventsSetted;
 import jakarta.ws.rs.Path;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
@@ -10,6 +10,6 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
 @Path("/users/set-notification-events")
 @APIResponse(responseCode = "200", content = {
-        @Content(mediaType = "application/json", schema = @Schema(implementation = UserNotificationMethodsSetted.class))})
-public class SetUserNotificationEventsController extends TypicalController<SetUserNotificationMethods, UserNotificationMethodsSetted> {
+        @Content(mediaType = "application/json", schema = @Schema(implementation = UserNotificationEventsSetted.class))})
+public class SetUserNotificationEventsController extends TypicalController<SetUserNotificationEvents, UserNotificationEventsSetted> {
 }
