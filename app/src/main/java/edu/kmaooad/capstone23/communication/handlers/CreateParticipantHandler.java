3d2cf3b2e04c755315c4109a5157a52b37277983
kkpagaev <kkpagaev@gmail.com>
2023-12-05 21:09:44 +0200

    added notification when added to a chat

--- app/src/main/java/edu/kmaooad/capstone23/communication/handlers/CreateParticipantHandler.java
+++ app/src/main/java/edu/kmaooad/capstone23/communication/handlers/CreateParticipantHandler.java
@@ -7,6 +7,8 @@ import edu.kmaooad.capstone23.communication.commands.CreateParticipant;
 import edu.kmaooad.capstone23.communication.dal.entities.Participant;
 import edu.kmaooad.capstone23.communication.events.ParticipantCreated;
 import edu.kmaooad.capstone23.communication.services.ParticipantService;
+import edu.kmaooad.capstone23.notifications.models.NotificationType;
+import edu.kmaooad.capstone23.notifications.services.NotificationService;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import org.bson.types.ObjectId;
@@ -15,6 +17,9 @@ import org.bson.types.ObjectId;
 public class CreateParticipantHandler implements CommandHandler<CreateParticipant, ParticipantCreated> {
   @Inject
   ParticipantService participantService;
+
+  @Inject
+  NotificationService notificationService;
 
   @Override
   public Result<ParticipantCreated> handle(CreateParticipant command) {
@@ -31,6 +36,8 @@ public class CreateParticipantHandler implements CommandHandler<CreateParticipan
     var participant = this.participantService.insert(mappedParticipant);
 
     ParticipantCreated createdParticipant = new ParticipantCreated(participant.id.toHexString());
+
+    this.notificationService.send(NotificationType.CHAT_PARTICIPANT_CREATED, new ObjectId(userId), "Successfuly added to chat " + chatId);
 
     return new Result<ParticipantCreated>(createdParticipant);
   }
