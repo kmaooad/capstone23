df772a3c68df5191d7d3711bbe41225e807b8426
Iryna Melnyk <iryna2.melnyk@ukma.edu.ua>
2023-12-05 13:41:24 -0500

    remove unused code and add notify function

--- app/src/main/java/edu/kmaooad/capstone23/notifications/services/NotificationServiceImpl.java
+++ app/src/main/java/edu/kmaooad/capstone23/notifications/services/NotificationServiceImpl.java
@@ -2,14 +2,16 @@ package edu.kmaooad.capstone23.notifications.services;
 
 import edu.kmaooad.capstone23.notifications.dal.Notification;
 import edu.kmaooad.capstone23.notifications.dal.NotificationRepository;
+import edu.kmaooad.capstone23.notifications.dal.NotificationType;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
 @ApplicationScoped
-public class NotificationServiceImpl implements NotificationService
-{
+public class NotificationServiceImpl implements NotificationService {
     @Inject
     NotificationRepository notificationRepository;
+    @Inject
+    NotifyProviderService notifyProviderService;
 
     @Override
     public Notification insert(Notification notification) {
@@ -17,7 +19,7 @@ public class NotificationServiceImpl implements NotificationService
     }
 
     @Override
-    public Notification findById(Notification notification) {
-        return notificationRepository.findById(notification.id);
+    public boolean notify(String userInfo, NotificationType type, String message) {
+        return notifyProviderService.sendNotification(userInfo, type, message);
     }
 }
